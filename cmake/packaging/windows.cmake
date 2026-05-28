Nonary/vibeshine

feat(display): brand experimental driver as Vibeshine
Rename the opt-in Sunshine driver surface to Vibeshine Display Driver in the UI, installer, docs, and packaging text while keeping the existing config key for compatibility.

Move the experimental driver switch to the end of display setup and make the readiness banner say SudoVDA driver ready or Vibeshine driver ready based on the selected backend.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.5 high

cmake/packaging/windows.cmake
@@ -63,7 +63,7 @@ install(FILES ${SUDOVDA_DRIVER_FILES}
         DESTINATION "drivers/sudovda"
         COMPONENT sudovda)
 
-# Drivers (experimental Sunshine virtual display)
+# Drivers (experimental Vibeshine Display Driver)
 set(SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR "${SUNSHINE_SOURCE_ASSETS_DIR}/windows/drivers/sunshine")
 set(SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT "${CMAKE_SOURCE_DIR}/packaging/windows/virtual_display_driver/refresh_driver_package.ps1")
 set(SUNSHINE_LIBVIRTUALDISPLAY_PREBUILT_DIR "" CACHE PATH "Path to a prebuilt libvirtualdisplay package root with driver/ and tools/")
@@ -85,11 +85,11 @@ unset(_sunshine_driver_optional_file)
 
 foreach(_sunshine_driver_file IN LISTS SUNSHINE_VIRTUAL_DISPLAY_DRIVER_FILES)
     if (NOT EXISTS "${_sunshine_driver_file}")
-        message(FATAL_ERROR "Required Sunshine virtual display driver artifact missing: ${_sunshine_driver_file}")
+        message(FATAL_ERROR "Required Vibeshine Display Driver artifact missing: ${_sunshine_driver_file}")
     endif()
     file(SIZE "${_sunshine_driver_file}" _sunshine_driver_file_size)
     if (_sunshine_driver_file_size EQUAL 0)
-        message(FATAL_ERROR "Required Sunshine virtual display driver artifact is empty (0 bytes): ${_sunshine_driver_file}")
+        message(FATAL_ERROR "Required Vibeshine Display Driver artifact is empty (0 bytes): ${_sunshine_driver_file}")
     endif()
 endforeach()
 unset(_sunshine_driver_file_size)
@@ -105,7 +105,7 @@ if(EXISTS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}")
                 -PackageDir "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR}"
         DEPENDS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}"
                 ${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_FILES}
-        COMMENT "Validating Sunshine virtual display driver package assets"
+        COMMENT "Validating Vibeshine Display Driver package assets"
         VERBATIM)
 
     add_custom_target(refresh_sunshine_virtual_display_driver_assets
@@ -116,7 +116,7 @@ if(EXISTS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}")
                 -PrebuiltPackageDir "${SUNSHINE_LIBVIRTUALDISPLAY_PREBUILT_DIR}"
                 -PackageDir "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR}"
         DEPENDS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}"
-        COMMENT "Building and refreshing Sunshine virtual display driver package assets"
+        COMMENT "Building and refreshing Vibeshine Display Driver package assets"
         VERBATIM)
 
     if(TARGET package_msi)
@@ -217,7 +217,7 @@ set(CPACK_COMPONENT_SUDOVDA_DESCRIPTION "Default virtual display driver.")
 set(CPACK_COMPONENT_SUDOVDA_GROUP "Drivers")
 set(CPACK_COMPONENT_SUDOVDA_REQUIRED true)
 
-set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DISPLAY_NAME "Sunshine Virtual Display Driver")
+set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DISPLAY_NAME "Vibeshine Display Driver")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DESCRIPTION "Experimental opt-in virtual display driver.")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_GROUP "Drivers")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_REQUIRED true)

packaging/windows/bootstrapper/VibeshineInstaller.cs
@@ -480,12 +480,12 @@ namespace VibeshineInstaller {
       _installPathGrid.Children.Add(_browseButton);
 
       _installVirtualDisplayCheckBox = new CheckBox {
-        Content = "Install experimental Sunshine virtual display driver",
+        Content = "Install experimental Vibeshine Display Driver",
         FontSize = 13,
         Foreground = new SolidColorBrush(Color.FromRgb(226, 235, 250)),
         Margin = new Thickness(0, 12, 0, 4),
         IsChecked = false,
-        ToolTip = "Experimental opt-in for the new Sunshine virtual display driver. SudoVDA remains the default."
+        ToolTip = "Experimental opt-in that may improve performance and smoothness on virtual displays. SudoVDA remains the default."
       };
 
       var installVirtualDisplayHintText = new TextBlock {
@@ -2126,7 +2126,7 @@ namespace VibeshineInstaller {
       Console.WriteLine();
       Console.WriteLine("Supported MSI properties:");
       Console.WriteLine("  INSTALL_ROOT=<path>  Install to a custom directory (default: %ProgramFiles%\\Sunshine)");
-      Console.WriteLine("  INSTALL_VIRTUAL_DISPLAY_DRIVER=1  Install the Sunshine virtual display driver");
+      Console.WriteLine("  INSTALL_VIRTUAL_DISPLAY_DRIVER=1  Install the experimental Vibeshine Display Driver");
       Console.WriteLine();
       Console.WriteLine("Examples:");
       Console.WriteLine("  VibeshineSetup.exe /qn");

src/config.h
@@ -184,7 +184,7 @@ namespace config {
       bool always_restore_from_golden;  ///< When true, prefer golden snapshot over session snapshots during restore (reduces stuck virtual screens).
       int snapshot_restore_hotkey;  ///< Virtual-key code for restore hotkey (0 disables).
       std::uint32_t snapshot_restore_hotkey_modifiers;  ///< Modifier flags for the restore hotkey.
-      bool use_sunshine_virtual_display_driver;  ///< Opt into the Sunshine virtual display driver instead of legacy virtual display drivers.
+      bool use_sunshine_virtual_display_driver;  ///< Opt into the Vibeshine Display Driver instead of legacy virtual display drivers.
       bool activate_virtual_display;  ///< Auto-activate Sunshine virtual display when selected as the target output.
       int virtual_display_permanent_count;  ///< Number of always-present Sunshine virtual displays to request when explicitly configured.
       bool virtual_display_permanent_count_configured;  ///< False preserves installs that predate this setting.

src_assets/windows/misc/migration/installer-migrations.ps1
@@ -333,7 +333,7 @@ $candidateJsonFiles = @(
 
 $changedAny = $false
 if (Update-SunshineVirtualDriverPreference -RootDir $rootDir -InstallVirtualDisplayDriver $InstallVirtualDisplayDriver) {
-    Write-Output 'Updated Sunshine virtual driver preference from installer selection.'
+    Write-Output 'Updated Vibeshine Display Driver preference from installer selection.'
     $changedAny = $true
 }
 

tests/integration/test_virtual_display_packaging.cpp
@@ -1,6 +1,6 @@
 /**
  * @file tests/integration/test_virtual_display_packaging.cpp
- * @brief Tests for Sunshine virtual display driver packaging invariants.
+ * @brief Tests for Vibeshine Display Driver packaging invariants.
  */
 #include "../tests_common.h"
 
@@ -191,8 +191,9 @@ TEST(SunshineVirtualDisplayPackaging, BootstrapperOffersSunshineDriverOptIn) {
 
   expect_contains(bootstrapper, "InternalInstallVirtualDisplay = false;");
   expect_contains(bootstrapper, "IsChecked = false,");
-  expect_contains(bootstrapper, "Install experimental Sunshine virtual display driver");
+  expect_contains(bootstrapper, "Install experimental Vibeshine Display Driver");
   expect_contains(bootstrapper, "SudoVDA remains the default. Existing SudoVDA or MttVDD drivers are left installed.");
+  expect_contains(bootstrapper, "may improve performance and smoothness on virtual displays");
   expect_contains(bootstrapper, "contentStack.Children.Add(tipsSection);");
   expect_contains(bootstrapper, "tipsStack.Children.Add(_installVirtualDisplayCheckBox);");
   EXPECT_EQ(bootstrapper.find("contentStack.Children.Add(_installVirtualDisplayCheckBox);"), std::string::npos);
@@ -228,7 +229,7 @@ TEST(SunshineVirtualDisplayPackaging, InstallerSelectionSeedsWebUiSunshineDriver
   expect_contains(migration, "[string]$InstallVirtualDisplayDriver");
   expect_contains(migration, "Update-SunshineVirtualDriverPreference");
   expect_contains(migration, "dd_use_sunshine_virtual_display_driver");
-  expect_contains(migration, "Updated Sunshine virtual driver preference from installer selection.");
+  expect_contains(migration, "Updated Vibeshine Display Driver preference from installer selection.");
   expect_contains(header, "use_sunshine_virtual_display_driver");
   expect_contains(config, "bool_f(vars, \"dd_use_sunshine_virtual_display_driver\", video.dd.use_sunshine_virtual_display_driver);");
   expect_contains(config, "\"dd_use_sunshine_virtual_display_driver\"");
@@ -237,8 +238,16 @@ TEST(SunshineVirtualDisplayPackaging, InstallerSelectionSeedsWebUiSunshineDriver
   expect_contains(audioVideo, "sunshineVirtualDriverEnabled");
   expect_contains(audioVideo, "config.dd_use_sunshine_virtual_display_driver");
   expect_contains(audioVideo, "config.dd_use_sunshine_virtual_display_driver_desc");
-  expect_contains(locale, "\"dd_use_sunshine_virtual_display_driver\": \"Sunshine virtual driver\"");
+  expect_contains(audioVideo, "currentDriverStatusMessage");
+  expect_contains(audioVideo, "virtual_display_status_sudovda_ready");
+  expect_contains(audioVideo, "virtual_display_status_vibeshine_ready");
+  expect_contains(locale, "\"dd_use_sunshine_virtual_display_driver\": \"Vibeshine Display Driver\"");
+  expect_contains(locale, "may improve performance and smoothness for games on virtual displays");
+  expect_contains(locale, "\"virtual_display_status_sudovda_ready\": \"SudoVDA driver ready\"");
+  expect_contains(locale, "\"virtual_display_status_vibeshine_ready\": \"Vibeshine driver ready\"");
   expect_contains(docs, "### dd_use_sunshine_virtual_display_driver");
+  expect_contains(docs, "experimental Vibeshine Display Driver");
+  EXPECT_LT(audioVideo.find("<FrameLimiterStep"), audioVideo.find("v-model:value=\"sunshineVirtualDriverEnabled\""));
 }
 
 TEST(SunshineVirtualDisplayPackaging, RuntimeFeatureFlagFallsBackToSudoVda) {
@@ -277,6 +286,7 @@ TEST(SunshineVirtualDisplayPackaging, InstallerKeepsSudoVdaDefaultAndSunshineDri
 
   expect_contains(cmake, "drivers/sudovda");
   expect_contains(cmake, "drivers/sunshine");
+  expect_contains(cmake, "Vibeshine Display Driver");
   expect_contains(actions, "SudoVdaRegistryDefaults");
   expect_contains(actions, "InstallSudovda");
   expect_contains(actions, "drivers\\sudovda\\install.ps1");

Nonary/Vibepollo

(cherry picked from commit 0a21a59692686d69c642b3eb37ec62c376c619c5)

cmake/packaging/windows.cmake
@@ -71,7 +71,7 @@ install(FILES ${SUDOVDA_DRIVER_FILES}
         DESTINATION "drivers/sudovda"
         COMPONENT sudovda)
 
-# Drivers (experimental Sunshine virtual display)
+# Drivers (experimental Vibepollo Display Driver)
 set(SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR "${SUNSHINE_SOURCE_ASSETS_DIR}/windows/drivers/sunshine")
 set(SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT "${CMAKE_SOURCE_DIR}/packaging/windows/virtual_display_driver/refresh_driver_package.ps1")
 set(SUNSHINE_LIBVIRTUALDISPLAY_PREBUILT_DIR "" CACHE PATH "Path to a prebuilt libvirtualdisplay package root with driver/ and tools/")
@@ -93,11 +93,11 @@ unset(_sunshine_driver_optional_file)
 
 foreach(_sunshine_driver_file IN LISTS SUNSHINE_VIRTUAL_DISPLAY_DRIVER_FILES)
     if (NOT EXISTS "${_sunshine_driver_file}")
-        message(FATAL_ERROR "Required Sunshine virtual display driver artifact missing: ${_sunshine_driver_file}")
+        message(FATAL_ERROR "Required Vibepollo Display Driver artifact missing: ${_sunshine_driver_file}")
     endif()
     file(SIZE "${_sunshine_driver_file}" _sunshine_driver_file_size)
     if (_sunshine_driver_file_size EQUAL 0)
-        message(FATAL_ERROR "Required Sunshine virtual display driver artifact is empty (0 bytes): ${_sunshine_driver_file}")
+        message(FATAL_ERROR "Required Vibepollo Display Driver artifact is empty (0 bytes): ${_sunshine_driver_file}")
     endif()
 endforeach()
 unset(_sunshine_driver_file_size)
@@ -113,7 +113,7 @@ if(EXISTS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}")
                 -PackageDir "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR}"
         DEPENDS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}"
                 ${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_FILES}
-        COMMENT "Validating Sunshine virtual display driver package assets"
+        COMMENT "Validating Vibepollo Display Driver package assets"
         VERBATIM)
 
     add_custom_target(refresh_sunshine_virtual_display_driver_assets
@@ -124,7 +124,7 @@ if(EXISTS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}")
                 -PrebuiltPackageDir "${SUNSHINE_LIBVIRTUALDISPLAY_PREBUILT_DIR}"
                 -PackageDir "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_SOURCE_DIR}"
         DEPENDS "${SUNSHINE_VIRTUAL_DISPLAY_DRIVER_REFRESH_SCRIPT}"
-        COMMENT "Building and refreshing Sunshine virtual display driver package assets"
+        COMMENT "Building and refreshing Vibepollo Display Driver package assets"
         VERBATIM)
 
     if(TARGET package_msi)
@@ -225,7 +225,7 @@ set(CPACK_COMPONENT_SUDOVDA_DESCRIPTION "Default virtual display driver.")
 set(CPACK_COMPONENT_SUDOVDA_GROUP "Drivers")
 set(CPACK_COMPONENT_SUDOVDA_REQUIRED true)
 
-set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DISPLAY_NAME "Sunshine Virtual Display Driver")
+set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DISPLAY_NAME "Vibepollo Display Driver")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_DESCRIPTION "Experimental opt-in virtual display driver.")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_GROUP "Drivers")
 set(CPACK_COMPONENT_VIRTUAL_DISPLAY_DRIVER_REQUIRED true)

packaging/windows/bootstrapper/VibeshineInstaller.cs
@@ -482,12 +482,12 @@ namespace VibepolloInstaller {
       _installPathGrid.Children.Add(_browseButton);
 
       _installVirtualDisplayCheckBox = new CheckBox {
-        Content = "Install experimental Sunshine virtual display driver",
+        Content = "Install experimental Vibepollo Display Driver",
         FontSize = 13,
         Foreground = new SolidColorBrush(Color.FromRgb(226, 235, 250)),
         Margin = new Thickness(0, 12, 0, 4),
         IsChecked = false,
-        ToolTip = "Experimental opt-in for the new Sunshine virtual display driver. SudoVDA remains the default."
+        ToolTip = "Experimental opt-in that may improve performance and smoothness on virtual displays. SudoVDA remains the default."
       };
 
       var installVirtualDisplayHintText = new TextBlock {
@@ -2164,7 +2164,7 @@ namespace VibepolloInstaller {
       Console.WriteLine();
       Console.WriteLine("Supported MSI properties:");
       Console.WriteLine("  INSTALL_ROOT=<path>  Install to a custom directory (default: %ProgramFiles%\\Apollo)");
-      Console.WriteLine("  INSTALL_VIRTUAL_DISPLAY_DRIVER=1  Install the Sunshine virtual display driver");
+      Console.WriteLine("  INSTALL_VIRTUAL_DISPLAY_DRIVER=1  Install the experimental Vibepollo Display Driver");
       Console.WriteLine();
       Console.WriteLine("Examples:");
       Console.WriteLine("  VibepolloSetup.exe /qn");

src/config.h
@@ -187,7 +187,7 @@ namespace config {
       bool always_restore_from_golden;  ///< When true, prefer golden snapshot over session snapshots during restore (reduces stuck virtual screens).
       int snapshot_restore_hotkey;  ///< Virtual-key code for restore hotkey (0 disables).
       std::uint32_t snapshot_restore_hotkey_modifiers;  ///< Modifier flags for the restore hotkey.
-      bool use_sunshine_virtual_display_driver;  ///< Opt into the Sunshine virtual display driver instead of legacy virtual display drivers.
+      bool use_sunshine_virtual_display_driver;  ///< Opt into the Vibepollo Display Driver instead of legacy virtual display drivers.
       bool activate_virtual_display;  ///< Auto-activate Sunshine virtual display when selected as the target output.
       int virtual_display_permanent_count;  ///< Number of always-present Sunshine virtual displays to request when explicitly configured.
       bool virtual_display_permanent_count_configured;  ///< False preserves installs that predate this setting.

src_assets/windows/misc/migration/installer-migrations.ps1
@@ -333,7 +333,7 @@ $candidateJsonFiles = @(
 
 $changedAny = $false
 if (Update-SunshineVirtualDriverPreference -RootDir $rootDir -InstallVirtualDisplayDriver $InstallVirtualDisplayDriver) {
-    Write-Output 'Updated Sunshine virtual driver preference from installer selection.'
+    Write-Output 'Updated Vibepollo Display Driver preference from installer selection.'
     $changedAny = $true
 }
 

tests/integration/test_virtual_display_packaging.cpp
@@ -1,6 +1,6 @@
 /**
  * @file tests/integration/test_virtual_display_packaging.cpp
- * @brief Tests for Sunshine virtual display driver packaging invariants.
+ * @brief Tests for Vibepollo Display Driver packaging invariants.
  */
 #include "../tests_common.h"
 
@@ -191,8 +191,9 @@ TEST(SunshineVirtualDisplayPackaging, BootstrapperOffersSunshineDriverOptIn) {
 
   expect_contains(bootstrapper, "InternalInstallVirtualDisplay = false;");
   expect_contains(bootstrapper, "IsChecked = false,");
-  expect_contains(bootstrapper, "Install experimental Sunshine virtual display driver");
+  expect_contains(bootstrapper, "Install experimental Vibepollo Display Driver");
   expect_contains(bootstrapper, "SudoVDA remains the default. Existing SudoVDA or MttVDD drivers are left installed.");
+  expect_contains(bootstrapper, "may improve performance and smoothness on virtual displays");
   expect_contains(bootstrapper, "contentStack.Children.Add(tipsSection);");
   expect_contains(bootstrapper, "tipsStack.Children.Add(_installVirtualDisplayCheckBox);");
   EXPECT_EQ(bootstrapper.find("contentStack.Children.Add(_installVirtualDisplayCheckBox);"), std::string::npos);
@@ -228,7 +229,7 @@ TEST(SunshineVirtualDisplayPackaging, InstallerSelectionSeedsWebUiSunshineDriver
   expect_contains(migration, "[string]$InstallVirtualDisplayDriver");
   expect_contains(migration, "Update-SunshineVirtualDriverPreference");
   expect_contains(migration, "dd_use_sunshine_virtual_display_driver");
-  expect_contains(migration, "Updated Sunshine virtual driver preference from installer selection.");
+  expect_contains(migration, "Updated Vibepollo Display Driver preference from installer selection.");
   expect_contains(header, "use_sunshine_virtual_display_driver");
   expect_contains(config, "bool_f(vars, \"dd_use_sunshine_virtual_display_driver\", video.dd.use_sunshine_virtual_display_driver);");
   expect_contains(config, "\"dd_use_sunshine_virtual_display_driver\"");
@@ -237,8 +238,16 @@ TEST(SunshineVirtualDisplayPackaging, InstallerSelectionSeedsWebUiSunshineDriver
   expect_contains(audioVideo, "sunshineVirtualDriverEnabled");
   expect_contains(audioVideo, "config.dd_use_sunshine_virtual_display_driver");
   expect_contains(audioVideo, "config.dd_use_sunshine_virtual_display_driver_desc");
-  expect_contains(locale, "\"dd_use_sunshine_virtual_display_driver\": \"Sunshine virtual driver\"");
+  expect_contains(audioVideo, "currentDriverStatusMessage");
+  expect_contains(audioVideo, "virtual_display_status_sudovda_ready");
+  expect_contains(audioVideo, "virtual_display_status_vibeshine_ready");
+  expect_contains(locale, "\"dd_use_sunshine_virtual_display_driver\": \"Vibepollo Display Driver\"");
+  expect_contains(locale, "may improve performance and smoothness for games on virtual displays");
+  expect_contains(locale, "\"virtual_display_status_sudovda_ready\": \"SudoVDA driver ready\"");
+  expect_contains(locale, "\"virtual_display_status_vibeshine_ready\": \"Vibepollo driver ready\"");
   expect_contains(docs, "### dd_use_sunshine_virtual_display_driver");
+  expect_contains(docs, "experimental Vibepollo Display Driver");
+  EXPECT_LT(audioVideo.find("<FrameLimiterStep"), audioVideo.find("v-model:value=\"sunshineVirtualDriverEnabled\""));
 }
 
 TEST(SunshineVirtualDisplayPackaging, RuntimeFeatureFlagFallsBackToSudoVda) {
@@ -277,6 +286,7 @@ TEST(SunshineVirtualDisplayPackaging, InstallerKeepsSudoVdaDefaultAndSunshineDri
 
   expect_contains(cmake, "drivers/sudovda");
   expect_contains(cmake, "drivers/sunshine");
+  expect_contains(cmake, "Vibepollo Display Driver");
   expect_contains(actions, "SudoVdaRegistryDefaults");
   expect_contains(actions, "InstallSudovda");
   expect_contains(actions, "drivers\\sudovda\\install.ps1");