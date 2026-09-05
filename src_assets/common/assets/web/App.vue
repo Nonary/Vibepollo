Nonary/vibeshine

feat(ui): polish v2 navigation, readiness, and page layouts
Make host readiness and common streaming tasks easier to find with
consistent themes, navigation, and progressively disclosed diagnostics.
Refine library and settings layouts, fix mobile keyboard behavior and
platform-specific pickers, and label stale performance samples.
Load secondary pages on demand to reduce initial JavaScript from 650 KB
to 199 KB. Validated production build, 24 unit tests, and 37 browser tests.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Max

src_assets/common/assets/web/App.vue
@@ -7,6 +7,7 @@ import AuthView from '@/views/AuthView.vue';
 import { useSystemStore } from '@/stores/system';
 
 const system = useSystemStore();
+const baseUrl = import.meta.env.BASE_URL;
 const { t } = useI18n();
 
 onMounted(() => {
@@ -16,7 +17,7 @@ onMounted(() => {
 
 <template>
   <div v-if="system.booting" class="boot-screen" role="status" aria-live="polite">
-    <img src="/images/logo-sunshine-45.png" alt="" width="45" height="45" />
+    <img :src="`${baseUrl}images/logo-sunshine-45.png`" alt="" width="45" height="45" />
     <div>
       <strong>Vibeshine</strong>
       <span>{{ t('ui.app.connecting') }}</span>

src_assets/common/assets/web/components/settings/LinuxCaptureStatus.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { StatusBadge } from '@/components/ui';
+import { StatusBadge, UiIcon } from '@/components/ui';
 import { linuxCaptureState, type ReadinessMetadata } from '@/utils/hostReadiness';
 const props = defineProps<{ metadata: ReadinessMetadata; virtualMode?: string }>();
 const { t, te } = useI18n();
@@ -13,47 +13,70 @@ const state = computed(() => linuxCaptureState(props.metadata, props.virtualMode
 </script>
 
 <template>
-  <section class="linux-capture" aria-labelledby="linux-capture-title">
-    <div class="linux-capture__heading">
+  <details class="linux-capture" :open="state === 'unavailable' || state === 'unknown'">
+    <summary class="linux-capture__heading">
+      <UiIcon name="devices" :size="20" />
       <h3 id="linux-capture-title">{{ t('ui.settings.linux.title') }}</h3>
       <StatusBadge
         :label="t(`ui.settings.linux.states.${state}`)"
         :tone="state === 'active' ? 'success' : state === 'unavailable' ? 'warning' : 'neutral'"
       />
+      <UiIcon class="linux-capture__chevron" name="chevron-down" />
+    </summary>
+    <div class="linux-capture__body">
+      <p>{{ t('ui.settings.linux.recommendation') }}</p>
+      <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
+      <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
+      <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
+      <a
+        v-if="state === 'unavailable' || state === 'unknown'"
+        href="https://github.com/Nonary/vibeshine/blob/vibe/docs/linux/install.md"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.settings.linux.repair') }}</a
+      >
     </div>
-    <p>{{ t('ui.settings.linux.recommendation') }}</p>
-    <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
-    <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
-    <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
-    <a
-      v-if="state === 'unavailable' || state === 'unknown'"
-      href="https://github.com/Nonary/vibeshine/blob/vibe/docs/linux/install.md"
-      target="_blank"
-      rel="noopener noreferrer"
-      >{{ t('ui.settings.linux.repair') }}</a
-    >
-  </section>
+  </details>
 </template>
 
 <style scoped>
 .linux-capture {
   margin-bottom: var(--vs-space-24);
-  padding: var(--vs-space-20);
-  border: 1px solid var(--vs-color-border-strong);
-  border-inline-start: 3px solid var(--vs-color-accent-default);
+  border: 1px solid var(--vs-color-border-subtle);
   border-radius: var(--vs-radius-card);
   background: var(--vs-color-bg-surface);
 }
 .linux-capture__heading {
   display: flex;
   align-items: center;
-  justify-content: space-between;
   flex-wrap: wrap;
   gap: var(--vs-space-12);
+  padding: var(--vs-space-16) var(--vs-space-20);
+  list-style: none;
+  cursor: pointer;
+  color: var(--vs-color-text-muted);
+  border-radius: inherit;
+}
+.linux-capture__heading::-webkit-details-marker {
+  display: none;
+}
+.linux-capture__heading:hover {
+  background: var(--vs-color-bg-subtle);
+}
+.linux-capture__heading h3 {
+  flex: 1;
+  min-width: 8rem;
+}
+.linux-capture[open] .linux-capture__chevron {
+  transform: rotate(180deg);
+}
+.linux-capture__body {
+  padding: 0 var(--vs-space-20) var(--vs-space-20);
+  font-size: var(--vs-type-size-control);
 }
 h3 {
   margin: 0;
-  font-size: 16px;
+  font-size: var(--vs-type-size-control);
 }
 p {
   margin: var(--vs-space-12) 0 0;

src_assets/common/assets/web/views/AuthView.vue
@@ -6,6 +6,7 @@ import { UiIcon } from '@/components/ui';
 import { useSystemStore } from '@/stores/system';
 
 const system = useSystemStore();
+const baseUrl = import.meta.env.BASE_URL;
 const { t } = useI18n();
 const username = ref('');
 const password = ref('');
@@ -47,7 +48,7 @@ async function submit(): Promise<void> {
   <main id="main-content" class="auth-page">
     <section class="auth-panel" aria-labelledby="auth-title">
       <div class="auth-brand">
-        <img src="/images/logo-sunshine-45.png" alt="" width="45" height="45" />
+        <img :src="`${baseUrl}images/logo-sunshine-45.png`" alt="" width="45" height="45" />
         <span>Vibeshine</span>
       </div>
 
@@ -56,11 +57,7 @@ async function submit(): Promise<void> {
         <div>
           <h1 id="auth-title">{{ t(isSetup ? 'auth.create_first_user' : 'auth.login_title') }}</h1>
           <p>
-            {{
-              isSetup
-                ? t('ui.auth.setup_description')
-                : t('ui.auth.login_description')
-            }}
+            {{ isSetup ? t('ui.auth.setup_description') : t('ui.auth.login_description') }}
           </p>
         </div>
       </div>
@@ -114,7 +111,11 @@ async function submit(): Promise<void> {
         </label>
 
         <button class="button button--primary auth-submit" type="submit" :disabled="submitting">
-          <span>{{ submitting ? t('_common.loading') : t(isSetup ? 'auth.create_user' : 'auth.login_sign_in') }}</span>
+          <span>{{
+            submitting
+              ? t('_common.loading')
+              : t(isSetup ? 'auth.create_user' : 'auth.login_sign_in')
+          }}</span>
           <UiIcon name="chevron-right" />
         </button>
       </form>

Nonary/Vibepollo

(cherry picked from commit 2fe5c50ab802c1f1c16af153de2d84b8b751d4d6)

src_assets/common/assets/web/App.vue
@@ -7,6 +7,7 @@ import AuthView from '@/views/AuthView.vue';
 import { useSystemStore } from '@/stores/system';
 
 const system = useSystemStore();
+const baseUrl = import.meta.env.BASE_URL;
 const { t } = useI18n();
 
 onMounted(() => {
@@ -16,7 +17,7 @@ onMounted(() => {
 
 <template>
   <div v-if="system.booting" class="boot-screen" role="status" aria-live="polite">
-    <img src="/images/logo-apollo-45.png" alt="" width="45" height="45" />
+    <img :src="`${baseUrl}images/logo-apollo-45.png`" alt="" width="45" height="45" />
     <div>
       <strong>Vibepollo</strong>
       <span>{{ t('ui.app.connecting') }}</span>

src_assets/common/assets/web/components/settings/LinuxCaptureStatus.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { StatusBadge } from '@/components/ui';
+import { StatusBadge, UiIcon } from '@/components/ui';
 import { linuxCaptureState, type ReadinessMetadata } from '@/utils/hostReadiness';
 const props = defineProps<{ metadata: ReadinessMetadata; virtualMode?: string }>();
 const { t, te } = useI18n();
@@ -13,47 +13,70 @@ const state = computed(() => linuxCaptureState(props.metadata, props.virtualMode
 </script>
 
 <template>
-  <section class="linux-capture" aria-labelledby="linux-capture-title">
-    <div class="linux-capture__heading">
+  <details class="linux-capture" :open="state === 'unavailable' || state === 'unknown'">
+    <summary class="linux-capture__heading">
+      <UiIcon name="devices" :size="20" />
       <h3 id="linux-capture-title">{{ t('ui.settings.linux.title') }}</h3>
       <StatusBadge
         :label="t(`ui.settings.linux.states.${state}`)"
         :tone="state === 'active' ? 'success' : state === 'unavailable' ? 'warning' : 'neutral'"
       />
+      <UiIcon class="linux-capture__chevron" name="chevron-down" />
+    </summary>
+    <div class="linux-capture__body">
+      <p>{{ t('ui.settings.linux.recommendation') }}</p>
+      <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
+      <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
+      <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
+      <a
+        v-if="state === 'unavailable' || state === 'unknown'"
+        href="https://github.com/Nonary/Vibepollo/blob/vibe/docs/linux/install.md"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.settings.linux.repair') }}</a
+      >
     </div>
-    <p>{{ t('ui.settings.linux.recommendation') }}</p>
-    <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
-    <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
-    <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
-    <a
-      v-if="state === 'unavailable' || state === 'unknown'"
-      href="https://github.com/Nonary/Vibepollo/blob/vibe/docs/linux/install.md"
-      target="_blank"
-      rel="noopener noreferrer"
-      >{{ t('ui.settings.linux.repair') }}</a
-    >
-  </section>
+  </details>
 </template>
 
 <style scoped>
 .linux-capture {
   margin-bottom: var(--vs-space-24);
-  padding: var(--vs-space-20);
-  border: 1px solid var(--vs-color-border-strong);
-  border-inline-start: 3px solid var(--vs-color-accent-default);
+  border: 1px solid var(--vs-color-border-subtle);
   border-radius: var(--vs-radius-card);
   background: var(--vs-color-bg-surface);
 }
 .linux-capture__heading {
   display: flex;
   align-items: center;
-  justify-content: space-between;
   flex-wrap: wrap;
   gap: var(--vs-space-12);
+  padding: var(--vs-space-16) var(--vs-space-20);
+  list-style: none;
+  cursor: pointer;
+  color: var(--vs-color-text-muted);
+  border-radius: inherit;
+}
+.linux-capture__heading::-webkit-details-marker {
+  display: none;
+}
+.linux-capture__heading:hover {
+  background: var(--vs-color-bg-subtle);
+}
+.linux-capture__heading h3 {
+  flex: 1;
+  min-width: 8rem;
+}
+.linux-capture[open] .linux-capture__chevron {
+  transform: rotate(180deg);
+}
+.linux-capture__body {
+  padding: 0 var(--vs-space-20) var(--vs-space-20);
+  font-size: var(--vs-type-size-control);
 }
 h3 {
   margin: 0;
-  font-size: 16px;
+  font-size: var(--vs-type-size-control);
 }
 p {
   margin: var(--vs-space-12) 0 0;

src_assets/common/assets/web/views/AuthView.vue
@@ -6,6 +6,7 @@ import { UiIcon } from '@/components/ui';
 import { useSystemStore } from '@/stores/system';
 
 const system = useSystemStore();
+const baseUrl = import.meta.env.BASE_URL;
 const { t } = useI18n();
 const username = ref('');
 const password = ref('');
@@ -47,7 +48,7 @@ async function submit(): Promise<void> {
   <main id="main-content" class="auth-page">
     <section class="auth-panel" aria-labelledby="auth-title">
       <div class="auth-brand">
-        <img src="/images/logo-apollo-45.png" alt="" width="45" height="45" />
+        <img :src="`${baseUrl}images/logo-apollo-45.png`" alt="" width="45" height="45" />
         <span>Vibepollo</span>
       </div>
 
@@ -56,11 +57,7 @@ async function submit(): Promise<void> {
         <div>
           <h1 id="auth-title">{{ t(isSetup ? 'auth.create_first_user' : 'auth.login_title') }}</h1>
           <p>
-            {{
-              isSetup
-                ? t('ui.auth.setup_description')
-                : t('ui.auth.login_description')
-            }}
+            {{ isSetup ? t('ui.auth.setup_description') : t('ui.auth.login_description') }}
           </p>
         </div>
       </div>
@@ -114,7 +111,11 @@ async function submit(): Promise<void> {
         </label>
 
         <button class="button button--primary auth-submit" type="submit" :disabled="submitting">
-          <span>{{ submitting ? t('_common.loading') : t(isSetup ? 'auth.create_user' : 'auth.login_sign_in') }}</span>
+          <span>{{
+            submitting
+              ? t('_common.loading')
+              : t(isSetup ? 'auth.create_user' : 'auth.login_sign_in')
+          }}</span>
           <UiIcon name="chevron-right" />
         </button>
       </form>