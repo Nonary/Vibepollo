Nonary/vibeshine

Complete v2 settings and prioritize Linux virtual screens

src_assets/common/assets/web/components/settings/LinuxCaptureStatus.vue
@@ -1,0 +1,68 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { StatusBadge } from '@/components/ui';
+import { linuxCaptureState, type ReadinessMetadata } from '@/utils/hostReadiness';
+const props = defineProps<{ metadata: ReadinessMetadata; virtualMode?: string }>();
+const { t, te } = useI18n();
+const reasonKey = computed(() => {
+  const key = `ui.settings.linux.reasons.${props.metadata.virtual_display?.reason || 'unknown'}`;
+  return te(key) || te(key, 'en') ? key : 'ui.settings.linux.reasons.unknown';
+});
+const state = computed(() => linuxCaptureState(props.metadata, props.virtualMode));
+</script>
+
+<template>
+  <section class="linux-capture" aria-labelledby="linux-capture-title">
+    <div class="linux-capture__heading">
+      <h3 id="linux-capture-title">{{ t('ui.settings.linux.title') }}</h3>
+      <StatusBadge
+        :label="t(`ui.settings.linux.states.${state}`)"
+        :tone="state === 'active' ? 'success' : state === 'unavailable' ? 'warning' : 'neutral'"
+      />
+    </div>
+    <p>{{ t('ui.settings.linux.recommendation') }}</p>
+    <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
+    <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
+    <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
+    <a
+      v-if="state === 'unavailable' || state === 'unknown'"
+      href="https://github.com/Nonary/vibeshine/blob/vibe/docs/linux/install.md"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ t('ui.settings.linux.repair') }}</a
+    >
+  </section>
+</template>
+
+<style scoped>
+.linux-capture {
+  margin-bottom: var(--vs-space-24);
+  padding: var(--vs-space-20);
+  border: 1px solid var(--vs-color-border-strong);
+  border-inline-start: 3px solid var(--vs-color-accent-default);
+  border-radius: var(--vs-radius-card);
+  background: var(--vs-color-bg-surface);
+}
+.linux-capture__heading {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: var(--vs-space-12);
+}
+h3 {
+  margin: 0;
+  font-size: 16px;
+}
+p {
+  margin: var(--vs-space-12) 0 0;
+}
+.linux-capture__detail {
+  color: var(--vs-color-text-secondary);
+}
+a {
+  display: inline-block;
+  margin-top: var(--vs-space-12);
+}
+</style>

src_assets/common/assets/web/components/settings/ReleaseNotes.vue
@@ -1,0 +1,99 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { AppButton, InlineAlert } from '@/components/ui';
+import {
+  githubReleaseToChangelogEntry,
+  mergeChangelogEntries,
+  type ChangelogEntry,
+  type GitHubReleaseLike,
+} from '@/utils/changelog';
+const props = defineProps<{ installedVersion?: string }>();
+const { t } = useI18n();
+const releases = ref<ChangelogEntry[]>([]);
+const loading = ref(false);
+const failed = ref(false);
+async function load(remote = false) {
+  if (loading.value) return;
+  loading.value = true;
+  failed.value = false;
+  try {
+    const response = await fetch(
+      remote ? 'https://api.github.com/repos/Nonary/vibeshine/releases' : '/assets/changelog.json',
+      { headers: { Accept: 'application/json' }, credentials: 'omit' },
+    );
+    if (!response.ok) throw new Error('releases-unavailable');
+    const data = await response.json();
+    const incoming = remote
+      ? (Array.isArray(data) ? data : [])
+          .map((entry: GitHubReleaseLike) => githubReleaseToChangelogEntry(entry))
+          .filter((entry): entry is ChangelogEntry => entry !== null)
+      : Array.isArray(data.releases)
+        ? data.releases
+        : [];
+    releases.value = mergeChangelogEntries(releases.value, incoming);
+  } catch {
+    failed.value = true;
+  } finally {
+    loading.value = false;
+  }
+}
+onMounted(() => void load());
+</script>
+<template>
+  <section class="release-notes">
+    <div class="release-notes__heading">
+      <h2>{{ t('ui.maintenance.releases.title') }}</h2>
+      <AppButton
+        :label="t('ui.maintenance.releases.check')"
+        :busy="loading"
+        :busy-label="t('ui.maintenance.releases.loading')"
+        variant="secondary"
+        @click="load(true)"
+      />
+    </div>
+    <p>
+      {{ t('ui.maintenance.releases.current') }}:
+      {{ props.installedVersion || t('_common.unknown') }}
+    </p>
+    <InlineAlert v-if="failed" tone="warning" :title="t('ui.maintenance.releases.unavailable')" />
+    <p v-else-if="!loading && !releases.length">{{ t('ui.maintenance.releases.empty') }}</p>
+    <details v-for="release in releases.slice(0, 10)" :key="release.tag">
+      <summary>{{ release.name || release.tag }} · {{ release.date }}</summary>
+      <p class="release-notes__body">{{ release.body }}</p>
+      <a
+        v-if="release.url?.startsWith('https://github.com/Nonary/vibeshine/releases/')"
+        :href="release.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.maintenance.releases.open') }}</a
+      >
+    </details>
+  </section>
+</template>
+<style scoped>
+.release-notes__heading {
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--vs-space-12);
+  align-items: center;
+  justify-content: space-between;
+}
+h2 {
+  font-size: 18px;
+  margin: 0;
+}
+details {
+  padding-block: var(--vs-space-16);
+  border-top: 1px solid var(--vs-color-border-subtle);
+}
+summary {
+  cursor: pointer;
+  font-weight: 600;
+}
+.release-notes__body {
+  white-space: pre-wrap;
+  overflow-wrap: anywhere;
+  color: var(--vs-color-text-secondary);
+}
+</style>

Nonary/Vibepollo

(cherry picked from commit 017cc6048bb6089111a8dfd6aaca1e65884917bc)
Migration adaptation: shared settings and cached encoder readiness; exclude unsupported Linux private-display telemetry. Preserve Vibepollo server/state commands and release identity.

src_assets/common/assets/web/components/settings/LinuxCaptureStatus.vue
@@ -1,0 +1,68 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { StatusBadge } from '@/components/ui';
+import { linuxCaptureState, type ReadinessMetadata } from '@/utils/hostReadiness';
+const props = defineProps<{ metadata: ReadinessMetadata; virtualMode?: string }>();
+const { t, te } = useI18n();
+const reasonKey = computed(() => {
+  const key = `ui.settings.linux.reasons.${props.metadata.virtual_display?.reason || 'unknown'}`;
+  return te(key) || te(key, 'en') ? key : 'ui.settings.linux.reasons.unknown';
+});
+const state = computed(() => linuxCaptureState(props.metadata, props.virtualMode));
+</script>
+
+<template>
+  <section class="linux-capture" aria-labelledby="linux-capture-title">
+    <div class="linux-capture__heading">
+      <h3 id="linux-capture-title">{{ t('ui.settings.linux.title') }}</h3>
+      <StatusBadge
+        :label="t(`ui.settings.linux.states.${state}`)"
+        :tone="state === 'active' ? 'success' : state === 'unavailable' ? 'warning' : 'neutral'"
+      />
+    </div>
+    <p>{{ t('ui.settings.linux.recommendation') }}</p>
+    <p class="linux-capture__detail">{{ t('ui.settings.linux.explanation') }}</p>
+    <p v-if="state === 'unavailable'">{{ t(reasonKey) }}</p>
+    <p v-if="metadata.linux?.session_role === 'greeter'">{{ t('ui.settings.linux.greeter') }}</p>
+    <a
+      v-if="state === 'unavailable' || state === 'unknown'"
+      href="https://github.com/Nonary/Vibepollo/blob/vibe/docs/linux/install.md"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ t('ui.settings.linux.repair') }}</a
+    >
+  </section>
+</template>
+
+<style scoped>
+.linux-capture {
+  margin-bottom: var(--vs-space-24);
+  padding: var(--vs-space-20);
+  border: 1px solid var(--vs-color-border-strong);
+  border-inline-start: 3px solid var(--vs-color-accent-default);
+  border-radius: var(--vs-radius-card);
+  background: var(--vs-color-bg-surface);
+}
+.linux-capture__heading {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: var(--vs-space-12);
+}
+h3 {
+  margin: 0;
+  font-size: 16px;
+}
+p {
+  margin: var(--vs-space-12) 0 0;
+}
+.linux-capture__detail {
+  color: var(--vs-color-text-secondary);
+}
+a {
+  display: inline-block;
+  margin-top: var(--vs-space-12);
+}
+</style>

src_assets/common/assets/web/components/settings/ReleaseNotes.vue
@@ -1,0 +1,99 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { AppButton, InlineAlert } from '@/components/ui';
+import {
+  githubReleaseToChangelogEntry,
+  mergeChangelogEntries,
+  type ChangelogEntry,
+  type GitHubReleaseLike,
+} from '@/utils/changelog';
+const props = defineProps<{ installedVersion?: string }>();
+const { t } = useI18n();
+const releases = ref<ChangelogEntry[]>([]);
+const loading = ref(false);
+const failed = ref(false);
+async function load(remote = false) {
+  if (loading.value) return;
+  loading.value = true;
+  failed.value = false;
+  try {
+    const response = await fetch(
+      remote ? 'https://api.github.com/repos/Nonary/Vibepollo/releases' : '/assets/changelog.json',
+      { headers: { Accept: 'application/json' }, credentials: 'omit' },
+    );
+    if (!response.ok) throw new Error('releases-unavailable');
+    const data = await response.json();
+    const incoming = remote
+      ? (Array.isArray(data) ? data : [])
+          .map((entry: GitHubReleaseLike) => githubReleaseToChangelogEntry(entry))
+          .filter((entry): entry is ChangelogEntry => entry !== null)
+      : Array.isArray(data.releases)
+        ? data.releases
+        : [];
+    releases.value = mergeChangelogEntries(releases.value, incoming);
+  } catch {
+    failed.value = true;
+  } finally {
+    loading.value = false;
+  }
+}
+onMounted(() => void load());
+</script>
+<template>
+  <section class="release-notes">
+    <div class="release-notes__heading">
+      <h2>{{ t('ui.maintenance.releases.title') }}</h2>
+      <AppButton
+        :label="t('ui.maintenance.releases.check')"
+        :busy="loading"
+        :busy-label="t('ui.maintenance.releases.loading')"
+        variant="secondary"
+        @click="load(true)"
+      />
+    </div>
+    <p>
+      {{ t('ui.maintenance.releases.current') }}:
+      {{ props.installedVersion || t('_common.unknown') }}
+    </p>
+    <InlineAlert v-if="failed" tone="warning" :title="t('ui.maintenance.releases.unavailable')" />
+    <p v-else-if="!loading && !releases.length">{{ t('ui.maintenance.releases.empty') }}</p>
+    <details v-for="release in releases.slice(0, 10)" :key="release.tag">
+      <summary>{{ release.name || release.tag }} · {{ release.date }}</summary>
+      <p class="release-notes__body">{{ release.body }}</p>
+      <a
+        v-if="release.url?.startsWith('https://github.com/Nonary/Vibepollo/releases/')"
+        :href="release.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.maintenance.releases.open') }}</a
+      >
+    </details>
+  </section>
+</template>
+<style scoped>
+.release-notes__heading {
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--vs-space-12);
+  align-items: center;
+  justify-content: space-between;
+}
+h2 {
+  font-size: 18px;
+  margin: 0;
+}
+details {
+  padding-block: var(--vs-space-16);
+  border-top: 1px solid var(--vs-color-border-subtle);
+}
+summary {
+  cursor: pointer;
+  font-weight: 600;
+}
+.release-notes__body {
+  white-space: pre-wrap;
+  overflow-wrap: anywhere;
+  color: var(--vs-color-text-secondary);
+}
+</style>