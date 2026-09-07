Nonary/vibeshine

feat(linux): prepare Vibeshine Arch release channel
Establish Vibeshine as a first-class Linux package identity with its own binary, configuration, services, and desktop metadata. Add validated Arch/CachyOS packaging, DKMS/MOK handling, and Nonary-owned Pages and signed repository publication while keeping candidate builds isolated on vibe-test until promotion.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol Medium

scripts/linux_build.sh
@@ -646,8 +646,8 @@ function run_step_cmake() {
     "-DBUILD_WERROR=ON"
     "-DCMAKE_BUILD_TYPE=Release"
     "-DCMAKE_INSTALL_PREFIX=/usr"
-    "-DSUNSHINE_ASSETS_DIR=share/sunshine"
-    "-DSUNSHINE_EXECUTABLE_PATH=/usr/bin/sunshine"
+    "-DSUNSHINE_ASSETS_DIR=share/vibeshine"
+    "-DSUNSHINE_EXECUTABLE_PATH=/usr/bin/vibeshine"
     "-DSUNSHINE_ENABLE_DRM=ON"
     "-DSUNSHINE_ENABLE_KWIN=ON"
     "-DSUNSHINE_ENABLE_PORTAL=ON"
@@ -698,11 +698,11 @@ function run_step_validation() {
   echo "Running step: Validation"
 
   # Run appstream validation, etc.
-  appstreamcli validate "build/dev.lizardbyte.app.Sunshine.metainfo.xml"
-  appstream-util validate "build/dev.lizardbyte.app.Sunshine.metainfo.xml"
-  desktop-file-validate "build/dev.lizardbyte.app.Sunshine.desktop"
+  appstreamcli validate "build/io.github.Nonary.vibeshine.metainfo.xml"
+  appstream-util validate "build/io.github.Nonary.vibeshine.metainfo.xml"
+  desktop-file-validate "build/io.github.Nonary.vibeshine.desktop"
   if [[ "$appimage_build" == 0 ]]; then
-    desktop-file-validate "build/dev.lizardbyte.app.Sunshine.terminal.desktop"
+    desktop-file-validate "build/io.github.Nonary.vibeshine.terminal.desktop"
   fi
   return 0
 }

src/platform/common_services.cpp
@@ -75,6 +75,6 @@ namespace platf::services {
   std::filesystem::path home_config_root(const std::optional<std::filesystem::path> &home,
                                          const std::filesystem::path &fallback_home) {
     const auto &root = home && !home->empty() ? *home : fallback_home;
-    return root / ".config" / "sunshine";
+    return root / ".config" / "vibeshine";
   }
 }  // namespace platf::services

src/platform/common_services.h
@@ -47,7 +47,7 @@ namespace platf::services {
     reader_t _reader;
   };
 
-  std::string host_name_or(const host_name_provider_t &provider, std::string fallback = "Sunshine");
+  std::string host_name_or(const host_name_provider_t &provider, std::string fallback = "Vibeshine");
 
   std::filesystem::path home_config_root(const std::optional<std::filesystem::path> &home,
                                          const std::filesystem::path &fallback_home);

tests/unit/platform/test_common.cpp
@@ -86,7 +86,7 @@ TEST(HostNameService, UsesValueAndFallsBackForEmptyOrFailure) {
   EXPECT_EQ(platf::services::host_name_or(provider), "deterministic-host");
 
   provider.value = "";
-  EXPECT_EQ(platf::services::host_name_or(provider), "Sunshine");
+  EXPECT_EQ(platf::services::host_name_or(provider), "Vibeshine");
 
   provider.throws = true;
   EXPECT_EQ(platf::services::host_name_or(provider, "fallback"), "fallback");
@@ -95,10 +95,10 @@ TEST(HostNameService, UsesValueAndFallsBackForEmptyOrFailure) {
 TEST(AppDataRootPolicy, PrefersProvidedHomeAndUsesFallbackWhenMissing) {
   EXPECT_EQ(
     platf::services::home_config_root(std::filesystem::path {"/provided"}, "/fallback"),
-    std::filesystem::path {"/provided/.config/sunshine"}
+    std::filesystem::path {"/provided/.config/vibeshine"}
   );
   EXPECT_EQ(
     platf::services::home_config_root(std::nullopt, "/fallback"),
-    std::filesystem::path {"/fallback/.config/sunshine"}
+    std::filesystem::path {"/fallback/.config/vibeshine"}
   );
 }

tests/unit/test_network.cpp
@@ -20,9 +20,9 @@ INSTANTIATE_TEST_SUITE_P(
     std::make_tuple("shortname-123", "shortname-123"),
     std::make_tuple("space 123", "space-123"),
     std::make_tuple("hostname.domain.test", "hostname"),
-    std::make_tuple("&", "Sunshine"),
-    std::make_tuple("", "Sunshine"),
-    std::make_tuple("😁", "Sunshine"),
+    std::make_tuple("&", "Vibeshine"),
+    std::make_tuple("", "Vibeshine"),
+    std::make_tuple("😁", "Vibeshine"),
     std::make_tuple(std::string(128, 'a'), std::string(63, 'a'))
   )
 );

Nonary/Vibepollo

(cherry picked from commit afd33f284299fbc2f2d053da01c3d7ff5f53e2ed)

scripts/linux_build.sh
@@ -659,8 +659,8 @@ function run_step_cmake() {
     "-DBUILD_WERROR=ON"
     "-DCMAKE_BUILD_TYPE=Release"
     "-DCMAKE_INSTALL_PREFIX=/usr"
-    "-DSUNSHINE_ASSETS_DIR=share/sunshine"
-    "-DSUNSHINE_EXECUTABLE_PATH=/usr/bin/sunshine"
+    "-DSUNSHINE_ASSETS_DIR=share/vibepollo"
+    "-DSUNSHINE_EXECUTABLE_PATH=/usr/bin/vibepollo"
     "-DSUNSHINE_ENABLE_DRM=ON"
     "-DSUNSHINE_ENABLE_KWIN=ON"
     "-DSUNSHINE_ENABLE_PORTAL=ON"
@@ -711,11 +711,11 @@ function run_step_validation() {
   echo "Running step: Validation"
 
   # Run appstream validation, etc.
-  appstreamcli validate "build/dev.lizardbyte.app.Sunshine.metainfo.xml"
-  appstream-util validate "build/dev.lizardbyte.app.Sunshine.metainfo.xml"
-  desktop-file-validate "build/dev.lizardbyte.app.Sunshine.desktop"
+  appstreamcli validate "build/io.github.Nonary.vibepollo.metainfo.xml"
+  appstream-util validate "build/io.github.Nonary.vibepollo.metainfo.xml"
+  desktop-file-validate "build/io.github.Nonary.vibepollo.desktop"
   if [[ "$appimage_build" == 0 ]]; then
-    desktop-file-validate "build/dev.lizardbyte.app.Sunshine.terminal.desktop"
+    desktop-file-validate "build/io.github.Nonary.vibepollo.terminal.desktop"
   fi
   return 0
 }

src/platform/common_services.cpp
@@ -75,6 +75,6 @@ namespace platf::services {
   std::filesystem::path home_config_root(const std::optional<std::filesystem::path> &home,
                                          const std::filesystem::path &fallback_home) {
     const auto &root = home && !home->empty() ? *home : fallback_home;
-    return root / ".config" / "sunshine";
+    return root / ".config" / "vibepollo";
   }
 }  // namespace platf::services

src/platform/common_services.h
@@ -47,7 +47,7 @@ namespace platf::services {
     reader_t _reader;
   };
 
-  std::string host_name_or(const host_name_provider_t &provider, std::string fallback = "Sunshine");
+  std::string host_name_or(const host_name_provider_t &provider, std::string fallback = "Vibepollo");
 
   std::filesystem::path home_config_root(const std::optional<std::filesystem::path> &home,
                                          const std::filesystem::path &fallback_home);

tests/unit/platform/test_common.cpp
@@ -86,7 +86,7 @@ TEST(HostNameService, UsesValueAndFallsBackForEmptyOrFailure) {
   EXPECT_EQ(platf::services::host_name_or(provider), "deterministic-host");
 
   provider.value = "";
-  EXPECT_EQ(platf::services::host_name_or(provider), "Sunshine");
+  EXPECT_EQ(platf::services::host_name_or(provider), "Vibepollo");
 
   provider.throws = true;
   EXPECT_EQ(platf::services::host_name_or(provider, "fallback"), "fallback");
@@ -95,10 +95,10 @@ TEST(HostNameService, UsesValueAndFallsBackForEmptyOrFailure) {
 TEST(AppDataRootPolicy, PrefersProvidedHomeAndUsesFallbackWhenMissing) {
   EXPECT_EQ(
     platf::services::home_config_root(std::filesystem::path {"/provided"}, "/fallback"),
-    std::filesystem::path {"/provided/.config/sunshine"}
+    std::filesystem::path {"/provided/.config/vibepollo"}
   );
   EXPECT_EQ(
     platf::services::home_config_root(std::nullopt, "/fallback"),
-    std::filesystem::path {"/fallback/.config/sunshine"}
+    std::filesystem::path {"/fallback/.config/vibepollo"}
   );
 }

tests/unit/test_network.cpp
@@ -21,9 +21,9 @@ INSTANTIATE_TEST_SUITE_P(
     std::make_tuple("shortname-123", "shortname-123"),
     std::make_tuple("space 123", "space-123"),
     std::make_tuple("hostname.domain.test", "hostname"),
-    std::make_tuple("&", "Sunshine"),
-    std::make_tuple("", "Sunshine"),
-    std::make_tuple("😁", "Sunshine"),
+    std::make_tuple("&", "Vibepollo"),
+    std::make_tuple("", "Vibepollo"),
+    std::make_tuple("😁", "Vibepollo"),
     std::make_tuple(std::string(128, 'a'), std::string(63, 'a'))
   )
 );