Nonary/vibeshine

fix(capture): bound virtual timestamp smoothing
An exact free-running RTP grid smoothed virtual KMS jitter but could outrun a client’s fractional refresh clock and increase Moonlight frame-queue delay.

Use bounded phase correction that damps short-term VRR jitter while following long-term source cadence, rebases across real stalls, and reports packet phase for live verification.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol X-High

src/platform/linux/kmsgrab.cpp
@@ -1642,7 +1642,13 @@ namespace platf {
               }
               last_source_presentation_timestamp = captured_timestamp;
 
-              img_out->frame_timestamp = presentation_timestamp_grid.normalize(*captured_timestamp);
+              const auto packet_timestamp = presentation_timestamp_grid.normalize(*captured_timestamp);
+              packet_timestamp_phase_logger.collect_and_log(
+                std::chrono::duration<double, std::milli>(
+                  packet_timestamp - *captured_timestamp
+                ).count()
+              );
+              img_out->frame_timestamp = packet_timestamp;
               img_out->host_processing_timestamp = captured_timestamp;
             }
 
@@ -1787,6 +1793,11 @@ namespace platf {
       logging::min_max_avg_periodic_logger<double> source_presentation_interval_logger {
         debug,
         "Vibeshine DRM source presentation interval",
+        "ms"
+      };
+      logging::min_max_avg_periodic_logger<double> packet_timestamp_phase_logger {
+        debug,
+        "Vibeshine DRM packet timestamp phase",
         "ms"
       };
       std::uint64_t crtc_gamma_lut_blob_id {};

Nonary/Vibepollo

(cherry picked from commit a55afb54de72121651c0aba8d005af1ab1fc4d74)

src/platform/linux/kmsgrab.cpp
@@ -1642,7 +1642,13 @@ namespace platf {
               }
               last_source_presentation_timestamp = captured_timestamp;
 
-              img_out->frame_timestamp = presentation_timestamp_grid.normalize(*captured_timestamp);
+              const auto packet_timestamp = presentation_timestamp_grid.normalize(*captured_timestamp);
+              packet_timestamp_phase_logger.collect_and_log(
+                std::chrono::duration<double, std::milli>(
+                  packet_timestamp - *captured_timestamp
+                ).count()
+              );
+              img_out->frame_timestamp = packet_timestamp;
               img_out->host_processing_timestamp = captured_timestamp;
             }
 
@@ -1787,6 +1793,11 @@ namespace platf {
       logging::min_max_avg_periodic_logger<double> source_presentation_interval_logger {
         debug,
         "Vibepollo DRM source presentation interval",
+        "ms"
+      };
+      logging::min_max_avg_periodic_logger<double> packet_timestamp_phase_logger {
+        debug,
+        "Vibepollo DRM packet timestamp phase",
         "ms"
       };
       std::uint64_t crtc_gamma_lut_blob_id {};