Nonary/vibeshine

fix(steam): preserve launch option shell semantics
Steam launch options can begin with environment assignments or contain wrapper commands, but the direct-launch path passed the expanded text to an argv parser. Sunshine then treated the assignment as the executable and failed before UMU or Proton started.

Execute the fully expanded direct command through /bin/sh so existing Steam option semantics survive while Vibeshine's inner MangoHud wrapper continues to apply the managed limit last.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol Medium

tests/test_steam_integration.cpp
@@ -246,12 +246,12 @@ TEST(SteamLaunch, DirectLaunchPlacesVibeshineInsideInheritedSteamOptions) {
 
   EXPECT_EQ(
     launch_command(game),
-    "PROTON_DLSS_UPGRADE=3.7 mangohud "
+    "/bin/sh -c 'PROTON_DLSS_UPGRADE=3.7 mangohud "
     "vibeshine-mangohud --appid 1182900 -- env SteamAppId=1182900 SteamGameId=1182900 "
     "GAMEID=umu-1182900 STORE=steam "
-    "WINEPREFIX='/games/steamapps/compatdata/1182900' "
-    "PROTONPATH='/steam/compatibilitytools.d/GE-Proton11-5' "
-    "umu-run '/games/A Plague Tale/APlagueTaleRequiem_x64.exe' -windowed"
+    "WINEPREFIX='\\''/games/steamapps/compatdata/1182900'\\'' "
+    "PROTONPATH='\\''/steam/compatibilitytools.d/GE-Proton11-5'\\'' "
+    "umu-run '\\''/games/A Plague Tale/APlagueTaleRequiem_x64.exe'\\'' -windowed'"
   );
 }
 
@@ -265,8 +265,8 @@ TEST(SteamLaunch, DirectNativeLaunchTreatsOptionsWithoutPlaceholderAsArguments) 
 
   EXPECT_EQ(
     launch_command(game),
-    "vibeshine-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
-    "'/games/Spacewar/spacewar' -default -user-option"
+    "/bin/sh -c 'vibeshine-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
+    "'\\''/games/Spacewar/spacewar'\\'' -default -user-option'"
   );
 }
 

tests/test_steam_sync.cpp
@@ -60,8 +60,8 @@ TEST(SteamSync, PublishesResolvedDirectCommandAndWorkingDirectory) {
   ASSERT_TRUE(platf::steam::sync::policy::reconcile(root, {game}));
   ASSERT_EQ(root["apps"].size(), 1U);
   EXPECT_EQ(root["apps"][0]["cmd"],
-            "mangohud vibeshine-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
-            "'/games/Direct/direct'");
+            "/bin/sh -c 'mangohud vibeshine-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
+            "'\\''/games/Direct/direct'\\'''");
   EXPECT_EQ(root["apps"][0]["working-dir"], "/games/Direct");
 }
 #endif

Nonary/Vibepollo

(cherry picked from commit 66c2bc23d5e255f02a4d397472a7535bbe1fefa7)

tests/test_steam_integration.cpp
@@ -246,12 +246,12 @@ TEST(SteamLaunch, DirectLaunchPlacesVibepolloInsideInheritedSteamOptions) {
 
   EXPECT_EQ(
     launch_command(game),
-    "PROTON_DLSS_UPGRADE=3.7 mangohud "
+    "/bin/sh -c 'PROTON_DLSS_UPGRADE=3.7 mangohud "
     "vibepollo-mangohud --appid 1182900 -- env SteamAppId=1182900 SteamGameId=1182900 "
     "GAMEID=umu-1182900 STORE=steam "
-    "WINEPREFIX='/games/steamapps/compatdata/1182900' "
-    "PROTONPATH='/steam/compatibilitytools.d/GE-Proton11-5' "
-    "umu-run '/games/A Plague Tale/APlagueTaleRequiem_x64.exe' -windowed"
+    "WINEPREFIX='\\''/games/steamapps/compatdata/1182900'\\'' "
+    "PROTONPATH='\\''/steam/compatibilitytools.d/GE-Proton11-5'\\'' "
+    "umu-run '\\''/games/A Plague Tale/APlagueTaleRequiem_x64.exe'\\'' -windowed'"
   );
 }
 
@@ -265,8 +265,8 @@ TEST(SteamLaunch, DirectNativeLaunchTreatsOptionsWithoutPlaceholderAsArguments) 
 
   EXPECT_EQ(
     launch_command(game),
-    "vibepollo-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
-    "'/games/Spacewar/spacewar' -default -user-option"
+    "/bin/sh -c 'vibepollo-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
+    "'\\''/games/Spacewar/spacewar'\\'' -default -user-option'"
   );
 }
 

tests/test_steam_sync.cpp
@@ -60,8 +60,8 @@ TEST(SteamSync, PublishesResolvedDirectCommandAndWorkingDirectory) {
   ASSERT_TRUE(platf::steam::sync::policy::reconcile(root, {game}));
   ASSERT_EQ(root["apps"].size(), 1U);
   EXPECT_EQ(root["apps"][0]["cmd"],
-            "mangohud vibepollo-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
-            "'/games/Direct/direct'");
+            "/bin/sh -c 'mangohud vibepollo-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
+            "'\\''/games/Direct/direct'\\'''");
   EXPECT_EQ(root["apps"][0]["working-dir"], "/games/Direct");
 }
 #endif