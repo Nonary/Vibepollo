Nonary/vibeshine

fix(linux): secure graphical stream handoff
Replace the root pam_exec shell boundary with a native PAM module that validates the login identity, generates per-session tokens, and invokes a fixed helper with an empty environment. Serialize per-UID handoffs, reject stale closes, fail closed on broker errors, and roll back pre-login streaming without blocking a valid desktop login. Package the required PAM and IPC dependencies and require CAP_SYS_ADMIN for event-driven presentation waits.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol Medium

cmake/packaging/linux.cmake
@@ -26,6 +26,21 @@ else()
     find_package(Udev)
 
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        find_path(VIBESHINE_PAM_INCLUDE_DIR security/pam_modules.h REQUIRED)
+        find_library(VIBESHINE_PAM_LIBRARY pam REQUIRED)
+        get_filename_component(VIBESHINE_PAM_LIBRARY_DIR "${VIBESHINE_PAM_LIBRARY}" DIRECTORY)
+        set(VIBESHINE_PAM_MODULE_INSTALL_DIR "${VIBESHINE_PAM_LIBRARY_DIR}/security")
+        add_library(pam_vibeshine_session MODULE
+                "${CMAKE_SOURCE_DIR}/packaging/linux/pam_vibeshine_session.c")
+        set_target_properties(pam_vibeshine_session PROPERTIES
+                PREFIX ""
+                OUTPUT_NAME pam_vibeshine_session)
+        target_include_directories(pam_vibeshine_session PRIVATE "${VIBESHINE_PAM_INCLUDE_DIR}")
+        target_link_libraries(pam_vibeshine_session PRIVATE "${VIBESHINE_PAM_LIBRARY}")
+        file(GENERATE
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
+                CONTENT "#define VIBESHINE_DRM_VERSION \"${PROJECT_VERSION_NUMERIC}\"\n")
+
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
@@ -35,6 +50,8 @@ else()
                 DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS vibeshine_vkms_peercred
                 RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
+        install(TARGETS pam_vibeshine_session
+                LIBRARY DESTINATION "${VIBESHINE_PAM_MODULE_INSTALL_DIR}")
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
@@ -48,6 +65,8 @@ else()
                 PATTERN "Makefile"
                 PATTERN "README*"
                 PATTERN "LICENSE*")
+        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
+                DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}")
         install(PROGRAMS "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/build-module"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}")
         install(FILES "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-dkms.conf"
@@ -126,24 +145,29 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             libdrm2, \
             libgbm1, \
             libevdev2, \
+            iproute2, \
             kmod, \
             libkscreen-bin | libkf5screen-bin, \
             make, \
             libnuma1, \
             libopus0, \
+            libpam0g, \
             libpulse0, \
             libva2, \
             libva-drm2, \
             libwayland-client0, \
             libx11-6, \
             miniupnpc, \
-            openssl | libssl3")
+            openssl | libssl3, \
+            socat, \
+            util-linux")
 set(CPACK_RPM_PACKAGE_REQUIRES "\
             ${CPACK_RPM_PLATFORM_PACKAGE_REQUIRES} \
             libcap >= 2.22, \
             libcurl >= 7.0, \
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \
+            iproute, \
             kmod, \
             libkscreen, \
             make, \
@@ -155,7 +179,10 @@ set(CPACK_RPM_PACKAGE_REQUIRES "\
             miniupnpc >= 2.2.4, \
             numactl-libs >= 2.0.14, \
             openssl >= 3.0.2, \
+            pam, \
             pulseaudio-libs >= 10.0, \
+            socat, \
+            util-linux, \
             which >= 2.21")
 set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "dkms")
 set(CPACK_RPM_PACKAGE_SUGGESTS "dkms, gcc, kernel-devel")

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -11,11 +11,14 @@ def require(text: str, needle: str, label: str) -> None:
 
 root = pathlib.Path(sys.argv[1])
 unit = (root / "packaging/linux/app-io.github.Nonary.vibeshine.service.in").read_text()
+pam_module = (root / "packaging/linux/pam_vibeshine_session.c").read_text()
 handoff = (root / "packaging/linux/vibeshine-session-handoff").read_text()
 ready = (root / "packaging/linux/vibeshine-session-ready").read_text()
 restore_unit = (root / "packaging/linux/vibeshine-session-restore@.service").read_text()
 packaging = (root / "cmake/packaging/linux.cmake").read_text()
+package_configuration = (root / "cmake/prep/special_package_configuration.cmake").read_text()
 rpm_spec = (root / "packaging/linux/copr/Sunshine.spec").read_text()
+arch_package = (root / "packaging/linux/Arch/PKGBUILD").read_text()
 
 require(unit, "StartLimitIntervalSec=0", "user service")
 require(unit, "PartOf=graphical-session.target", "user service")
@@ -26,28 +29,49 @@ require(unit, "@SUNSHINE_SERVICE_READINESS_COMMAND@", "user service")
 if "StartLimitBurst=" in unit:
     raise AssertionError("user service must not retain a permanent start-limit latch")
 
-require(handoff, '[[ "${PAM_TYPE:-}" == close_session', "PAM handoff")
-require(handoff, "systemctl start --no-block \"$prelogin_service\"", "PAM handoff")
-require(handoff, 'systemctl --user stop "$desktop_target"', "PAM handoff")
-require(handoff, "desktop_target=graphical-session.target", "PAM handoff")
-require(handoff, 'signal=KILL "$desktop_compositor"', "PAM handoff")
-require(handoff, 'start --no-block "$desktop_service"', "PAM handoff")
-require(handoff, 'start --no-block "${restore_unit_prefix}${desktop_uid}.service"', "PAM handoff")
-require(handoff, 'restore_prelogin "$2"', "restore supervisor")
-require(handoff, "/usr/bin/sleep 1", "restore supervisor")
-require(handoff, 'start "$greeter_target"', "restore supervisor")
-require(handoff, "stream_ports_are_free", "PAM handoff")
-require(handoff, "release_connector", "PAM handoff")
-require(handoff, "root:root:644", "PAM handoff")
-require(handoff, "wait-prelogin", "pre-login readiness gate")
-require(handoff, "attempt & (attempt - 1)", "pre-login readiness gate")
-require(handoff, "maximum_delay=5", "pre-login readiness gate")
+# The privileged boundary obtains identity from PAM and execs a fixed helper
+# with an empty environment, so a login request cannot inject root shell or
+# dynamic-loader startup behavior.
+require(pam_module, "pam_get_item(pamh, PAM_SERVICE", "PAM module")
+require(pam_module, 'strcmp(service, "plasmalogin")', "PAM module")
+require(pam_module, "pam_get_user(pamh", "PAM module")
+require(pam_module, "getpwnam_r", "PAM module")
+require(pam_module, "getrandom", "PAM module")
+require(pam_module, "pam_set_data", "PAM module")
+require(pam_module, "pam_get_data", "PAM module")
+require(pam_module, "char *const environment[] = {NULL}", "PAM module")
+require(pam_module, "execve(helper_path, arguments, environment)", "PAM module")
+require(pam_module, "allowing the desktop login without streaming handoff", "fail-soft PAM policy")
+if "getenv(" in pam_module or "system(" in pam_module:
+    raise AssertionError("PAM module must not consume the caller environment or a shell")
 
+require(handoff, "pam-open USER UID TOKEN", "session helper")
+require(handoff, "pam-close USER UID TOKEN", "session helper")
+require(handoff, "session-handoffs", "per-UID handoff state")
+require(handoff, "session-restores", "per-UID restore state")
+require(handoff, "valid_token", "session token validation")
+require(handoff, "/usr/bin/flock --exclusive --timeout 30", "serialized handoff")
+require(handoff, "root:root:600:regular file", "trusted transition lock")
+require(handoff, "/usr/bin/env -i", "sanitized user-manager call")
+require(handoff, '[[ -S "$control_socket" && ! -L "$control_socket" ]]', "control socket validation")
+require(handoff, '/usr/bin/timeout --signal=KILL "$socket_timeout"', "control socket timeout")
+require(handoff, "rollback_to_prelogin", "transactional login handoff")
+require(handoff, "another desktop session owns the stream handoff", "multi-user exclusion")
+require(handoff, '[[ "$state_token" != "$token" ]]', "stale close rejection")
+require(handoff, 'systemctl start --no-block "$prelogin_service"', "pre-login restoration")
+require(handoff, 'user_systemctl "$desktop_user" stop "$desktop_target"', "desktop shutdown")
+require(handoff, "stream_ports_are_free", "port ownership handoff")
+require(handoff, "release_connector", "connector ownership handoff")
+require(handoff, "wait-prelogin", "pre-login readiness gate")
+require(handoff, "attempt & (attempt - 1)", "rate-limited status logging")
+if "PAM_TYPE" in handoff or "PAM_USER" in handoff:
+    raise AssertionError("session helper must not trust PAM environment variables")
+
+require(ready, "session-handoffs/$(/usr/bin/id -u)", "per-UID readiness gate")
 require(ready, "systemctl --user is-active --quiet plasma-kwin_wayland.service", "readiness gate")
 require(ready, "kscreen-doctor -j", "readiness gate")
 require(ready, "timeout --signal=KILL 5", "readiness gate")
 require(ready, "attempt & (attempt - 1)", "readiness gate")
-require(ready, "maximum_delay=5", "readiness gate")
 require(ready, "VIBESHINE_REQUIRE_PRELOGIN_HANDOFF", "readiness gate")
 require(ready, "kwin_service_is_installed", "readiness gate")
 
@@ -55,12 +79,22 @@ require(restore_unit, "Type=oneshot", "restore supervisor unit")
 require(restore_unit, "vibeshine-session-handoff restore %i", "restore supervisor unit")
 require(restore_unit, "TimeoutStartSec=infinity", "restore supervisor unit")
 
+require(packaging, "add_library(pam_vibeshine_session MODULE", "native PAM build")
+require(packaging, "install(TARGETS pam_vibeshine_session", "native PAM install")
+require(packaging, "libpam0g", "Debian PAM dependency")
+require(packaging, "socat", "native socket dependency")
+require(packaging, "iproute2", "Debian socket-inspection dependency")
+require(packaging, "util-linux", "native flock/runuser dependency")
+require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"',
+        "versioned kernel source fingerprint")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-handoff"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"', "native install")
 
-require(rpm_spec, "vibeshine-session-handoff", "RPM package")
-require(rpm_spec, "vibeshine-session-ready", "RPM package")
-require(rpm_spec, "vibeshine-session-restore@.service", "RPM package")
+require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")
+require(rpm_spec, "Requires: pam", "RPM PAM runtime dependency")
+require(rpm_spec, "pam_vibeshine_session.so", "RPM PAM module")
+for dependency in ("'pam'", "'socat'", "'iproute2'", "'util-linux'"):
+    require(arch_package, dependency, "Arch runtime dependency")
 
-print("PASS: Linux session handoff contract")
+print("PASS: hardened Linux session handoff contract")

Nonary/Vibepollo

(cherry picked from commit d93fa08ed45c0e94d3fd95acf448eff940a42954)

cmake/packaging/linux.cmake
@@ -26,6 +26,21 @@ else()
     find_package(Udev)
 
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        find_path(VIBESHINE_PAM_INCLUDE_DIR security/pam_modules.h REQUIRED)
+        find_library(VIBESHINE_PAM_LIBRARY pam REQUIRED)
+        get_filename_component(VIBESHINE_PAM_LIBRARY_DIR "${VIBESHINE_PAM_LIBRARY}" DIRECTORY)
+        set(VIBESHINE_PAM_MODULE_INSTALL_DIR "${VIBESHINE_PAM_LIBRARY_DIR}/security")
+        add_library(pam_vibepollo_session MODULE
+                "${CMAKE_SOURCE_DIR}/packaging/linux/pam_vibepollo_session.c")
+        set_target_properties(pam_vibepollo_session PROPERTIES
+                PREFIX ""
+                OUTPUT_NAME pam_vibepollo_session)
+        target_include_directories(pam_vibepollo_session PRIVATE "${VIBESHINE_PAM_INCLUDE_DIR}")
+        target_link_libraries(pam_vibepollo_session PRIVATE "${VIBESHINE_PAM_LIBRARY}")
+        file(GENERATE
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
+                CONTENT "#define VIBESHINE_DRM_VERSION \"${PROJECT_VERSION_NUMERIC}\"\n")
+
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
@@ -35,6 +50,8 @@ else()
                 DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS vibeshine_vkms_peercred
                 RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
+        install(TARGETS pam_vibepollo_session
+                LIBRARY DESTINATION "${VIBESHINE_PAM_MODULE_INSTALL_DIR}")
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
@@ -48,6 +65,8 @@ else()
                 PATTERN "Makefile"
                 PATTERN "README*"
                 PATTERN "LICENSE*")
+        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
+                DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}")
         install(PROGRAMS "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/build-module"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}")
         install(FILES "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-dkms.conf"
@@ -132,24 +151,29 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             libdrm2, \
             libgbm1, \
             libevdev2, \
+            iproute2, \
             kmod, \
             libkscreen-bin | libkf5screen-bin, \
             make, \
             libnuma1, \
             libopus0, \
+            libpam0g, \
             libpulse0, \
             libva2, \
             libva-drm2, \
             libwayland-client0, \
             libx11-6, \
             miniupnpc, \
-            openssl | libssl3")
+            openssl | libssl3, \
+            socat, \
+            util-linux")
 set(CPACK_RPM_PACKAGE_REQUIRES "\
             ${CPACK_RPM_PLATFORM_PACKAGE_REQUIRES} \
             libcap >= 2.22, \
             libcurl >= 7.0, \
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \
+            iproute, \
             kmod, \
             libkscreen, \
             make, \
@@ -161,7 +185,10 @@ set(CPACK_RPM_PACKAGE_REQUIRES "\
             miniupnpc >= 2.2.4, \
             numactl-libs >= 2.0.14, \
             openssl >= 3.0.2, \
+            pam, \
             pulseaudio-libs >= 10.0, \
+            socat, \
+            util-linux, \
             which >= 2.21")
 set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "dkms")
 set(CPACK_RPM_PACKAGE_SUGGESTS "dkms, gcc, kernel-devel")

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -11,11 +11,14 @@ def require(text: str, needle: str, label: str) -> None:
 
 root = pathlib.Path(sys.argv[1])
 unit = (root / "packaging/linux/app-io.github.Nonary.vibepollo.service.in").read_text()
+pam_module = (root / "packaging/linux/pam_vibepollo_session.c").read_text()
 handoff = (root / "packaging/linux/vibepollo-session-handoff").read_text()
 ready = (root / "packaging/linux/vibepollo-session-ready").read_text()
 restore_unit = (root / "packaging/linux/vibepollo-session-restore@.service").read_text()
 packaging = (root / "cmake/packaging/linux.cmake").read_text()
+package_configuration = (root / "cmake/prep/special_package_configuration.cmake").read_text()
 rpm_spec = (root / "packaging/linux/copr/Sunshine.spec").read_text()
+arch_package = (root / "packaging/linux/Arch/PKGBUILD").read_text()
 
 require(unit, "StartLimitIntervalSec=0", "user service")
 require(unit, "PartOf=graphical-session.target", "user service")
@@ -26,28 +29,49 @@ require(unit, "@SUNSHINE_SERVICE_READINESS_COMMAND@", "user service")
 if "StartLimitBurst=" in unit:
     raise AssertionError("user service must not retain a permanent start-limit latch")
 
-require(handoff, '[[ "${PAM_TYPE:-}" == close_session', "PAM handoff")
-require(handoff, "systemctl start --no-block \"$prelogin_service\"", "PAM handoff")
-require(handoff, 'systemctl --user stop "$desktop_target"', "PAM handoff")
-require(handoff, "desktop_target=graphical-session.target", "PAM handoff")
-require(handoff, 'signal=KILL "$desktop_compositor"', "PAM handoff")
-require(handoff, 'start --no-block "$desktop_service"', "PAM handoff")
-require(handoff, 'start --no-block "${restore_unit_prefix}${desktop_uid}.service"', "PAM handoff")
-require(handoff, 'restore_prelogin "$2"', "restore supervisor")
-require(handoff, "/usr/bin/sleep 1", "restore supervisor")
-require(handoff, 'start "$greeter_target"', "restore supervisor")
-require(handoff, "stream_ports_are_free", "PAM handoff")
-require(handoff, "release_connector", "PAM handoff")
-require(handoff, "root:root:644", "PAM handoff")
-require(handoff, "wait-prelogin", "pre-login readiness gate")
-require(handoff, "attempt & (attempt - 1)", "pre-login readiness gate")
-require(handoff, "maximum_delay=5", "pre-login readiness gate")
+# The privileged boundary obtains identity from PAM and execs a fixed helper
+# with an empty environment, so a login request cannot inject root shell or
+# dynamic-loader startup behavior.
+require(pam_module, "pam_get_item(pamh, PAM_SERVICE", "PAM module")
+require(pam_module, 'strcmp(service, "plasmalogin")', "PAM module")
+require(pam_module, "pam_get_user(pamh", "PAM module")
+require(pam_module, "getpwnam_r", "PAM module")
+require(pam_module, "getrandom", "PAM module")
+require(pam_module, "pam_set_data", "PAM module")
+require(pam_module, "pam_get_data", "PAM module")
+require(pam_module, "char *const environment[] = {NULL}", "PAM module")
+require(pam_module, "execve(helper_path, arguments, environment)", "PAM module")
+require(pam_module, "allowing the desktop login without streaming handoff", "fail-soft PAM policy")
+if "getenv(" in pam_module or "system(" in pam_module:
+    raise AssertionError("PAM module must not consume the caller environment or a shell")
 
+require(handoff, "pam-open USER UID TOKEN", "session helper")
+require(handoff, "pam-close USER UID TOKEN", "session helper")
+require(handoff, "session-handoffs", "per-UID handoff state")
+require(handoff, "session-restores", "per-UID restore state")
+require(handoff, "valid_token", "session token validation")
+require(handoff, "/usr/bin/flock --exclusive --timeout 30", "serialized handoff")
+require(handoff, "root:root:600:regular file", "trusted transition lock")
+require(handoff, "/usr/bin/env -i", "sanitized user-manager call")
+require(handoff, '[[ -S "$control_socket" && ! -L "$control_socket" ]]', "control socket validation")
+require(handoff, '/usr/bin/timeout --signal=KILL "$socket_timeout"', "control socket timeout")
+require(handoff, "rollback_to_prelogin", "transactional login handoff")
+require(handoff, "another desktop session owns the stream handoff", "multi-user exclusion")
+require(handoff, '[[ "$state_token" != "$token" ]]', "stale close rejection")
+require(handoff, 'systemctl start --no-block "$prelogin_service"', "pre-login restoration")
+require(handoff, 'user_systemctl "$desktop_user" stop "$desktop_target"', "desktop shutdown")
+require(handoff, "stream_ports_are_free", "port ownership handoff")
+require(handoff, "release_connector", "connector ownership handoff")
+require(handoff, "wait-prelogin", "pre-login readiness gate")
+require(handoff, "attempt & (attempt - 1)", "rate-limited status logging")
+if "PAM_TYPE" in handoff or "PAM_USER" in handoff:
+    raise AssertionError("session helper must not trust PAM environment variables")
+
+require(ready, "session-handoffs/$(/usr/bin/id -u)", "per-UID readiness gate")
 require(ready, "systemctl --user is-active --quiet plasma-kwin_wayland.service", "readiness gate")
 require(ready, "kscreen-doctor -j", "readiness gate")
 require(ready, "timeout --signal=KILL 5", "readiness gate")
 require(ready, "attempt & (attempt - 1)", "readiness gate")
-require(ready, "maximum_delay=5", "readiness gate")
 require(ready, "VIBEPOLLO_REQUIRE_PRELOGIN_HANDOFF", "readiness gate")
 require(ready, "kwin_service_is_installed", "readiness gate")
 
@@ -55,12 +79,22 @@ require(restore_unit, "Type=oneshot", "restore supervisor unit")
 require(restore_unit, "vibepollo-session-handoff restore %i", "restore supervisor unit")
 require(restore_unit, "TimeoutStartSec=infinity", "restore supervisor unit")
 
+require(packaging, "add_library(pam_vibepollo_session MODULE", "native PAM build")
+require(packaging, "install(TARGETS pam_vibepollo_session", "native PAM install")
+require(packaging, "libpam0g", "Debian PAM dependency")
+require(packaging, "socat", "native socket dependency")
+require(packaging, "iproute2", "Debian socket-inspection dependency")
+require(packaging, "util-linux", "native flock/runuser dependency")
+require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"',
+        "versioned kernel source fingerprint")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-handoff"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"', "native install")
 
-require(rpm_spec, "vibepollo-session-handoff", "RPM package")
-require(rpm_spec, "vibepollo-session-ready", "RPM package")
-require(rpm_spec, "vibepollo-session-restore@.service", "RPM package")
+require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")
+require(rpm_spec, "Requires: pam", "RPM PAM runtime dependency")
+require(rpm_spec, "pam_vibepollo_session.so", "RPM PAM module")
+for dependency in ("'pam'", "'socat'", "'iproute2'", "'util-linux'"):
+    require(arch_package, dependency, "Arch runtime dependency")
 
-print("PASS: Linux session handoff contract")
+print("PASS: hardened Linux session handoff contract")