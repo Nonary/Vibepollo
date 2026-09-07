Nonary/vibeshine

fix(linux): make a fresh package install stream out of the box
A first installation on a machine with no legacy per-user profile left the
machine host disabled: automatic setup demanded an existing profile, the
importer refused a missing one, command authorization refused a missing
apps.json, the host unit joined a vibeshine-uinput group no package created,
and readiness required an AV1 encoder that pre-AV1 GPUs cannot provide.

- select the single interactive desktop account when no profile exists and
  name the accounts when a choice is required
- treat a missing desktop profile and application list as a fresh start
- create the vibeshine-uinput group and assign uinput/uhid to it
- require only the Web UI listener and the H.264 encoder for readiness
- accept SDDM-started Plasma Wayland desktops and log why a session was refused
- pin the greeter to NVENC only when an NVIDIA display device is present
- ship firewalld/ufw service definitions and the PipeWire quantum drop-in
- print actionable next steps and header hints from the package hooks
- document requirements, post-install steps, and services; add beta.5 notes

Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01WucQDoN6766cSmGuX1r6jh

cmake/packaging/linux.cmake
@@ -153,6 +153,19 @@ else()
     if(UDEV_FOUND)
         install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.rules"
                 DESTINATION "${UDEV_RULES_INSTALL_DIR}")
+        if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/70-vibeshine-uinput.rules"
+                    DESTINATION "${UDEV_RULES_INSTALL_DIR}")
+        endif()
+    endif()
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        # Firewall service definitions and the PipeWire quantum the stream expects.
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/firewalld/vibeshine.xml"
+                DESTINATION "lib/firewalld/services")
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/ufw/vibeshine"
+                DESTINATION "${CMAKE_INSTALL_FULL_SYSCONFDIR}/ufw/applications.d")
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/pipewire/50-vibeshine-audio.conf"
+                DESTINATION "${CMAKE_INSTALL_DATADIR}/pipewire/pipewire.conf.d")
     endif()
     if(SYSTEMD_FOUND)
         if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")

Nonary/Vibepollo

(cherry picked from commit c7e31da27643cc04f57b00f7fbb03848f3e2fc7f)

cmake/packaging/linux.cmake
@@ -153,6 +153,19 @@ else()
     if(UDEV_FOUND)
         install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.rules"
                 DESTINATION "${UDEV_RULES_INSTALL_DIR}")
+        if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/70-vibepollo-uinput.rules"
+                    DESTINATION "${UDEV_RULES_INSTALL_DIR}")
+        endif()
+    endif()
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        # Firewall service definitions and the PipeWire quantum the stream expects.
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/firewalld/vibepollo.xml"
+                DESTINATION "lib/firewalld/services")
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/ufw/vibepollo"
+                DESTINATION "${CMAKE_INSTALL_FULL_SYSCONFDIR}/ufw/applications.d")
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/pipewire/50-vibepollo-audio.conf"
+                DESTINATION "${CMAKE_INSTALL_DATADIR}/pipewire/pipewire.conf.d")
     endif()
     if(SYSTEMD_FOUND)
         if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")