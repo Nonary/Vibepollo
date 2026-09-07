Nonary/vibeshine

fix(linux): Preserve host logs across restarts and reboots
Store host logs in persistent machine state so diagnostics survive service teardown. Reuse bounded retention across launches and ignore retained logs when checking startup readiness.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Medium

tests/unit/platform/linux/test_machine_host_manifest.sh
@@ -56,3 +56,38 @@ if normalize_application_value '0123456789' 9 >/dev/null 2>&1; then
 fi
 
 /usr/bin/printf 'PASS: bounded command manifest and permitted-only host policy\n'
+
+# Persistent history must never satisfy a new host's readiness check.
+test_logs=$(mktemp -d)
+trap 'rm -rf -- "$test_logs"' EXIT
+expected_owner=$(stat -c '%U:%G' "$test_logs")
+old_log=$test_logs/vibeshine-20260901-120000-000.log
+new_log=$test_logs/vibeshine-20260904-120000-000.log
+printf 'Configuration UI available at localhost\nFound H.264 encoder: h264_nvenc\n' >"$old_log"
+declare -A retained_logs=(["$old_log"]=1)
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'retained ready log was accepted for a new host'
+fi
+[[ -z "$host_log" ]] || fail_test 'stale readiness path was retained'
+# A rollover belongs to the same launch, but is not its current log.
+: >"$new_log.1"
+printf 'starting\n' >"$new_log"
+find_host_readiness_log "$test_logs" "$expected_owner" retained_logs ||
+  fail_test 'new host log was not discovered alongside persistent history'
+[[ "$host_log" == "$new_log" ]] || fail_test 'wrong host log selected'
+chmod 644 "$new_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'publicly readable host log was accepted'
+fi
+chmod 600 "$new_log"
+second_log=$test_logs/vibeshine-20260904-120001-000.log
+printf 'starting\n' >"$second_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'ambiguous new host logs were accepted'
+fi
+rm -- "$second_log" "$new_log"
+ln -s "$old_log" "$new_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'symlink host log was accepted'
+fi
+printf 'PASS: persistent host log readiness isolation\n'

Nonary/Vibepollo

(cherry picked from commit 23935e95f12a903ad3741c49159b5ab519939354)

tests/unit/platform/linux/test_machine_host_manifest.sh
@@ -56,3 +56,38 @@ if normalize_application_value '0123456789' 9 >/dev/null 2>&1; then
 fi
 
 /usr/bin/printf 'PASS: bounded command manifest and permitted-only host policy\n'
+
+# Persistent history must never satisfy a new host's readiness check.
+test_logs=$(mktemp -d)
+trap 'rm -rf -- "$test_logs"' EXIT
+expected_owner=$(stat -c '%U:%G' "$test_logs")
+old_log=$test_logs/vibepollo-20260901-120000-000.log
+new_log=$test_logs/vibepollo-20260904-120000-000.log
+printf 'Configuration UI available at localhost\nFound H.264 encoder: h264_nvenc\n' >"$old_log"
+declare -A retained_logs=(["$old_log"]=1)
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'retained ready log was accepted for a new host'
+fi
+[[ -z "$host_log" ]] || fail_test 'stale readiness path was retained'
+# A rollover belongs to the same launch, but is not its current log.
+: >"$new_log.1"
+printf 'starting\n' >"$new_log"
+find_host_readiness_log "$test_logs" "$expected_owner" retained_logs ||
+  fail_test 'new host log was not discovered alongside persistent history'
+[[ "$host_log" == "$new_log" ]] || fail_test 'wrong host log selected'
+chmod 644 "$new_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'publicly readable host log was accepted'
+fi
+chmod 600 "$new_log"
+second_log=$test_logs/vibepollo-20260904-120001-000.log
+printf 'starting\n' >"$second_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'ambiguous new host logs were accepted'
+fi
+rm -- "$second_log" "$new_log"
+ln -s "$old_log" "$new_log"
+if find_host_readiness_log "$test_logs" "$expected_owner" retained_logs; then
+  fail_test 'symlink host log was accepted'
+fi
+printf 'PASS: persistent host log readiness isolation\n'