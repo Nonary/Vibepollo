Nonary/vibeshine

Simplify Linux maintenance commands and preserve profiles on upgrade

packaging/linux/copr/Sunshine.spec
@@ -1399,8 +1399,8 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       exit 1
     fi
   else
-    echo "==> ACTION REQUIRED: Vibeshine could not choose the desktop user who owns streaming on this machine." >&2
-    echo "    Run:  sudo /usr/libexec/vibeshine/vibeshine-machine-host configure USER" >&2
+    echo "==> ACTION REQUIRED: Vibeshine could not prepare the machine profile; review the preceding setup or migration error." >&2
+    echo "    Run:  sudo vibeshine configure USER" >&2
     echo "    then: sudo systemctl enable --now vibeshine-session-controller.service" >&2
   fi
 else

src/platform/linux/maintenance_cli.h
@@ -1,0 +1,99 @@
+/**
+ * @file src/platform/linux/maintenance_cli.h
+ * @brief Native-package maintenance commands, dispatched before host startup.
+ */
+#pragma once
+
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <optional>
+#include <string_view>
+#include <vector>
+
+#include <unistd.h>
+
+namespace platf::linux_cli {
+  inline constexpr const char *help =
+    "Linux maintenance (native packages):\n"
+    "  vibeshine paths                    Show settings and program locations\n"
+    "  vibeshine status                   Show machine service status\n"
+    "  sudo vibeshine logs                 Show recent service logs\n"
+    "  sudo vibeshine configure USER       Select the desktop owner and migrate settings\n"
+    "  sudo vibeshine migrate              Prepare existing settings after an upgrade\n"
+    "  sudo vibeshine authorize-commands   Approve commands in the application list\n"
+    "  sudo vibeshine driver install       Install/update the virtual-display driver\n"
+    "  sudo vibeshine driver status        Show virtual-display driver status\n"
+    "  sudo vibeshine reset                Erase settings and pairings; ends active streams\n";
+
+  // A missing optional means this is a normal host invocation. An empty vector
+  // denotes a recognized command with invalid arguments; it must never fall
+  // through to configuration parsing or start another host.
+  inline std::optional<std::vector<const char *>> command(int argc, char **argv) {
+    if (argc < 2) {
+      return std::nullopt;
+    }
+    const std::string_view name {argv[1]};
+    constexpr auto machine = "/usr/libexec/vibeshine/vibeshine-machine-host";
+    if (name == "configure") {
+      if (argc == 3 && argv[2][0] != '\0' && argv[2][0] != '-') {
+        return std::vector<const char *> {machine, "configure", argv[2]};
+      }
+    } else if (name == "migrate" || name == "authorize-commands" || name == "reset") {
+      if (argc == 2) {
+        return std::vector<const char *> {machine, name == "migrate" ? "configure-auto" : argv[1]};
+      }
+    } else if (name == "driver") {
+      if (argc == 3 && (std::string_view {argv[2]} == "install" || std::string_view {argv[2]} == "status")) {
+        return std::vector<const char *> {"/usr/libexec/vibeshine/vibeshine-drm-install", argv[2]};
+      }
+    } else if (name == "status") {
+      if (argc == 2) {
+        return std::vector<const char *> {"/usr/bin/systemctl", "--no-pager", "--full", "status",
+                                        "vibeshine-session-controller.service", "vibeshine-session-exec.socket", "vibeshine.service"};
+      }
+    } else if (name == "logs") {
+      if (argc == 2) {
+        return std::vector<const char *> {"/usr/bin/journalctl", "--no-pager", "-n", "200",
+                                        "-u", "vibeshine-session-controller.service", "-u", "vibeshine-session-exec@.service",
+                                        "-u", "vibeshine.service"};
+      }
+    } else if (name != "paths" && name != "maintenance-help") {
+      return std::nullopt;
+    }
+    return std::vector<const char *> {};
+  }
+
+  inline std::optional<int> dispatch(int argc, char **argv) {
+    auto arguments = command(argc, argv);
+    if (!arguments) {
+      return std::nullopt;
+    }
+    if (argc == 2 && std::string_view {argv[1]} == "paths") {
+      std::puts("Native Linux package locations:\n"
+                "  Settings, credentials, pairings, apps: /var/lib/vibeshine\n"
+                "  Configuration file: /var/lib/vibeshine/vibeshine.conf\n"
+                "  Administrator policy: /etc/vibeshine\n"
+                "  Temporary session data: /run/vibeshine\n"
+                "  Programs: /usr/bin/vibeshine, /usr/libexec/vibeshine\n"
+                "  Assets: /usr/share/vibeshine\n"
+                "  Logs: sudo vibeshine logs\n"
+                "  Legacy user settings (imported once): ~/.config/vibeshine");
+      return 0;
+    }
+    if (argc == 2 && std::string_view {argv[1]} == "maintenance-help") {
+      std::fputs(help, stdout);
+      return 0;
+    }
+    if (arguments->empty()) {
+      std::fputs(help, stderr);
+      return 2;
+    }
+    arguments->push_back(nullptr);
+    // Fixed executables and separate arguments: no shell, PATH search, or
+    // privilege escalation. Administrative helpers enforce their own UID check.
+    execv(arguments->front(), const_cast<char *const *>(arguments->data()));
+    std::fprintf(stderr, "Vibeshine: cannot execute %s: %s\n", arguments->front(), std::strerror(errno));
+    return 1;
+  }
+}  // namespace platf::linux_cli

tests/unit/platform/linux/test_maintenance_cli.cpp
@@ -1,0 +1,59 @@
+#include "../../../../src/platform/linux/maintenance_cli.h"
+
+#include <cstdlib>
+#include <initializer_list>
+#include <string>
+
+static void check(std::initializer_list<const char *> input,
+                  std::initializer_list<const char *> expected, bool recognized = true) {
+  std::vector<char *> argv;
+  for (auto value : input) {
+    argv.push_back(const_cast<char *>(value));
+  }
+  const auto result = platf::linux_cli::command(static_cast<int>(argv.size()), argv.data());
+  if (result.has_value() != recognized || (result && result->size() != expected.size())) {
+    std::abort();
+  }
+  if (result) {
+    auto actual = result->begin();
+    for (auto value : expected) {
+      if (std::string {*actual++} != value) {
+        std::abort();
+      }
+    }
+  }
+}
+
+int main() {
+  constexpr auto helper = "/usr/libexec/vibeshine/vibeshine-machine-host";
+  check({"vibeshine", "configure", "alice"}, {helper, "configure", "alice"});
+  // Arguments remain literal and cannot become shell commands or helper options.
+  check({"vibeshine", "configure", "alice; touch /tmp/unwanted"}, {helper, "configure", "alice; touch /tmp/unwanted"});
+  check({"vibeshine", "configure", "--help"}, {});
+  check({"vibeshine", "configure"}, {});
+  check({"vibeshine", "configure", "alice", "bob"}, {});
+  check({"vibeshine", "migrate"}, {helper, "configure-auto"});
+  check({"vibeshine", "reset"}, {helper, "reset"});
+  check({"vibeshine", "reset", "anything"}, {});
+  check({"vibeshine", "authorize-commands"}, {helper, "authorize-commands"});
+  check({"vibeshine", "driver", "status"}, {"/usr/libexec/vibeshine/vibeshine-drm-install", "status"});
+  check({"vibeshine", "driver", "install"}, {"/usr/libexec/vibeshine/vibeshine-drm-install", "install"});
+  check({"vibeshine", "driver", "arbitrary-operation"}, {});
+  check({"vibeshine", "status"}, {"/usr/bin/systemctl", "--no-pager", "--full", "status",
+                                  "vibeshine-session-controller.service", "vibeshine-session-exec.socket", "vibeshine.service"});
+  check({"vibeshine", "logs"}, {"/usr/bin/journalctl", "--no-pager", "-n", "200",
+                                "-u", "vibeshine-session-controller.service", "-u", "vibeshine-session-exec@.service", "-u", "vibeshine.service"});
+  check({"vibeshine"}, {}, false);
+  check({"vibeshine", "/var/lib/vibeshine/vibeshine.conf"}, {}, false);
+  check({"vibeshine", "--version"}, {}, false);
+  check({"vibeshine", "encoder=nvenc"}, {}, false);
+
+  char name[] = "vibeshine";
+  char paths[] = "paths";
+  char extra[] = "unexpected";
+  char *argv[] = {name, paths, extra};
+  if (platf::linux_cli::dispatch(2, argv) != 0 || platf::linux_cli::dispatch(3, argv) != 2) {
+    return 1;
+  }
+  std::puts("PASS: Linux maintenance dispatch");
+}

tests/unit/platform/linux/test_profile_upgrade.py
@@ -1,0 +1,122 @@
+#!/usr/bin/env python3
+"""Exercise production upgrade control flow with isolated system-operation fakes.
+
+No root access or Linux host mutation is needed. The confined importer and
+ownership syscalls still require separate integration testing on Linux.
+"""
+import pathlib
+import shutil
+import subprocess
+import sys
+import tempfile
+
+root = pathlib.Path(sys.argv[1]).resolve()
+source = (root / "packaging/linux/vibeshine-machine-host").read_text()
+
+
+def function(name):
+    start = source.index(name + "() {\n")
+    end = source.index("\n}\n", start) + 3
+    return source[start:end]
+
+
+with tempfile.TemporaryDirectory() as directory:
+    fixture = pathlib.Path(directory)
+    # Source only the relevant production functions, allowing these control-flow
+    # tests to run on macOS's Bash as well as Linux. System calls are faked;
+    # profile files, markers, and symlinks below are real temporary files.
+    script = "\n".join(function(name) for name in (
+        "prepare_upgrade_profile", "migrate_profile", "configure_automatically",
+        "read_desktop_user",
+    ))
+    script = script.replace("/usr/bin/stat", "fixture_stat").replace("/usr/bin/getent", "fixture_getent")
+    script += r'''
+fail() { printf '%s\n' "$*" >&2; return 1; }
+require_root() { return 0; }
+load_settings() { return 0; }
+fixture_getent() { return 0; }
+fixture_stat() {
+  if [[ "${@: -1}" == /var/lib ]]; then printf 'root:root:755:directory\n'
+  elif [[ "$fail_at" == policy ]]; then printf 'alice:alice:644:regular file\n'
+  else printf 'root:root:600:regular file\n'; fi
+}
+service_user=vibeshine
+machine_host=/usr/libexec/vibeshine/vibeshine-machine-host
+machine_profile=$PWD/machine
+profile_marker=$machine_profile/.machine-profile
+settings_file=$PWD/machine.conf
+legacy_settings_file=$PWD/prelogin.conf
+printf 'desktop_user=alice\n' >"$settings_file"
+write_settings() { [[ "$1" == alice && "$fail_at" != settings ]]; }
+profile_tree_is_sanitized() { [[ "$fail_at" != validate ]]; }
+repair_profile_tree() { printf 'repair\n' >>trace; [[ "$fail_at" != repair ]]; }
+user_exec() {
+  [[ "$1" == vibeshine && "$2" == "$machine_host" && "$3" == prepare-profile ]] || exit 80
+  printf 'prepare\n' >>trace
+  [[ "$fail_at" != prepare ]]
+}
+ensure_command_manifest() { printf 'manifest\n' >>trace; [[ "$fail_at" != manifest ]]; }
+publish_migrated_profile() {
+  printf 'import\n' >>trace
+  [[ "$fail_at" != import ]] || return 1
+  [[ ! -e "$machine_profile" ]] || return 1
+  cp -R legacy "$machine_profile" || return 1
+  printf 'source_user=alice\n' >"$profile_marker"
+}
+assert_trace() { [[ "$(cat trace)" == "$1" ]] || { cat trace; exit 81; }; }
+mkdir legacy
+printf 'paired-identity-and-credentials\n' >legacy/sunshine_state.json
+printf 'user-settings\n' >legacy/vibeshine.conf
+printf 'applications\n' >legacy/apps.json
+fail_at=''
+: >trace
+configure_automatically || exit 82
+assert_trace $'repair\nimport\nprepare\nmanifest'
+diff legacy/sunshine_state.json "$machine_profile/sunshine_state.json" || exit 83
+diff legacy/vibeshine.conf "$machine_profile/vibeshine.conf" || exit 84
+diff legacy/apps.json "$machine_profile/apps.json" || exit 85
+# The old source remains a backup. Newer machine data must win on every rerun.
+printf 'new-pairing\n' >"$machine_profile/sunshine_state.json"
+for pass in 1 2; do
+  : >trace
+  if configure_automatically; then :; else exit 86; fi
+  assert_trace $'repair\nprepare\nmanifest'
+  [[ "$(cat "$machine_profile/sunshine_state.json")" == new-pairing ]] || exit 87
+done
+# Failure checks deliberately call the function in an if, disabling Bash's
+# implicit errexit. Every failed stage must still stop all subsequent stages.
+for fail_at in policy settings repair validate prepare manifest; do
+  : >trace
+  if configure_automatically; then echo "unexpected success: $fail_at"; exit 88; fi
+  case "$fail_at" in
+    policy|settings) assert_trace '' ;;
+    repair|validate) assert_trace repair ;;
+    prepare) assert_trace $'repair\nprepare' ;;
+    manifest) assert_trace $'repair\nprepare\nmanifest' ;;
+  esac
+done
+# Malformed administrator policy must not continue to a profile import/repair.
+fail_at=''
+for policy in 'unknown=value' $'desktop_user=alice\ndesktop_user=bob' '# no selected user'; do
+  printf '%s\n' "$policy" >"$settings_file"
+  : >trace
+  if configure_automatically; then exit 92; fi
+  assert_trace ''
+done
+printf 'desktop_user=alice\n' >"$settings_file"
+rm -rf "$machine_profile"
+fail_at=import
+: >trace
+if configure_automatically; then exit 89; fi
+assert_trace $'repair\nimport'
+[[ ! -e "$profile_marker" && -f legacy/sunshine_state.json ]] || exit 90
+# A symlink marker is never treated as a completed migration or replaced.
+fail_at=''
+mkdir "$machine_profile"
+ln -s "$PWD/legacy/sunshine_state.json" "$profile_marker"
+: >trace
+if migrate_profile; then exit 91; fi
+assert_trace ''
+printf 'PASS: profile upgrade preservation, repeatability, and failure handling\n'
+'''
+    subprocess.run([shutil.which("bash"), "-eu", "-c", script], cwd=fixture, check=True)

Nonary/Vibepollo

(cherry picked from commit 78108720424a17342e8a05c410bfae08e17fcc30)

packaging/linux/copr/Sunshine.spec
@@ -1396,8 +1396,8 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       exit 1
     fi
   else
-    echo "==> ACTION REQUIRED: Vibepollo could not choose the desktop user who owns streaming on this machine." >&2
-    echo "    Run:  sudo /usr/libexec/vibeshine/vibepollo-machine-host configure USER" >&2
+    echo "==> ACTION REQUIRED: Vibepollo could not prepare the machine profile; review the preceding setup or migration error." >&2
+    echo "    Run:  sudo vibepollo configure USER" >&2
     echo "    then: sudo systemctl enable --now vibepollo-session-controller.service" >&2
   fi
 else

src/platform/linux/maintenance_cli.h
@@ -1,0 +1,99 @@
+/**
+ * @file src/platform/linux/maintenance_cli.h
+ * @brief Native-package maintenance commands, dispatched before host startup.
+ */
+#pragma once
+
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <optional>
+#include <string_view>
+#include <vector>
+
+#include <unistd.h>
+
+namespace platf::linux_cli {
+  inline constexpr const char *help =
+    "Linux maintenance (native packages):\n"
+    "  vibepollo paths                    Show settings and program locations\n"
+    "  vibepollo status                   Show machine service status\n"
+    "  sudo vibepollo logs                 Show recent service logs\n"
+    "  sudo vibepollo configure USER       Select the desktop owner and migrate settings\n"
+    "  sudo vibepollo migrate              Prepare existing settings after an upgrade\n"
+    "  sudo vibepollo authorize-commands   Approve commands in the application list\n"
+    "  sudo vibepollo driver install       Install/update the virtual-display driver\n"
+    "  sudo vibepollo driver status        Show virtual-display driver status\n"
+    "  sudo vibepollo reset                Erase settings and pairings; ends active streams\n";
+
+  // A missing optional means this is a normal host invocation. An empty vector
+  // denotes a recognized command with invalid arguments; it must never fall
+  // through to configuration parsing or start another host.
+  inline std::optional<std::vector<const char *>> command(int argc, char **argv) {
+    if (argc < 2) {
+      return std::nullopt;
+    }
+    const std::string_view name {argv[1]};
+    constexpr auto machine = "/usr/libexec/vibeshine/vibepollo-machine-host";
+    if (name == "configure") {
+      if (argc == 3 && argv[2][0] != '\0' && argv[2][0] != '-') {
+        return std::vector<const char *> {machine, "configure", argv[2]};
+      }
+    } else if (name == "migrate" || name == "authorize-commands" || name == "reset") {
+      if (argc == 2) {
+        return std::vector<const char *> {machine, name == "migrate" ? "configure-auto" : argv[1]};
+      }
+    } else if (name == "driver") {
+      if (argc == 3 && (std::string_view {argv[2]} == "install" || std::string_view {argv[2]} == "status")) {
+        return std::vector<const char *> {"/usr/libexec/vibeshine/vibeshine-drm-install", argv[2]};
+      }
+    } else if (name == "status") {
+      if (argc == 2) {
+        return std::vector<const char *> {"/usr/bin/systemctl", "--no-pager", "--full", "status",
+                                        "vibepollo-session-controller.service", "vibepollo-session-exec.socket", "vibepollo.service"};
+      }
+    } else if (name == "logs") {
+      if (argc == 2) {
+        return std::vector<const char *> {"/usr/bin/journalctl", "--no-pager", "-n", "200",
+                                        "-u", "vibepollo-session-controller.service", "-u", "vibepollo-session-exec@.service",
+                                        "-u", "vibepollo.service"};
+      }
+    } else if (name != "paths" && name != "maintenance-help") {
+      return std::nullopt;
+    }
+    return std::vector<const char *> {};
+  }
+
+  inline std::optional<int> dispatch(int argc, char **argv) {
+    auto arguments = command(argc, argv);
+    if (!arguments) {
+      return std::nullopt;
+    }
+    if (argc == 2 && std::string_view {argv[1]} == "paths") {
+      std::puts("Native Linux package locations:\n"
+                "  Settings, credentials, pairings, apps: /var/lib/vibepollo\n"
+                "  Configuration file: /var/lib/vibepollo/vibepollo.conf\n"
+                "  Administrator policy: /etc/vibepollo\n"
+                "  Temporary session data: /run/vibepollo\n"
+                "  Programs: /usr/bin/vibepollo, /usr/libexec/vibeshine\n"
+                "  Assets: /usr/share/vibepollo\n"
+                "  Logs: sudo vibepollo logs\n"
+                "  Legacy user settings (imported once): ~/.config/vibepollo");
+      return 0;
+    }
+    if (argc == 2 && std::string_view {argv[1]} == "maintenance-help") {
+      std::fputs(help, stdout);
+      return 0;
+    }
+    if (arguments->empty()) {
+      std::fputs(help, stderr);
+      return 2;
+    }
+    arguments->push_back(nullptr);
+    // Fixed executables and separate arguments: no shell, PATH search, or
+    // privilege escalation. Administrative helpers enforce their own UID check.
+    execv(arguments->front(), const_cast<char *const *>(arguments->data()));
+    std::fprintf(stderr, "Vibepollo: cannot execute %s: %s\n", arguments->front(), std::strerror(errno));
+    return 1;
+  }
+}  // namespace platf::linux_cli

tests/unit/platform/linux/test_maintenance_cli.cpp
@@ -1,0 +1,59 @@
+#include "../../../../src/platform/linux/maintenance_cli.h"
+
+#include <cstdlib>
+#include <initializer_list>
+#include <string>
+
+static void check(std::initializer_list<const char *> input,
+                  std::initializer_list<const char *> expected, bool recognized = true) {
+  std::vector<char *> argv;
+  for (auto value : input) {
+    argv.push_back(const_cast<char *>(value));
+  }
+  const auto result = platf::linux_cli::command(static_cast<int>(argv.size()), argv.data());
+  if (result.has_value() != recognized || (result && result->size() != expected.size())) {
+    std::abort();
+  }
+  if (result) {
+    auto actual = result->begin();
+    for (auto value : expected) {
+      if (std::string {*actual++} != value) {
+        std::abort();
+      }
+    }
+  }
+}
+
+int main() {
+  constexpr auto helper = "/usr/libexec/vibeshine/vibepollo-machine-host";
+  check({"vibepollo", "configure", "alice"}, {helper, "configure", "alice"});
+  // Arguments remain literal and cannot become shell commands or helper options.
+  check({"vibepollo", "configure", "alice; touch /tmp/unwanted"}, {helper, "configure", "alice; touch /tmp/unwanted"});
+  check({"vibepollo", "configure", "--help"}, {});
+  check({"vibepollo", "configure"}, {});
+  check({"vibepollo", "configure", "alice", "bob"}, {});
+  check({"vibepollo", "migrate"}, {helper, "configure-auto"});
+  check({"vibepollo", "reset"}, {helper, "reset"});
+  check({"vibepollo", "reset", "anything"}, {});
+  check({"vibepollo", "authorize-commands"}, {helper, "authorize-commands"});
+  check({"vibepollo", "driver", "status"}, {"/usr/libexec/vibeshine/vibeshine-drm-install", "status"});
+  check({"vibepollo", "driver", "install"}, {"/usr/libexec/vibeshine/vibeshine-drm-install", "install"});
+  check({"vibepollo", "driver", "arbitrary-operation"}, {});
+  check({"vibepollo", "status"}, {"/usr/bin/systemctl", "--no-pager", "--full", "status",
+                                  "vibepollo-session-controller.service", "vibepollo-session-exec.socket", "vibepollo.service"});
+  check({"vibepollo", "logs"}, {"/usr/bin/journalctl", "--no-pager", "-n", "200",
+                                "-u", "vibepollo-session-controller.service", "-u", "vibepollo-session-exec@.service", "-u", "vibepollo.service"});
+  check({"vibepollo"}, {}, false);
+  check({"vibepollo", "/var/lib/vibepollo/vibepollo.conf"}, {}, false);
+  check({"vibepollo", "--version"}, {}, false);
+  check({"vibepollo", "encoder=nvenc"}, {}, false);
+
+  char name[] = "vibepollo";
+  char paths[] = "paths";
+  char extra[] = "unexpected";
+  char *argv[] = {name, paths, extra};
+  if (platf::linux_cli::dispatch(2, argv) != 0 || platf::linux_cli::dispatch(3, argv) != 2) {
+    return 1;
+  }
+  std::puts("PASS: Linux maintenance dispatch");
+}

tests/unit/platform/linux/test_profile_upgrade.py
@@ -1,0 +1,122 @@
+#!/usr/bin/env python3
+"""Exercise production upgrade control flow with isolated system-operation fakes.
+
+No root access or Linux host mutation is needed. The confined importer and
+ownership syscalls still require separate integration testing on Linux.
+"""
+import pathlib
+import shutil
+import subprocess
+import sys
+import tempfile
+
+root = pathlib.Path(sys.argv[1]).resolve()
+source = (root / "packaging/linux/vibepollo-machine-host").read_text()
+
+
+def function(name):
+    start = source.index(name + "() {\n")
+    end = source.index("\n}\n", start) + 3
+    return source[start:end]
+
+
+with tempfile.TemporaryDirectory() as directory:
+    fixture = pathlib.Path(directory)
+    # Source only the relevant production functions, allowing these control-flow
+    # tests to run on macOS's Bash as well as Linux. System calls are faked;
+    # profile files, markers, and symlinks below are real temporary files.
+    script = "\n".join(function(name) for name in (
+        "prepare_upgrade_profile", "migrate_profile", "configure_automatically",
+        "read_desktop_user",
+    ))
+    script = script.replace("/usr/bin/stat", "fixture_stat").replace("/usr/bin/getent", "fixture_getent")
+    script += r'''
+fail() { printf '%s\n' "$*" >&2; return 1; }
+require_root() { return 0; }
+load_settings() { return 0; }
+fixture_getent() { return 0; }
+fixture_stat() {
+  if [[ "${@: -1}" == /var/lib ]]; then printf 'root:root:755:directory\n'
+  elif [[ "$fail_at" == policy ]]; then printf 'alice:alice:644:regular file\n'
+  else printf 'root:root:600:regular file\n'; fi
+}
+service_user=vibepollo
+machine_host=/usr/libexec/vibeshine/vibepollo-machine-host
+machine_profile=$PWD/machine
+profile_marker=$machine_profile/.machine-profile
+settings_file=$PWD/machine.conf
+legacy_settings_file=$PWD/prelogin.conf
+printf 'desktop_user=alice\n' >"$settings_file"
+write_settings() { [[ "$1" == alice && "$fail_at" != settings ]]; }
+profile_tree_is_sanitized() { [[ "$fail_at" != validate ]]; }
+repair_profile_tree() { printf 'repair\n' >>trace; [[ "$fail_at" != repair ]]; }
+user_exec() {
+  [[ "$1" == vibepollo && "$2" == "$machine_host" && "$3" == prepare-profile ]] || exit 80
+  printf 'prepare\n' >>trace
+  [[ "$fail_at" != prepare ]]
+}
+ensure_command_manifest() { printf 'manifest\n' >>trace; [[ "$fail_at" != manifest ]]; }
+publish_migrated_profile() {
+  printf 'import\n' >>trace
+  [[ "$fail_at" != import ]] || return 1
+  [[ ! -e "$machine_profile" ]] || return 1
+  cp -R legacy "$machine_profile" || return 1
+  printf 'source_user=alice\n' >"$profile_marker"
+}
+assert_trace() { [[ "$(cat trace)" == "$1" ]] || { cat trace; exit 81; }; }
+mkdir legacy
+printf 'paired-identity-and-credentials\n' >legacy/sunshine_state.json
+printf 'user-settings\n' >legacy/vibepollo.conf
+printf 'applications\n' >legacy/apps.json
+fail_at=''
+: >trace
+configure_automatically || exit 82
+assert_trace $'repair\nimport\nprepare\nmanifest'
+diff legacy/sunshine_state.json "$machine_profile/sunshine_state.json" || exit 83
+diff legacy/vibepollo.conf "$machine_profile/vibepollo.conf" || exit 84
+diff legacy/apps.json "$machine_profile/apps.json" || exit 85
+# The old source remains a backup. Newer machine data must win on every rerun.
+printf 'new-pairing\n' >"$machine_profile/sunshine_state.json"
+for pass in 1 2; do
+  : >trace
+  if configure_automatically; then :; else exit 86; fi
+  assert_trace $'repair\nprepare\nmanifest'
+  [[ "$(cat "$machine_profile/sunshine_state.json")" == new-pairing ]] || exit 87
+done
+# Failure checks deliberately call the function in an if, disabling Bash's
+# implicit errexit. Every failed stage must still stop all subsequent stages.
+for fail_at in policy settings repair validate prepare manifest; do
+  : >trace
+  if configure_automatically; then echo "unexpected success: $fail_at"; exit 88; fi
+  case "$fail_at" in
+    policy|settings) assert_trace '' ;;
+    repair|validate) assert_trace repair ;;
+    prepare) assert_trace $'repair\nprepare' ;;
+    manifest) assert_trace $'repair\nprepare\nmanifest' ;;
+  esac
+done
+# Malformed administrator policy must not continue to a profile import/repair.
+fail_at=''
+for policy in 'unknown=value' $'desktop_user=alice\ndesktop_user=bob' '# no selected user'; do
+  printf '%s\n' "$policy" >"$settings_file"
+  : >trace
+  if configure_automatically; then exit 92; fi
+  assert_trace ''
+done
+printf 'desktop_user=alice\n' >"$settings_file"
+rm -rf "$machine_profile"
+fail_at=import
+: >trace
+if configure_automatically; then exit 89; fi
+assert_trace $'repair\nimport'
+[[ ! -e "$profile_marker" && -f legacy/sunshine_state.json ]] || exit 90
+# A symlink marker is never treated as a completed migration or replaced.
+fail_at=''
+mkdir "$machine_profile"
+ln -s "$PWD/legacy/sunshine_state.json" "$profile_marker"
+: >trace
+if migrate_profile; then exit 91; fi
+assert_trace ''
+printf 'PASS: profile upgrade preservation, repeatability, and failure handling\n'
+'''
+    subprocess.run([shutil.which("bash"), "-eu", "-c", script], cwd=fixture, check=True)