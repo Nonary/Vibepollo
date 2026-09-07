Nonary/vibeshine

fix(linux): make host identity machine-wide
Replace copied user and greeter profiles with one machine-owned state directory so pairing, PINs, credentials, and settings survive login handoff. Run the network host as a dedicated non-login account, isolate root-only preparation in a oneshot unit, and route session work through a capability-dropping helper.

Prevent inconsistent KScreen restore state from aborting stream teardown, proxy Pulse control and capture through the active session without widening filesystem access, and prepare capability-free vendor KWin before loading the root-owned NVIDIA bridge. Restore KWin’s original optional capability during shutdown or uninstall.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol High

cmake/packaging/linux.cmake
@@ -37,6 +37,11 @@ else()
                 OUTPUT_NAME pam_vibeshine_session)
         target_include_directories(pam_vibeshine_session PRIVATE "${VIBESHINE_PAM_INCLUDE_DIR}")
         target_link_libraries(pam_vibeshine_session PRIVATE "${VIBESHINE_PAM_LIBRARY}")
+        add_executable(vibeshine_session_exec
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-exec.c")
+        set_target_properties(vibeshine_session_exec PROPERTIES OUTPUT_NAME "vibeshine-session-exec")
+        target_include_directories(vibeshine_session_exec PRIVATE "${LIBCAP_INCLUDE_DIRS}")
+        target_link_libraries(vibeshine_session_exec PRIVATE "${LIBCAP_LIBRARIES}")
         file(GENERATE
                 OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
                 CONTENT "#define VIBESHINE_DRM_VERSION \"${PROJECT_VERSION_NUMERIC}\"\n")
@@ -44,11 +49,12 @@ else()
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin-sync"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-machine-host"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-handoff"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-ready"
                 "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-install"
                 DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
+        install(TARGETS vibeshine_session_exec
+                RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS vibeshine_vkms_peercred
                 RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS pam_vibeshine_session
@@ -56,9 +62,10 @@ else()
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
-        install(FILES
-                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/vibeshine.conf"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine.sysusers"
+                DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
+                RENAME vibeshine.conf)
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
                 DESTINATION "${SUNSHINE_ASSETS_DIR}/prelogin")
         install(DIRECTORY "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}"
@@ -84,8 +91,10 @@ else()
                 DESTINATION "${UDEV_RULES_INSTALL_DIR}")
     endif()
     if(SYSTEMD_FOUND)
-        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/app-${PROJECT_FQDN}.service"
-                DESTINATION "${SYSTEMD_USER_UNIT_INSTALL_DIR}")
+        if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            install(FILES "${CMAKE_CURRENT_BINARY_DIR}/app-${PROJECT_FQDN}.service"
+                    DESTINATION "${SYSTEMD_USER_UNIT_INSTALL_DIR}")
+        endif()
         install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.conf"
                 DESTINATION "${SYSTEMD_MODULES_LOAD_DIR}")
         if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
@@ -94,7 +103,8 @@ else()
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-setup.service"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control.socket"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control@.service"
-                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-machine-prepare.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
@@ -139,13 +149,15 @@ endif()
 
 # Apply setcap for RPM
 # https://github.com/coreos/rpm-ostree/discussions/5036#discussioncomment-10291071
-set(CPACK_RPM_USER_FILELIST "%caps(cap_sys_admin,cap_sys_nice+p) ${SUNSHINE_EXECUTABLE_PATH}")
+set(CPACK_RPM_USER_FILELIST
+        "%caps(cap_sys_admin,cap_sys_nice+p) ${SUNSHINE_EXECUTABLE_PATH};%attr(0750,root,vibeshine) %caps(cap_setgid,cap_setuid+ep) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-session-exec")
 
 # Dependencies
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             ${CPACK_DEB_PLATFORM_PACKAGE_DEPENDS} \
             debianutils, \
+            acl, \
             libcap2, \
             libcurl4, \
             libdrm2, \
@@ -171,6 +183,7 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
 set(CPACK_RPM_PACKAGE_REQUIRES "\
             ${CPACK_RPM_PLATFORM_PACKAGE_REQUIRES} \
             libcap >= 2.22, \
+            acl, \
             libcurl >= 7.0, \
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \

packaging/linux/copr/Sunshine.spec
@@ -138,6 +138,7 @@ BuildRequires: gcc14-c++
 
 # Common runtime requirements
 Requires: miniupnpc >= 2.2.4
+Requires: acl
 Requires: which >= 2.21
 Requires: kmod
 Requires: iproute
@@ -352,6 +353,9 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
     echo "error: udevadm not found or not executable."
   fi
 
+  %{_prefix}/libexec/vibeshine/vibeshine-kwin-capability prepare || \
+    echo "warning: could not prepare capability-free KWin for the Vibeshine GPU bridge."
+
   if %{_prefix}/libexec/vibeshine/vibeshine-drm-install install; then
     :
   else
@@ -362,16 +366,16 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       echo "warning: Vibeshine DRM installation failed; managed virtual displays are unavailable."
     fi
   fi
-  if %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync configure-auto; then
-    if %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync install-pam; then
+  if %{_prefix}/libexec/vibeshine/vibeshine-machine-host configure-auto; then
+    if %{_prefix}/libexec/vibeshine/vibeshine-machine-host install-pam; then
       systemctl daemon-reload || true
-      systemctl enable vibeshine-prelogin.service || \
-        echo "warning: could not enable Vibeshine pre-login streaming."
+      systemctl disable --now vibeshine-prelogin.service 2>/dev/null || true
+      systemctl enable vibeshine.service || echo "warning: could not enable the Vibeshine machine host."
     else
       echo "warning: could not install the Plasma Login Manager handoff hook."
     fi
   else
-    echo "warning: configure a paired-client allowlist before enabling Vibeshine pre-login streaming."
+    echo "warning: configure a machine profile owner before enabling Vibeshine."
   fi
 else
   echo "rpm-ostree environment detected, skipping post install steps. Restart to apply the changes."
@@ -379,9 +383,11 @@ fi
 
 %preun
 if [ "$1" -eq 0 ]; then
-  systemctl disable --now vibeshine-prelogin.service 2>/dev/null || true
-  %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync remove-pam || true
+  systemctl disable --now vibeshine.service vibeshine-prelogin.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibeshine-machine-host remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
+  systemctl stop vibeshine-drm-setup.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibeshine-kwin-capability restore || true
   %{_prefix}/libexec/vibeshine/vibeshine-drm-install remove || \
     echo "warning: could not remove the Vibeshine HDR DRM module cleanly."
 fi
@@ -394,14 +400,15 @@ fi
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-quiesce
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-peercred
 %{_prefix}/libexec/vibeshine/vibeshine-session-handoff
-%{_prefix}/libexec/vibeshine/vibeshine-session-ready
-%{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync
-%{_prefix}/libexec/vibeshine/kwin-preload/kwin_wayland
+%attr(0750,root,vibeshine) %caps(cap_setgid,cap_setuid=ep) %{_prefix}/libexec/vibeshine/vibeshine-session-exec
+%{_prefix}/libexec/vibeshine/vibeshine-machine-host
+%{_prefix}/libexec/vibeshine/vibeshine-kwin-capability
 %{_prefix}/lib/vibeshine/libvibeshine-kwin-gpu.so
 %{_libdir}/security/pam_vibeshine_session.so
 
 # Dedicated access group for the privileged virtual-display control socket
 %{_prefix}/lib/sysusers.d/vibeshine-vkms.conf
+%{_prefix}/lib/sysusers.d/vibeshine.conf
 
 # Versioned DKMS/direct-build source tree
 /usr/src/vibeshine-drm-*
@@ -416,7 +423,8 @@ fi
 %{_unitdir}/vibeshine-vkms-control.socket
 %{_unitdir}/vibeshine-vkms-control@.service
 %{_unitdir}/vibeshine-vkms.service
-%{_unitdir}/vibeshine-prelogin.service
+%{_unitdir}/vibeshine-machine-prepare.service
+%{_unitdir}/vibeshine.service
 %{_unitdir}/vibeshine-session-restore@.service
 
 # Udev rules

src/lutris_integration.cpp
@@ -239,7 +239,11 @@ namespace platf::lutris {
         _exit(127);
       }
       setsid();
-      execl(executable.c_str(), executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      if (std::getenv("VIBESHINE_MACHINE_HOST")) {
+        execl("/usr/libexec/vibeshine/vibeshine-session-exec", "vibeshine-session-exec", executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      } else {
+        execl(executable.c_str(), executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      }
       const int error = errno;
       const auto ignored = write(error_pipe[1], &error, sizeof(error));
       (void) ignored;

src/platform/linux/misc.cpp
@@ -336,6 +336,11 @@ namespace platf {
       args.assign(parts.begin() + 1, parts.end());
     }
 
+    if (std::getenv("VIBESHINE_MACHINE_HOST")) {
+      args.insert(args.begin(), exe_path.string());
+      exe_path = v2::filesystem::path("/usr/libexec/vibeshine/vibeshine-session-exec");
+    }
+
     v2::process_stdio stdio {};
     stdio.in = nullptr;
     if (file) {

src/steam_integration.cpp
@@ -1115,7 +1115,11 @@ namespace platf::steam {
       execlp("open", "open", uri.c_str(), static_cast<char *>(nullptr));
   #else
       const auto app_id_string = std::to_string(app_id);
-      execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      if (std::getenv("VIBESHINE_MACHINE_HOST")) {
+        execl("/usr/libexec/vibeshine/vibeshine-session-exec", "vibeshine-session-exec", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      } else {
+        execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      }
   #endif
       const int error = errno;
       const auto ignored = write(exec_error_pipe[1], &error, sizeof(error));

Nonary/Vibepollo

(cherry picked from commit 6be2667e416f0b4af7b8b86d271f4eae55ec2d54)

cmake/packaging/linux.cmake
@@ -37,6 +37,11 @@ else()
                 OUTPUT_NAME pam_vibepollo_session)
         target_include_directories(pam_vibepollo_session PRIVATE "${VIBESHINE_PAM_INCLUDE_DIR}")
         target_link_libraries(pam_vibepollo_session PRIVATE "${VIBESHINE_PAM_LIBRARY}")
+        add_executable(vibepollo_session_exec
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-exec.c")
+        set_target_properties(vibepollo_session_exec PROPERTIES OUTPUT_NAME "vibepollo-session-exec")
+        target_include_directories(vibepollo_session_exec PRIVATE "${LIBCAP_INCLUDE_DIRS}")
+        target_link_libraries(vibepollo_session_exec PRIVATE "${LIBCAP_LIBRARIES}")
         file(GENERATE
                 OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/vibeshine_drm_version.h"
                 CONTENT "#define VIBESHINE_DRM_VERSION \"${PROJECT_VERSION_NUMERIC}\"\n")
@@ -44,11 +49,12 @@ else()
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin-sync"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-machine-host"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-handoff"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-ready"
                 "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-install"
                 DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
+        install(TARGETS vibepollo_session_exec
+                RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS vibeshine_vkms_peercred
                 RUNTIME DESTINATION "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}")
         install(TARGETS pam_vibepollo_session
@@ -56,9 +62,10 @@ else()
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
-        install(FILES
-                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/vibepollo.conf"
-                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo.sysusers"
+                DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
+                RENAME vibepollo.conf)
+        install(FILES "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
                 DESTINATION "${SUNSHINE_ASSETS_DIR}/prelogin")
         install(DIRECTORY "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}"
@@ -84,8 +91,10 @@ else()
                 DESTINATION "${UDEV_RULES_INSTALL_DIR}")
     endif()
     if(SYSTEMD_FOUND)
-        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/app-${PROJECT_FQDN}.service"
-                DESTINATION "${SYSTEMD_USER_UNIT_INSTALL_DIR}")
+        if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            install(FILES "${CMAKE_CURRENT_BINARY_DIR}/app-${PROJECT_FQDN}.service"
+                    DESTINATION "${SYSTEMD_USER_UNIT_INSTALL_DIR}")
+        endif()
         install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.conf"
                 DESTINATION "${SYSTEMD_MODULES_LOAD_DIR}")
         if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
@@ -94,7 +103,8 @@ else()
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-setup.service"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control.socket"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control@.service"
-                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-machine-prepare.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
@@ -145,13 +155,15 @@ endif()
 
 # Apply setcap for RPM
 # https://github.com/coreos/rpm-ostree/discussions/5036#discussioncomment-10291071
-set(CPACK_RPM_USER_FILELIST "%caps(cap_sys_admin,cap_sys_nice+p) ${SUNSHINE_EXECUTABLE_PATH}")
+set(CPACK_RPM_USER_FILELIST
+        "%caps(cap_sys_admin,cap_sys_nice+p) ${SUNSHINE_EXECUTABLE_PATH};%attr(0750,root,vibepollo) %caps(cap_setgid,cap_setuid+ep) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-session-exec")
 
 # Dependencies
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             ${CPACK_DEB_PLATFORM_PACKAGE_DEPENDS} \
             debianutils, \
+            acl, \
             libcap2, \
             libcurl4, \
             libdrm2, \
@@ -177,6 +189,7 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
 set(CPACK_RPM_PACKAGE_REQUIRES "\
             ${CPACK_RPM_PLATFORM_PACKAGE_REQUIRES} \
             libcap >= 2.22, \
+            acl, \
             libcurl >= 7.0, \
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \

packaging/linux/copr/Sunshine.spec
@@ -139,6 +139,7 @@ BuildRequires: gcc14-c++
 
 # Common runtime requirements
 Requires: miniupnpc >= 2.2.4
+Requires: acl
 Requires: which >= 2.21
 Requires: kmod
 Requires: iproute
@@ -353,6 +354,9 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
     echo "error: udevadm not found or not executable."
   fi
 
+  %{_prefix}/libexec/vibeshine/vibeshine-kwin-capability prepare || \
+    echo "warning: could not prepare capability-free KWin for the Vibepollo GPU bridge."
+
   if %{_prefix}/libexec/vibeshine/vibeshine-drm-install install; then
     :
   else
@@ -363,16 +367,16 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       echo "warning: Vibepollo DRM installation failed; managed virtual displays are unavailable."
     fi
   fi
-  if %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync configure-auto; then
-    if %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync install-pam; then
+  if %{_prefix}/libexec/vibeshine/vibepollo-machine-host configure-auto; then
+    if %{_prefix}/libexec/vibeshine/vibepollo-machine-host install-pam; then
       systemctl daemon-reload || true
-      systemctl enable vibepollo-prelogin.service || \
-        echo "warning: could not enable Vibepollo pre-login streaming."
+      systemctl disable --now vibepollo-prelogin.service 2>/dev/null || true
+      systemctl enable vibepollo.service || echo "warning: could not enable the Vibepollo machine host."
     else
       echo "warning: could not install the Plasma Login Manager handoff hook."
     fi
   else
-    echo "warning: configure a paired-client allowlist before enabling Vibepollo pre-login streaming."
+    echo "warning: configure a machine profile owner before enabling Vibepollo."
   fi
 else
   echo "rpm-ostree environment detected, skipping post install steps. Restart to apply the changes."
@@ -380,9 +384,11 @@ fi
 
 %preun
 if [ "$1" -eq 0 ]; then
-  systemctl disable --now vibepollo-prelogin.service 2>/dev/null || true
-  %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync remove-pam || true
+  systemctl disable --now vibepollo.service vibepollo-prelogin.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibepollo-machine-host remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
+  systemctl stop vibeshine-drm-setup.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibeshine-kwin-capability restore || true
   %{_prefix}/libexec/vibeshine/vibeshine-drm-install remove || \
     echo "warning: could not remove the Vibepollo HDR DRM module cleanly."
 fi
@@ -396,14 +402,15 @@ fi
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-quiesce
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-peercred
 %{_prefix}/libexec/vibeshine/vibepollo-session-handoff
-%{_prefix}/libexec/vibeshine/vibepollo-session-ready
-%{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync
-%{_prefix}/libexec/vibeshine/kwin-preload/kwin_wayland
+%attr(0750,root,vibepollo) %caps(cap_setgid,cap_setuid=ep) %{_prefix}/libexec/vibeshine/vibepollo-session-exec
+%{_prefix}/libexec/vibeshine/vibepollo-machine-host
+%{_prefix}/libexec/vibeshine/vibeshine-kwin-capability
 %{_prefix}/lib/vibeshine/libvibeshine-kwin-gpu.so
 %{_libdir}/security/pam_vibepollo_session.so
 
 # Dedicated access group for the privileged virtual-display control socket
 %{_prefix}/lib/sysusers.d/vibeshine-vkms.conf
+%{_prefix}/lib/sysusers.d/vibepollo.conf
 
 # Versioned DKMS/direct-build source tree
 /usr/src/vibeshine-drm-*
@@ -418,7 +425,8 @@ fi
 %{_unitdir}/vibeshine-vkms-control.socket
 %{_unitdir}/vibeshine-vkms-control@.service
 %{_unitdir}/vibeshine-vkms.service
-%{_unitdir}/vibepollo-prelogin.service
+%{_unitdir}/vibepollo-machine-prepare.service
+%{_unitdir}/vibepollo.service
 %{_unitdir}/vibepollo-session-restore@.service
 
 # Udev rules

src/lutris_integration.cpp
@@ -239,7 +239,11 @@ namespace platf::lutris {
         _exit(127);
       }
       setsid();
-      execl(executable.c_str(), executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
+        execl("/usr/libexec/vibeshine/vibepollo-session-exec", "vibepollo-session-exec", executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      } else {
+        execl(executable.c_str(), executable.c_str(), uri.c_str(), static_cast<char *>(nullptr));
+      }
       const int error = errno;
       const auto ignored = write(error_pipe[1], &error, sizeof(error));
       (void) ignored;

src/platform/linux/misc.cpp
@@ -427,6 +427,11 @@ namespace platf {
       args.assign(parts.begin() + 1, parts.end());
     }
 
+    if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
+      args.insert(args.begin(), exe_path.string());
+      exe_path = v2::filesystem::path("/usr/libexec/vibeshine/vibepollo-session-exec");
+    }
+
     v2::process_stdio stdio {};
     stdio.in = nullptr;
     if (file) {

src/steam_integration.cpp
@@ -1115,7 +1115,11 @@ namespace platf::steam {
       execlp("open", "open", uri.c_str(), static_cast<char *>(nullptr));
   #else
       const auto app_id_string = std::to_string(app_id);
-      execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
+        execl("/usr/libexec/vibeshine/vibepollo-session-exec", "vibepollo-session-exec", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      } else {
+        execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+      }
   #endif
       const int error = errno;
       const auto ignored = write(exec_error_pipe[1], &error, sizeof(error));