Nonary/vibeshine

fix(linux): enforce MangoHud limits for Steam launches
Steam can evaluate per-game launch options after Vibeshine's launcher environment, allowing user settings or an existing client to replace the stream cap. Add a last-mile wrapper with owner-checked per-app state so Vibeshine's limit is parsed last while preserving unrelated HUD settings.

Avoid MangoHud 0.8.4's integer compatibility-variable path for fractional caps, preserving values such as 59.94 exactly.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol Medium

cmake/packaging/linux.cmake
@@ -11,6 +11,9 @@ file(COPY "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/assets/"
 file(CREATE_LINK "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/assets/shaders"
         "${CMAKE_BINARY_DIR}/assets/shaders" COPY_ON_ERROR SYMBOLIC)
 
+install(PROGRAMS "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/vibeshine-mangohud"
+        DESTINATION "${CMAKE_INSTALL_BINDIR}")
+
 if(${SUNSHINE_BUILD_APPIMAGE} OR ${SUNSHINE_BUILD_FLATPAK})
     install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.rules"
             DESTINATION "${SUNSHINE_ASSETS_DIR}/udev/rules.d")

src/platform/linux/mangohud_policy.h
@@ -121,6 +121,38 @@ namespace platf::mangohud {
     return config;
   }
 
+  inline std::string config_override(
+    std::string_view inherited_config,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph
+  ) {
+    std::string config = "read_cfg";
+    if (!inherited_config.empty()) {
+      config.push_back(',');
+      config += inherited_config;
+    }
+
+    const auto managed = config_override(limit, preset, always_show_graph);
+    // The first token in the managed fragment is already supplied above.
+    config.append(managed, std::string_view("read_cfg").size());
+    return config;
+  }
+
+  inline std::string fps_limit_environment_override(
+    std::uint32_t limit_millihz,
+    std::string_view formatted_limit
+  ) {
+    // MangoHud 0.8.4 (and current upstream as of 2026-08) parses this variable
+    // through an int. Let MANGOHUD_CONFIG carry fractional limits exactly so
+    // the compatibility variable cannot silently turn 59.94 into 59.
+    return limit_millihz % 1000 == 0 ? std::string(formatted_limit) : std::string {};
+  }
+
+  inline std::string steam_launch_option(std::string_view app_id) {
+    return "vibeshine-mangohud --appid " + std::string(app_id) + " -- %command%";
+  }
+
   inline launch_policy_t make_launch_policy(
     std::string_view provider,
     bool limiter_enabled,

src/platform/linux/mangohud_state.h
@@ -1,0 +1,145 @@
+/**
+ * @file src/platform/linux/mangohud_state.h
+ * @brief Runtime handoff used by the Steam last-mile MangoHud wrapper.
+ */
+#pragma once
+
+#include <chrono>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <system_error>
+
+#include <unistd.h>
+
+namespace platf::mangohud {
+
+  inline std::filesystem::path state_directory() {
+    if (const char *override_dir = std::getenv("VIBESHINE_MANGOHUD_STATE_DIR");
+        override_dir && *override_dir) {
+      return override_dir;
+    }
+    if (const char *runtime_dir = std::getenv("XDG_RUNTIME_DIR"); runtime_dir && *runtime_dir) {
+      return std::filesystem::path(runtime_dir) / "vibeshine" / "mangohud";
+    }
+    if (const char *config_home = std::getenv("XDG_CONFIG_HOME"); config_home && *config_home) {
+      return std::filesystem::path(config_home) / "sunshine" / "mangohud-runtime";
+    }
+    if (const char *home = std::getenv("HOME"); home && *home) {
+      return std::filesystem::path(home) / ".config" / "sunshine" / "mangohud-runtime";
+    }
+    return {};
+  }
+
+  inline bool valid_steam_app_id(std::string_view app_id) {
+    if (app_id.empty()) {
+      return false;
+    }
+    for (const char ch : app_id) {
+      if (ch < '0' || ch > '9') {
+        return false;
+      }
+    }
+    return app_id != "0";
+  }
+
+  inline std::filesystem::path state_path(std::string_view app_id) {
+    const auto directory = state_directory();
+    if (directory.empty() || !valid_steam_app_id(app_id)) {
+      return {};
+    }
+    return directory / (std::string(app_id) + ".state");
+  }
+
+  inline std::string serialize_state(
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph,
+    std::chrono::system_clock::time_point expires_at
+  ) {
+    const bool standard_preset = preset == "1" || preset == "2" || preset == "3" || preset == "4";
+    const auto expires = std::chrono::duration_cast<std::chrono::seconds>(
+      expires_at.time_since_epoch()
+    ).count();
+    return "version=1\nlimit=" + std::string(limit) +
+           "\npreset=" + (standard_preset ? std::string(preset) : "custom") +
+           "\nalways_show_graph=" + (always_show_graph ? "1" : "0") +
+           "\nowner_pid=" + std::to_string(static_cast<unsigned long>(getpid())) +
+           "\nexpires=" + std::to_string(expires) + "\n";
+  }
+
+  inline std::filesystem::path write_state(
+    std::string_view app_id,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph
+  ) {
+    const auto path = state_path(app_id);
+    if (path.empty()) {
+      return {};
+    }
+
+    std::error_code ec;
+    std::filesystem::create_directories(path.parent_path(), ec);
+    if (ec) {
+      return {};
+    }
+    std::filesystem::permissions(
+      path.parent_path(),
+      std::filesystem::perms::owner_all,
+      std::filesystem::perm_options::replace,
+      ec
+    );
+    if (ec) {
+      return {};
+    }
+
+    auto temporary = path;
+    temporary += ".tmp-" + std::to_string(static_cast<unsigned long>(getpid()));
+    {
+      std::ofstream output(temporary, std::ios::binary | std::ios::trunc);
+      if (!output) {
+        return {};
+      }
+      output << serialize_state(
+        limit,
+        preset,
+        always_show_graph,
+        std::chrono::system_clock::now() + std::chrono::hours(1)
+      );
+      if (!output) {
+        output.close();
+        std::filesystem::remove(temporary, ec);
+        return {};
+      }
+    }
+    std::filesystem::permissions(
+      temporary,
+      std::filesystem::perms::owner_read | std::filesystem::perms::owner_write,
+      std::filesystem::perm_options::replace,
+      ec
+    );
+    if (ec) {
+      std::filesystem::remove(temporary, ec);
+      return {};
+    }
+    std::filesystem::rename(temporary, path, ec);
+    if (ec) {
+      std::filesystem::remove(temporary, ec);
+      return {};
+    }
+    return path;
+  }
+
+  inline void remove_state(std::string_view app_id) {
+    const auto path = state_path(app_id);
+    if (path.empty()) {
+      return;
+    }
+    std::error_code ec;
+    std::filesystem::remove(path, ec);
+  }
+
+}  // namespace platf::mangohud

tests/unit/platform/linux/test_mangohud_policy.cpp
@@ -5,6 +5,9 @@
 #include "../../../tests_common.h"
 
 #include <src/platform/linux/mangohud_policy.h>
+#include <src/platform/linux/mangohud_state.h>
+
+#include <chrono>
 
 namespace mangohud = platf::mangohud;
 
@@ -62,6 +65,35 @@ TEST(MangoHudPolicy, BuildsSteamCompatibleAuthoritativeLimitConfig) {
     mangohud::config_override("120", "4", true),
     "read_cfg,preset=4,no_display=0,frame_timing=1,fps_limit=120"
   );
+  EXPECT_EQ(
+    mangohud::config_override("position=top-right,fps_limit=30", "120", "custom", false),
+    "read_cfg,position=top-right,fps_limit=30,fps_limit=120"
+  );
+}
+
+TEST(MangoHudPolicy, AvoidsMangoHudFractionalEnvironmentTruncation) {
+  EXPECT_EQ(mangohud::fps_limit_environment_override(120000, "120"), "120");
+  EXPECT_TRUE(mangohud::fps_limit_environment_override(59940, "59.94").empty());
+  EXPECT_EQ(
+    mangohud::steam_launch_option("480"),
+    "vibeshine-mangohud --appid 480 -- %command%"
+  );
+}
+
+TEST(MangoHudPolicy, ValidatesAndSerializesLastMileSteamState) {
+  EXPECT_TRUE(mangohud::valid_steam_app_id("480"));
+  EXPECT_FALSE(mangohud::valid_steam_app_id("0"));
+  EXPECT_FALSE(mangohud::valid_steam_app_id("48/0"));
+
+  const auto state = mangohud::serialize_state(
+    "59.94",
+    "3",
+    true,
+    std::chrono::system_clock::time_point {std::chrono::seconds {12345}}
+  );
+  EXPECT_NE(state.find("version=1\nlimit=59.94\npreset=3\nalways_show_graph=1\n"), std::string::npos);
+  EXPECT_NE(state.find("owner_pid="), std::string::npos);
+  EXPECT_TRUE(state.ends_with("expires=12345\n"));
 }
 
 TEST(MangoHudPolicy, AutomaticVirtualLimiterDoesNotEnablePhysicalStreams) {

tests/unit/platform/linux/test_mangohud_wrapper.sh
@@ -1,0 +1,47 @@
+#!/bin/sh
+set -eu
+
+wrapper=$1
+test_root=$(mktemp -d)
+trap 'rm -rf -- "$test_root"' EXIT HUP INT TERM
+
+write_state() {
+  limit=$1
+  expires=$(($(date +%s) + 60))
+  printf '%s\n' \
+    'version=1' \
+    "limit=$limit" \
+    'preset=custom' \
+    'always_show_graph=0' \
+    "owner_pid=$$" \
+    "expires=$expires" > "$test_root/480.state"
+}
+
+probe='printf "config=%s\nlimit=%s\n" "$MANGOHUD_CONFIG" "${MANGOHUD_FPS_LIMIT-UNSET}"'
+
+write_state 59.94
+fractional=$(
+  VIBESHINE_MANGOHUD_STATE_DIR=$test_root \
+  MANGOHUD_CONFIG='position=top-right,fps_limit=30' \
+  MANGOHUD_FPS_LIMIT=30 \
+    "$wrapper" --appid 480 -- /bin/sh -c "$probe"
+)
+expected_fractional='config=read_cfg,position=top-right,fps_limit=30,fps_limit=59.94
+limit=UNSET'
+[ "$fractional" = "$expected_fractional" ]
+
+write_state 120
+integer=$(
+  VIBESHINE_MANGOHUD_STATE_DIR=$test_root \
+    "$wrapper" --appid 480 -- /bin/sh -c "$probe"
+)
+expected_integer='config=read_cfg,fps_limit=120
+limit=120'
+[ "$integer" = "$expected_integer" ]
+
+rm -f -- "$test_root/480.state"
+passthrough=$(
+  VIBESHINE_MANGOHUD_STATE_DIR=$test_root \
+    "$wrapper" --appid 480 -- /bin/sh -c 'printf pass'
+)
+[ "$passthrough" = pass ]

Nonary/Vibepollo

(cherry picked from commit 0b92c8e27d8c625e10872d09f986d6d8337739e9)

cmake/packaging/linux.cmake
@@ -11,6 +11,9 @@ file(COPY "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/assets/"
 file(CREATE_LINK "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/assets/shaders"
         "${CMAKE_BINARY_DIR}/assets/shaders" COPY_ON_ERROR SYMBOLIC)
 
+install(PROGRAMS "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/vibepollo-mangohud"
+        DESTINATION "${CMAKE_INSTALL_BINDIR}")
+
 if(${SUNSHINE_BUILD_APPIMAGE} OR ${SUNSHINE_BUILD_FLATPAK})
     install(FILES "${SUNSHINE_SOURCE_ASSETS_DIR}/linux/misc/60-sunshine.rules"
             DESTINATION "${SUNSHINE_ASSETS_DIR}/udev/rules.d")

src/platform/linux/mangohud_policy.h
@@ -121,6 +121,38 @@ namespace platf::mangohud {
     return config;
   }
 
+  inline std::string config_override(
+    std::string_view inherited_config,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph
+  ) {
+    std::string config = "read_cfg";
+    if (!inherited_config.empty()) {
+      config.push_back(',');
+      config += inherited_config;
+    }
+
+    const auto managed = config_override(limit, preset, always_show_graph);
+    // The first token in the managed fragment is already supplied above.
+    config.append(managed, std::string_view("read_cfg").size());
+    return config;
+  }
+
+  inline std::string fps_limit_environment_override(
+    std::uint32_t limit_millihz,
+    std::string_view formatted_limit
+  ) {
+    // MangoHud 0.8.4 (and current upstream as of 2026-08) parses this variable
+    // through an int. Let MANGOHUD_CONFIG carry fractional limits exactly so
+    // the compatibility variable cannot silently turn 59.94 into 59.
+    return limit_millihz % 1000 == 0 ? std::string(formatted_limit) : std::string {};
+  }
+
+  inline std::string steam_launch_option(std::string_view app_id) {
+    return "vibepollo-mangohud --appid " + std::string(app_id) + " -- %command%";
+  }
+
   inline launch_policy_t make_launch_policy(
     std::string_view provider,
     bool limiter_enabled,

src/platform/linux/mangohud_state.h
@@ -1,0 +1,145 @@
+/**
+ * @file src/platform/linux/mangohud_state.h
+ * @brief Runtime handoff used by the Steam last-mile MangoHud wrapper.
+ */
+#pragma once
+
+#include <chrono>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <system_error>
+
+#include <unistd.h>
+
+namespace platf::mangohud {
+
+  inline std::filesystem::path state_directory() {
+    if (const char *override_dir = std::getenv("VIBEPOLLO_MANGOHUD_STATE_DIR");
+        override_dir && *override_dir) {
+      return override_dir;
+    }
+    if (const char *runtime_dir = std::getenv("XDG_RUNTIME_DIR"); runtime_dir && *runtime_dir) {
+      return std::filesystem::path(runtime_dir) / "vibepollo" / "mangohud";
+    }
+    if (const char *config_home = std::getenv("XDG_CONFIG_HOME"); config_home && *config_home) {
+      return std::filesystem::path(config_home) / "vibepollo" / "mangohud-runtime";
+    }
+    if (const char *home = std::getenv("HOME"); home && *home) {
+      return std::filesystem::path(home) / ".config" / "vibepollo" / "mangohud-runtime";
+    }
+    return {};
+  }
+
+  inline bool valid_steam_app_id(std::string_view app_id) {
+    if (app_id.empty()) {
+      return false;
+    }
+    for (const char ch : app_id) {
+      if (ch < '0' || ch > '9') {
+        return false;
+      }
+    }
+    return app_id != "0";
+  }
+
+  inline std::filesystem::path state_path(std::string_view app_id) {
+    const auto directory = state_directory();
+    if (directory.empty() || !valid_steam_app_id(app_id)) {
+      return {};
+    }
+    return directory / (std::string(app_id) + ".state");
+  }
+
+  inline std::string serialize_state(
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph,
+    std::chrono::system_clock::time_point expires_at
+  ) {
+    const bool standard_preset = preset == "1" || preset == "2" || preset == "3" || preset == "4";
+    const auto expires = std::chrono::duration_cast<std::chrono::seconds>(
+      expires_at.time_since_epoch()
+    ).count();
+    return "version=1\nlimit=" + std::string(limit) +
+           "\npreset=" + (standard_preset ? std::string(preset) : "custom") +
+           "\nalways_show_graph=" + (always_show_graph ? "1" : "0") +
+           "\nowner_pid=" + std::to_string(static_cast<unsigned long>(getpid())) +
+           "\nexpires=" + std::to_string(expires) + "\n";
+  }
+
+  inline std::filesystem::path write_state(
+    std::string_view app_id,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph
+  ) {
+    const auto path = state_path(app_id);
+    if (path.empty()) {
+      return {};
+    }
+
+    std::error_code ec;
+    std::filesystem::create_directories(path.parent_path(), ec);
+    if (ec) {
+      return {};
+    }
+    std::filesystem::permissions(
+      path.parent_path(),
+      std::filesystem::perms::owner_all,
+      std::filesystem::perm_options::replace,
+      ec
+    );
+    if (ec) {
+      return {};
+    }
+
+    auto temporary = path;
+    temporary += ".tmp-" + std::to_string(static_cast<unsigned long>(getpid()));
+    {
+      std::ofstream output(temporary, std::ios::binary | std::ios::trunc);
+      if (!output) {
+        return {};
+      }
+      output << serialize_state(
+        limit,
+        preset,
+        always_show_graph,
+        std::chrono::system_clock::now() + std::chrono::hours(1)
+      );
+      if (!output) {
+        output.close();
+        std::filesystem::remove(temporary, ec);
+        return {};
+      }
+    }
+    std::filesystem::permissions(
+      temporary,
+      std::filesystem::perms::owner_read | std::filesystem::perms::owner_write,
+      std::filesystem::perm_options::replace,
+      ec
+    );
+    if (ec) {
+      std::filesystem::remove(temporary, ec);
+      return {};
+    }
+    std::filesystem::rename(temporary, path, ec);
+    if (ec) {
+      std::filesystem::remove(temporary, ec);
+      return {};
+    }
+    return path;
+  }
+
+  inline void remove_state(std::string_view app_id) {
+    const auto path = state_path(app_id);
+    if (path.empty()) {
+      return;
+    }
+    std::error_code ec;
+    std::filesystem::remove(path, ec);
+  }
+
+}  // namespace platf::mangohud

tests/unit/platform/linux/test_mangohud_policy.cpp
@@ -5,6 +5,9 @@
 #include "../../../tests_common.h"
 
 #include <src/platform/linux/mangohud_policy.h>
+#include <src/platform/linux/mangohud_state.h>
+
+#include <chrono>
 
 namespace mangohud = platf::mangohud;
 
@@ -62,6 +65,35 @@ TEST(MangoHudPolicy, BuildsSteamCompatibleAuthoritativeLimitConfig) {
     mangohud::config_override("120", "4", true),
     "read_cfg,preset=4,no_display=0,frame_timing=1,fps_limit=120"
   );
+  EXPECT_EQ(
+    mangohud::config_override("position=top-right,fps_limit=30", "120", "custom", false),
+    "read_cfg,position=top-right,fps_limit=30,fps_limit=120"
+  );
+}
+
+TEST(MangoHudPolicy, AvoidsMangoHudFractionalEnvironmentTruncation) {
+  EXPECT_EQ(mangohud::fps_limit_environment_override(120000, "120"), "120");
+  EXPECT_TRUE(mangohud::fps_limit_environment_override(59940, "59.94").empty());
+  EXPECT_EQ(
+    mangohud::steam_launch_option("480"),
+    "vibepollo-mangohud --appid 480 -- %command%"
+  );
+}
+
+TEST(MangoHudPolicy, ValidatesAndSerializesLastMileSteamState) {
+  EXPECT_TRUE(mangohud::valid_steam_app_id("480"));
+  EXPECT_FALSE(mangohud::valid_steam_app_id("0"));
+  EXPECT_FALSE(mangohud::valid_steam_app_id("48/0"));
+
+  const auto state = mangohud::serialize_state(
+    "59.94",
+    "3",
+    true,
+    std::chrono::system_clock::time_point {std::chrono::seconds {12345}}
+  );
+  EXPECT_NE(state.find("version=1\nlimit=59.94\npreset=3\nalways_show_graph=1\n"), std::string::npos);
+  EXPECT_NE(state.find("owner_pid="), std::string::npos);
+  EXPECT_TRUE(state.ends_with("expires=12345\n"));
 }
 
 TEST(MangoHudPolicy, AutomaticVirtualLimiterDoesNotEnablePhysicalStreams) {

tests/unit/platform/linux/test_mangohud_wrapper.sh
@@ -1,0 +1,47 @@
+#!/bin/sh
+set -eu
+
+wrapper=$1
+test_root=$(mktemp -d)
+trap 'rm -rf -- "$test_root"' EXIT HUP INT TERM
+
+write_state() {
+  limit=$1
+  expires=$(($(date +%s) + 60))
+  printf '%s\n' \
+    'version=1' \
+    "limit=$limit" \
+    'preset=custom' \
+    'always_show_graph=0' \
+    "owner_pid=$$" \
+    "expires=$expires" > "$test_root/480.state"
+}
+
+probe='printf "config=%s\nlimit=%s\n" "$MANGOHUD_CONFIG" "${MANGOHUD_FPS_LIMIT-UNSET}"'
+
+write_state 59.94
+fractional=$(
+  VIBEPOLLO_MANGOHUD_STATE_DIR=$test_root \
+  MANGOHUD_CONFIG='position=top-right,fps_limit=30' \
+  MANGOHUD_FPS_LIMIT=30 \
+    "$wrapper" --appid 480 -- /bin/sh -c "$probe"
+)
+expected_fractional='config=read_cfg,position=top-right,fps_limit=30,fps_limit=59.94
+limit=UNSET'
+[ "$fractional" = "$expected_fractional" ]
+
+write_state 120
+integer=$(
+  VIBEPOLLO_MANGOHUD_STATE_DIR=$test_root \
+    "$wrapper" --appid 480 -- /bin/sh -c "$probe"
+)
+expected_integer='config=read_cfg,fps_limit=120
+limit=120'
+[ "$integer" = "$expected_integer" ]
+
+rm -f -- "$test_root/480.state"
+passthrough=$(
+  VIBEPOLLO_MANGOHUD_STATE_DIR=$test_root \
+    "$wrapper" --appid 480 -- /bin/sh -c 'printf pass'
+)
+[ "$passthrough" = pass ]