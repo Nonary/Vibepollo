Nonary/vibeshine

fix(packaging): complete running-host upgrades reliably
Thaw the validated controller before its ordered stop, allow enough time for host cleanup, and let the Arch post-upgrade hook recognize the fail-closed quiescent state left by its pre-upgrade invocation. This prevents reinstalling over a live host from timing out or remaining fenced behind runtime masks.

Update the beta.5 notes for the pairing and upgrade fixes included in the respun tag.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol Medium

packaging/linux/copr/Sunshine.spec
@@ -471,7 +471,9 @@ vibeshine_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  # Controller cleanup can legitimately spend more than 30 seconds draining
+  # the host and GPU bindings; keep waiting for the manager's ordered stop.
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibeshine_bounded_unit_list() (
   vibeshine_unit_pattern=$1
@@ -758,6 +760,20 @@ vibeshine_controller_remains_frozen() {
     printf '%%s\n' "$vibeshine_controller_state" | grep -qx "ControlGroup=$vibeshine_controller_cgroup" && \
     printf '%%s\n' "$vibeshine_controller_state" | grep -qx 'FreezerState=frozen'
 }
+vibeshine_thaw_controller() {
+  [ "$vibeshine_controller_was_frozen" -eq 1 ] || return 0
+  timeout --signal=KILL 15 systemctl thaw \
+    vibeshine-session-controller.service 2>/dev/null || return 1
+  vibeshine_controller_was_frozen=0
+  vibeshine_controller_state=$(timeout --signal=KILL 5 systemctl show \
+    vibeshine-session-controller.service --property=ActiveState --property=SubState \
+    --property=MainPID --property=ControlGroup --property=FreezerState 2>/dev/null) || return 1
+  printf '%%s\n' "$vibeshine_controller_state" | grep -qx 'ActiveState=active' && \
+    printf '%%s\n' "$vibeshine_controller_state" | grep -qx 'SubState=running' && \
+    printf '%%s\n' "$vibeshine_controller_state" | grep -qx "MainPID=$vibeshine_controller_pid" && \
+    printf '%%s\n' "$vibeshine_controller_state" | grep -qx "ControlGroup=$vibeshine_controller_cgroup" && \
+    printf '%%s\n' "$vibeshine_controller_state" | grep -qx 'FreezerState=running'
+}
 vibeshine_run_optional_legacy_command() {
   if [ ! -e "$vibeshine_legacy_host" ] && [ ! -L "$vibeshine_legacy_host" ]; then return 2; fi
   vibeshine_privileged_helper_is_safe "$vibeshine_legacy_host" || return 1
@@ -925,9 +941,11 @@ vibeshine_quiesce_machine_host() {
     vibeshine_stop_restore_instances || return 1
     vibeshine_stop_brokers || return 1
     vibeshine_controller_remains_frozen || return 1
+    # systemd refuses StopUnit for a frozen service. Admission and the host are
+    # already masked here, so thaw the controller only for its ordered stop.
+    vibeshine_thaw_controller || return 1
     vibeshine_stop_exact_unit vibeshine-session-controller.service
     vibeshine_unit_is_quiescent vibeshine-session-controller.service || return 1
-    vibeshine_controller_was_frozen=0
     vibeshine_stop_exact_unit vibeshine-prelogin.service
     vibeshine_stop_exact_unit vibeshine-machine-prepare.service
     timeout --signal=KILL 15 systemctl disable vibeshine-session-controller.service --now 2>/dev/null || true
@@ -1112,7 +1130,7 @@ vibeshine_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibeshine_bounded_broker_list() (
   vibeshine_broker_list=$(mktemp /run/vibeshine-broker-units.XXXXXX) || exit 1
@@ -1463,7 +1481,7 @@ vibeshine_preun_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibeshine_preun_bounded_broker_list() (
   vibeshine_broker_list=$(mktemp /run/vibeshine-broker-units.XXXXXX) || exit 1

tests/unit/platform/linux/test_machine_service_contract.py
@@ -723,9 +723,13 @@ for lifecycle, label in (
         "SendSIGKILL=no",
         "--property=Job",
         "systemctl freeze",
+        "--kill-after=2 60 systemctl stop",
         "FreezerState=frozen",
         "frozen 1",
         "vibeshine_controller_remains_frozen",
+        "vibeshine_thaw_controller",
+        "systemctl thaw",
+        "FreezerState=running",
         "vibeshine-session-controller.service",
         "vibeshine.service",
         "vibeshine-prelogin.service",
@@ -785,7 +789,8 @@ for lifecycle, label in (
     first_restore_stop = quiesce.index("vibeshine_stop_restore_instances || return 1", host_mask_at)
     broker_drain_at = quiesce.index("vibeshine_stop_brokers || return 1", first_restore_stop)
     frozen_proof_at = quiesce.index("vibeshine_controller_remains_frozen || return 1", broker_drain_at)
-    controller_stop_at = quiesce.index("vibeshine_stop_exact_unit vibeshine-session-controller.service", frozen_proof_at)
+    thaw_at = quiesce.index("vibeshine_thaw_controller || return 1", frozen_proof_at)
+    controller_stop_at = quiesce.index("vibeshine_stop_exact_unit vibeshine-session-controller.service", thaw_at)
     remove_pam_at = normalized.index("vibeshine_run_optional_legacy_command remove-pam", first_restore_stop)
     last_restore_stop = normalized.rindex("vibeshine_stop_restore_instances || return 1")
     legacy_cleanup_at = normalized.rindex("vibeshine_cleanup_legacy_transition_state || return 1")
@@ -805,6 +810,7 @@ for lifecycle, label in (
         < first_restore_stop
         < broker_drain_at
         < frozen_proof_at
+        < thaw_at
         < controller_stop_at
     ):
         raise AssertionError(f"{label} does not fence the old controller/host and drain GPU ownership in fail-closed order")
@@ -822,6 +828,28 @@ for function_name in (
     require(rpm_pre, f"{function_name}() (", "RPM pre-replacement subshell isolation")
 require(rpm_post, "vibeshine_stop_brokers() (", "RPM post-replacement subshell isolation")
 require(rpm_preun, "vibeshine_preun_stop_brokers() (", "RPM preun subshell isolation")
+
+# Pacman runs the same install script before and after replacement. The second
+# invocation must accept the fully masked, quiescent state left by the first
+# instead of attempting to activate a drop-in through the runtime host mask.
+for invariant in (
+    "vibeshine_preserved_quiesce_is_safe",
+    "vibeshine_runtime_root_is_safe_or_absent",
+    "vibeshine_control_socket_is_masked",
+    "vibeshine_broker_socket_is_masked",
+    "vibeshine_host_unit_is_masked",
+    "vibeshine_control_instances_are_quiescent",
+    "vibeshine_brokers_are_quiescent",
+    "vibeshine_restore_instances_are_quiescent",
+    "! -e \"$vibeshine_broker_socket\"",
+    "! -e \"$vibeshine_control_socket\"",
+):
+    require(arch_pre, invariant, "Arch preserved quiesce detection")
+arch_quiesce = arch_pre.rsplit("\ndo_quiesce_machine_host() {\n", 1)[1].split("\n}\n", 1)[0]
+preserved_at = arch_quiesce.index("vibeshine_preserved_quiesce_is_safe && return 0")
+select_at = arch_quiesce.index("vibeshine_select_upgrade_kill_mode || return 1")
+if not preserved_at < select_at:
+    raise AssertionError("Arch post-upgrade does not recognize preserved quiesce before fence activation")
 for command in (
     "systemctl daemon-reload || exit 1",
     ") || exit 1",

Nonary/Vibepollo

(cherry picked from commit eaa8bea16e9f19a827cf90561238decfc86b598f)

packaging/linux/copr/Sunshine.spec
@@ -472,7 +472,9 @@ vibepollo_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  # Controller cleanup can legitimately spend more than 30 seconds draining
+  # the host and GPU bindings; keep waiting for the manager's ordered stop.
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibepollo_bounded_unit_list() (
   vibepollo_unit_pattern=$1
@@ -756,6 +758,20 @@ vibepollo_controller_remains_frozen() {
     printf '%%s\n' "$vibepollo_controller_state" | grep -qx "ControlGroup=$vibepollo_controller_cgroup" && \
     printf '%%s\n' "$vibepollo_controller_state" | grep -qx 'FreezerState=frozen'
 }
+vibepollo_thaw_controller() {
+  [ "$vibepollo_controller_was_frozen" -eq 1 ] || return 0
+  timeout --signal=KILL 15 systemctl thaw \
+    vibepollo-session-controller.service 2>/dev/null || return 1
+  vibepollo_controller_was_frozen=0
+  vibepollo_controller_state=$(timeout --signal=KILL 5 systemctl show \
+    vibepollo-session-controller.service --property=ActiveState --property=SubState \
+    --property=MainPID --property=ControlGroup --property=FreezerState 2>/dev/null) || return 1
+  printf '%%s\n' "$vibepollo_controller_state" | grep -qx 'ActiveState=active' && \
+    printf '%%s\n' "$vibepollo_controller_state" | grep -qx 'SubState=running' && \
+    printf '%%s\n' "$vibepollo_controller_state" | grep -qx "MainPID=$vibepollo_controller_pid" && \
+    printf '%%s\n' "$vibepollo_controller_state" | grep -qx "ControlGroup=$vibepollo_controller_cgroup" && \
+    printf '%%s\n' "$vibepollo_controller_state" | grep -qx 'FreezerState=running'
+}
 vibepollo_run_optional_legacy_command() {
   if [ ! -e "$vibepollo_legacy_host" ] && [ ! -L "$vibepollo_legacy_host" ]; then return 2; fi
   vibepollo_privileged_helper_is_safe "$vibepollo_legacy_host" || return 1
@@ -923,9 +939,11 @@ vibepollo_quiesce_machine_host() {
     vibepollo_stop_restore_instances || return 1
     vibepollo_stop_brokers || return 1
     vibepollo_controller_remains_frozen || return 1
+    # systemd refuses StopUnit for a frozen service. Admission and the host are
+    # already masked here, so thaw the controller only for its ordered stop.
+    vibepollo_thaw_controller || return 1
     vibepollo_stop_exact_unit vibepollo-session-controller.service
     vibepollo_unit_is_quiescent vibepollo-session-controller.service || return 1
-    vibepollo_controller_was_frozen=0
     vibepollo_stop_exact_unit vibepollo-prelogin.service
     vibepollo_stop_exact_unit vibepollo-machine-prepare.service
     timeout --signal=KILL 15 systemctl disable vibepollo-session-controller.service --now 2>/dev/null || true
@@ -1110,7 +1128,7 @@ vibepollo_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibepollo_bounded_broker_list() (
   vibepollo_broker_list=$(mktemp /run/vibepollo-broker-units.XXXXXX) || exit 1
@@ -1460,7 +1478,7 @@ vibepollo_preun_stop_exact_unit() {
   # Never bypass a service's ordered resource teardown.  Killing systemctl
   # only abandons the client while its manager job continues; killing the unit
   # cgroup can strand live GPU imports and is therefore forbidden here.
-  timeout --signal=TERM --kill-after=2 30 systemctl stop "$1" 2>/dev/null
+  timeout --signal=TERM --kill-after=2 60 systemctl stop "$1" 2>/dev/null
 }
 vibepollo_preun_bounded_broker_list() (
   vibepollo_broker_list=$(mktemp /run/vibepollo-broker-units.XXXXXX) || exit 1

tests/unit/platform/linux/test_machine_service_contract.py
@@ -739,9 +739,13 @@ for lifecycle, label in (
         "SendSIGKILL=no",
         "--property=Job",
         "systemctl freeze",
+        "--kill-after=2 60 systemctl stop",
         "FreezerState=frozen",
         "frozen 1",
         "vibepollo_controller_remains_frozen",
+        "vibepollo_thaw_controller",
+        "systemctl thaw",
+        "FreezerState=running",
         "vibepollo-session-controller.service",
         "vibepollo.service",
         "vibepollo-prelogin.service",
@@ -801,7 +805,8 @@ for lifecycle, label in (
     first_restore_stop = quiesce.index("vibepollo_stop_restore_instances || return 1", host_mask_at)
     broker_drain_at = quiesce.index("vibepollo_stop_brokers || return 1", first_restore_stop)
     frozen_proof_at = quiesce.index("vibepollo_controller_remains_frozen || return 1", broker_drain_at)
-    controller_stop_at = quiesce.index("vibepollo_stop_exact_unit vibepollo-session-controller.service", frozen_proof_at)
+    thaw_at = quiesce.index("vibepollo_thaw_controller || return 1", frozen_proof_at)
+    controller_stop_at = quiesce.index("vibepollo_stop_exact_unit vibepollo-session-controller.service", thaw_at)
     remove_pam_at = normalized.index("vibepollo_run_optional_legacy_command remove-pam", first_restore_stop)
     last_restore_stop = normalized.rindex("vibepollo_stop_restore_instances || return 1")
     legacy_cleanup_at = normalized.rindex("vibepollo_cleanup_legacy_transition_state || return 1")
@@ -821,6 +826,7 @@ for lifecycle, label in (
         < first_restore_stop
         < broker_drain_at
         < frozen_proof_at
+        < thaw_at
         < controller_stop_at
     ):
         raise AssertionError(f"{label} does not fence the old controller/host and drain GPU ownership in fail-closed order")
@@ -838,6 +844,28 @@ for function_name in (
     require(rpm_pre, f"{function_name}() (", "RPM pre-replacement subshell isolation")
 require(rpm_post, "vibepollo_stop_brokers() (", "RPM post-replacement subshell isolation")
 require(rpm_preun, "vibepollo_preun_stop_brokers() (", "RPM preun subshell isolation")
+
+# Pacman runs the same install script before and after replacement. The second
+# invocation must accept the fully masked, quiescent state left by the first
+# instead of attempting to activate a drop-in through the runtime host mask.
+for invariant in (
+    "vibepollo_preserved_quiesce_is_safe",
+    "vibepollo_runtime_root_is_safe_or_absent",
+    "vibepollo_control_socket_is_masked",
+    "vibepollo_broker_socket_is_masked",
+    "vibepollo_host_unit_is_masked",
+    "vibepollo_control_instances_are_quiescent",
+    "vibepollo_brokers_are_quiescent",
+    "vibepollo_restore_instances_are_quiescent",
+    "! -e \"$vibepollo_broker_socket\"",
+    "! -e \"$vibepollo_control_socket\"",
+):
+    require(arch_pre, invariant, "Arch preserved quiesce detection")
+arch_quiesce = arch_pre.rsplit("\ndo_quiesce_machine_host() {\n", 1)[1].split("\n}\n", 1)[0]
+preserved_at = arch_quiesce.index("vibepollo_preserved_quiesce_is_safe && return 0")
+select_at = arch_quiesce.index("vibepollo_select_upgrade_kill_mode || return 1")
+if not preserved_at < select_at:
+    raise AssertionError("Arch post-upgrade does not recognize preserved quiesce before fence activation")
 for command in (
     "systemctl daemon-reload || exit 1",
     ") || exit 1",