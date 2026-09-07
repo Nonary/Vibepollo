Nonary/vibeshine

fix(linux): harden machine-session handoff
Login transitions failed because PAM-based UID switching could not run inside the restricted preparation service. Use a non-PAM transition, restore device and client allowlists, and bind cross-user application launches to a root-owned command manifest. Clean runtime ACLs on stop and refresh KWin capability isolation after package replacement.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol High

cmake/packaging/linux.cmake
@@ -106,6 +106,8 @@ else()
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-machine-prepare.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-kwin-capability-refresh.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-kwin-capability.path"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
     endif()

packaging/linux/copr/Sunshine.spec
@@ -371,6 +371,7 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       systemctl daemon-reload || true
       systemctl disable --now vibeshine-prelogin.service 2>/dev/null || true
       systemctl enable vibeshine.service || echo "warning: could not enable the Vibeshine machine host."
+      systemctl enable --now vibeshine-kwin-capability.path || echo "warning: could not enable the KWin capability watcher."
     else
       echo "warning: could not install the Plasma Login Manager handoff hook."
     fi
@@ -384,6 +385,8 @@ fi
 %preun
 if [ "$1" -eq 0 ]; then
   systemctl disable --now vibeshine.service vibeshine-prelogin.service 2>/dev/null || true
+  systemctl disable --now vibeshine-kwin-capability.path 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibeshine-machine-host cleanup || true
   %{_prefix}/libexec/vibeshine/vibeshine-machine-host remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
   systemctl stop vibeshine-drm-setup.service 2>/dev/null || true
@@ -426,6 +429,8 @@ fi
 %{_unitdir}/vibeshine-machine-prepare.service
 %{_unitdir}/vibeshine.service
 %{_unitdir}/vibeshine-session-restore@.service
+%{_unitdir}/vibeshine-kwin-capability-refresh.service
+%{_unitdir}/vibeshine-kwin-capability.path
 
 # Udev rules
 %{_udevrulesdir}/*-sunshine.rules

src/nvhttp.cpp
@@ -2165,6 +2165,22 @@ namespace nvhttp {
     return std::nullopt;
   }
 
+  bool greeter_client_allowed(std::string_view uuid) {
+    const auto *role = std::getenv("VIBESHINE_SESSION_ROLE");
+    if (!role || std::string_view {role} != "greeter") return true;
+    const auto *configured = std::getenv("VIBESHINE_ALLOWED_CLIENT_UUIDS");
+    if (!configured || !*configured || uuid.empty()) return false;
+    std::string_view remaining {configured};
+    while (!remaining.empty()) {
+      const auto separator = remaining.find(',');
+      const auto candidate = remaining.substr(0, separator);
+      if (candidate == uuid) return true;
+      if (separator == std::string_view::npos) break;
+      remaining.remove_prefix(separator + 1);
+    }
+    return false;
+  }
+
   void remember_tls_client_identity(const boost::asio::ip::tcp::endpoint &endpoint, const resolved_client_identity_t &identity) {
     const auto key = endpoint_key(endpoint);
     if (key.empty() || identity.uuid.empty()) {
@@ -5011,10 +5027,15 @@ namespace nvhttp {
         return verified;
       }
 
-      verified = 1;
-      if (auto identity = resolve_client_identity_from_peer_cert(x509_verify)) {
-        remember_tls_client_identity(remote_endpoint, *identity);
+      auto identity = resolve_client_identity_from_peer_cert(x509_verify);
+      if ((!identity || !greeter_client_allowed(identity->uuid)) &&
+          std::getenv("VIBESHINE_SESSION_ROLE") &&
+          std::string_view {std::getenv("VIBESHINE_SESSION_ROLE")} == "greeter") {
+        BOOST_LOG(warning) << "Pre-login TLS client is not in the administrator allowlist.";
+        return verified;
       }
+      verified = 1;
+      if (identity) remember_tls_client_identity(remote_endpoint, *identity);
       tl_peer_certificate = std::move(x509_verify);
 
       return verified;

src/platform/linux/misc.cpp
@@ -309,36 +309,35 @@ namespace platf {
     (void) interactive;
     ec.clear();
 
-    std::vector<std::string> parts;
-    try {
-      parts = boost::program_options::split_unix(cmd);
-    } catch (...) {
-    }
-
-    if (parts.empty()) {
-      ec = std::make_error_code(std::errc::invalid_argument);
-      return bp::child();
-    }
-
-    auto exe_path = v2::filesystem::path(parts.front());
-    // Only PATH-search when there's no directory component (e.g., "foo" not "./foo" or "../foo")
-    if (!exe_path.is_absolute() && exe_path.parent_path().empty()) {
-      exe_path = v2::environment::find_executable(exe_path);
-    }
-
-    if (exe_path.empty()) {
-      ec = std::make_error_code(std::errc::no_such_file_or_directory);
-      return bp::child();
-    }
-
     std::vector<std::string> args;
-    if (parts.size() > 1) {
-      args.assign(parts.begin() + 1, parts.end());
-    }
-
-    if (std::getenv("VIBESHINE_MACHINE_HOST")) {
-      args.insert(args.begin(), exe_path.string());
+    v2::filesystem::path exe_path;
+    const bool session_command = std::getenv("VIBESHINE_MACHINE_HOST") != nullptr;
+    if (session_command) {
+      if (cmd.empty()) {
+        ec = std::make_error_code(std::errc::invalid_argument);
+        return bp::child();
+      }
       exe_path = v2::filesystem::path("/usr/libexec/vibeshine/vibeshine-session-exec");
+      args = {"app", cmd, working_dir.string()};
+    } else {
+      std::vector<std::string> parts;
+      try {
+        parts = boost::program_options::split_unix(cmd);
+      } catch (...) {
+      }
+      if (parts.empty()) {
+        ec = std::make_error_code(std::errc::invalid_argument);
+        return bp::child();
+      }
+      exe_path = v2::filesystem::path(parts.front());
+      if (!exe_path.is_absolute() && exe_path.parent_path().empty()) {
+        exe_path = v2::environment::find_executable(exe_path);
+      }
+      if (exe_path.empty()) {
+        ec = std::make_error_code(std::errc::no_such_file_or_directory);
+        return bp::child();
+      }
+      if (parts.size() > 1) args.assign(parts.begin() + 1, parts.end());
     }
 
     v2::process_stdio stdio {};
@@ -357,7 +356,7 @@ namespace platf {
     try {
 #ifndef _WIN32
       if (group) {
-        if (!working_dir.empty()) {
+        if (!session_command && !working_dir.empty()) {
           auto start = v2::process_start_dir(v2::filesystem::path(working_dir.string()));
           auto proc = v2::process(exec, exe_path, args, start, stdio, env_init, bp::detail::posix_group_initer {group});
           return bp::child(std::move(proc));
@@ -366,7 +365,7 @@ namespace platf {
         return bp::child(std::move(proc));
       }
 #endif
-      if (!working_dir.empty()) {
+      if (!session_command && !working_dir.empty()) {
         auto start = v2::process_start_dir(v2::filesystem::path(working_dir.string()));
         auto proc = v2::process(exec, exe_path, args, start, stdio, env_init);
         return bp::child(std::move(proc));

src/steam_integration.cpp
@@ -1116,7 +1116,7 @@ namespace platf::steam {
   #else
       const auto app_id_string = std::to_string(app_id);
       if (std::getenv("VIBESHINE_MACHINE_HOST")) {
-        execl("/usr/libexec/vibeshine/vibeshine-session-exec", "vibeshine-session-exec", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+        execl("/usr/libexec/vibeshine/vibeshine-session-exec", "vibeshine-session-exec", "steam", app_id_string.c_str(), static_cast<char *>(nullptr));
       } else {
         execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
       }

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -66,10 +66,12 @@ require(handoff, '/usr/bin/timeout --signal=KILL "$socket_timeout"', "control so
 require(handoff, "rollback_to_prelogin", "transactional login handoff")
 require(handoff, "another desktop session owns the stream handoff", "multi-user exclusion")
 require(handoff, '[[ "$state_token" != "$token" ]]', "stale close rejection")
-require(handoff, 'systemctl start --no-block "$prelogin_service"', "pre-login restoration")
-require(handoff, 'user_systemctl "$desktop_user" stop "$desktop_target"', "desktop shutdown")
+require(handoff, 'systemctl start --no-block "$host_service"', "machine-host restoration")
 require(handoff, "stream_ports_are_free", "port ownership handoff")
 require(handoff, "release_connector", "connector ownership handoff")
+require(handoff, "/usr/bin/setpriv --reuid", "non-PAM user-manager transition")
+if "/usr/bin/runuser" in handoff:
+    raise AssertionError("restricted handoff must not invoke PAM through runuser")
 require(handoff, "wait-prelogin", "pre-login readiness gate")
 require(handoff, "attempt & (attempt - 1)", "rate-limited status logging")
 if "PAM_TYPE" in handoff or "PAM_USER" in handoff:
@@ -118,7 +120,7 @@ if "source \"$settings_file\"" in prelogin_sync or ". \"$settings_file\"" in pre
     raise AssertionError("root helper must parse rather than source administrator settings")
 require(prelogin_config, "capture = kms", "pre-login KMS policy")
 require(prelogin_config, "virtual_display_layout = exclusive", "exclusive greeter layout")
-require(prelogin_apps, "/usr/libexec/vibeshine/vibeshine-prelogin-sync activate", "greeter activation command")
+require(prelogin_apps, "/usr/libexec/vibeshine/vibeshine-machine-host activate", "greeter activation command")
 
 require(packaging, "add_library(pam_vibeshine_session MODULE", "native PAM build")
 require(packaging, "install(TARGETS pam_vibeshine_session", "native PAM install")
@@ -129,10 +131,9 @@ require(packaging, "util-linux", "native flock/runuser dependency")
 require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"',
         "versioned kernel source fingerprint")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-handoff"', "native install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"', "native install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin-sync"', "pre-login helper install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin.service"', "pre-login unit install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-machine-host"', "machine-host helper install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine.service"', "machine-host unit install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"', "pre-login profile install")
 
 require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")

Nonary/Vibepollo

(cherry picked from commit 9728bc000fbce8539a444e11e708a4af04c13c35)

cmake/packaging/linux.cmake
@@ -106,6 +106,8 @@ else()
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-machine-prepare.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-kwin-capability-refresh.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-kwin-capability.path"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
     endif()

packaging/linux/copr/Sunshine.spec
@@ -372,6 +372,7 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       systemctl daemon-reload || true
       systemctl disable --now vibepollo-prelogin.service 2>/dev/null || true
       systemctl enable vibepollo.service || echo "warning: could not enable the Vibepollo machine host."
+      systemctl enable --now vibepollo-kwin-capability.path || echo "warning: could not enable the KWin capability watcher."
     else
       echo "warning: could not install the Plasma Login Manager handoff hook."
     fi
@@ -385,6 +386,8 @@ fi
 %preun
 if [ "$1" -eq 0 ]; then
   systemctl disable --now vibepollo.service vibepollo-prelogin.service 2>/dev/null || true
+  systemctl disable --now vibepollo-kwin-capability.path 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibepollo-machine-host cleanup || true
   %{_prefix}/libexec/vibeshine/vibepollo-machine-host remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
   systemctl stop vibeshine-drm-setup.service 2>/dev/null || true
@@ -428,6 +431,8 @@ fi
 %{_unitdir}/vibepollo-machine-prepare.service
 %{_unitdir}/vibepollo.service
 %{_unitdir}/vibepollo-session-restore@.service
+%{_unitdir}/vibepollo-kwin-capability-refresh.service
+%{_unitdir}/vibepollo-kwin-capability.path
 
 # Udev rules
 %{_udevrulesdir}/*-sunshine.rules

src/nvhttp.cpp
@@ -2321,6 +2321,23 @@ namespace nvhttp {
       return std::nullopt;
     }
 
+  bool greeter_client_allowed(std::string_view uuid) {
+    const auto *role = std::getenv("VIBEPOLLO_SESSION_ROLE");
+    if (!role || std::string_view {role} != "greeter") return true;
+    const auto *configured = std::getenv("VIBEPOLLO_ALLOWED_CLIENT_UUIDS");
+    if (!configured || !*configured || uuid.empty()) return false;
+    std::string_view remaining {configured};
+    while (!remaining.empty()) {
+      const auto separator = remaining.find(',');
+      const auto candidate = remaining.substr(0, separator);
+      if (candidate == uuid) return true;
+      if (separator == std::string_view::npos) break;
+      remaining.remove_prefix(separator + 1);
+    }
+    return false;
+  }
+
+
     void remember_tls_client_identity(req_https_t request, const resolved_client_identity_t &identity) {
       const auto key = endpoint_key(request);
       if (key.empty() || identity.uuid.empty()) {
@@ -5832,13 +5849,16 @@ namespace nvhttp {
         return verified;
       }
 
-      verified = true;
-      if (x509_verify) {
-        if (auto identity = resolve_client_identity_from_peer_cert(x509_verify)) {
-          remember_tls_client_identity(req, *identity);
-        }
-        tl_peer_certificate = std::move(x509_verify);
+      auto identity = resolve_client_identity_from_peer_cert(x509_verify);
+      if ((!identity || !greeter_client_allowed(identity->uuid)) &&
+          std::getenv("VIBEPOLLO_SESSION_ROLE") &&
+          std::string_view {std::getenv("VIBEPOLLO_SESSION_ROLE")} == "greeter") {
+        BOOST_LOG(warning) << "Pre-login TLS client is not in the administrator allowlist.";
+        return verified;
       }
+      verified = true;
+      if (identity) remember_tls_client_identity(req, *identity);
+      tl_peer_certificate = std::move(x509_verify);
 
       return true;
     };

src/platform/linux/misc.cpp
@@ -400,36 +400,35 @@ namespace platf {
     (void) interactive;
     ec.clear();
 
-    std::vector<std::string> parts;
-    try {
-      parts = boost::program_options::split_unix(cmd);
-    } catch (...) {
-    }
-
-    if (parts.empty()) {
-      ec = std::make_error_code(std::errc::invalid_argument);
-      return bp::child();
-    }
-
-    auto exe_path = v2::filesystem::path(parts.front());
-    // Only PATH-search when there's no directory component (e.g., "foo" not "./foo" or "../foo")
-    if (!exe_path.is_absolute() && exe_path.parent_path().empty()) {
-      exe_path = v2::environment::find_executable(exe_path);
-    }
-
-    if (exe_path.empty()) {
-      ec = std::make_error_code(std::errc::no_such_file_or_directory);
-      return bp::child();
-    }
-
     std::vector<std::string> args;
-    if (parts.size() > 1) {
-      args.assign(parts.begin() + 1, parts.end());
-    }
-
-    if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
-      args.insert(args.begin(), exe_path.string());
+    v2::filesystem::path exe_path;
+    const bool session_command = std::getenv("VIBEPOLLO_MACHINE_HOST") != nullptr;
+    if (session_command) {
+      if (cmd.empty()) {
+        ec = std::make_error_code(std::errc::invalid_argument);
+        return bp::child();
+      }
       exe_path = v2::filesystem::path("/usr/libexec/vibeshine/vibepollo-session-exec");
+      args = {"app", cmd, working_dir.string()};
+    } else {
+      std::vector<std::string> parts;
+      try {
+        parts = boost::program_options::split_unix(cmd);
+      } catch (...) {
+      }
+      if (parts.empty()) {
+        ec = std::make_error_code(std::errc::invalid_argument);
+        return bp::child();
+      }
+      exe_path = v2::filesystem::path(parts.front());
+      if (!exe_path.is_absolute() && exe_path.parent_path().empty()) {
+        exe_path = v2::environment::find_executable(exe_path);
+      }
+      if (exe_path.empty()) {
+        ec = std::make_error_code(std::errc::no_such_file_or_directory);
+        return bp::child();
+      }
+      if (parts.size() > 1) args.assign(parts.begin() + 1, parts.end());
     }
 
     v2::process_stdio stdio {};
@@ -448,7 +447,7 @@ namespace platf {
     try {
 #ifndef _WIN32
       if (group) {
-        if (!working_dir.empty()) {
+        if (!session_command && !working_dir.empty()) {
           auto start = v2::process_start_dir(v2::filesystem::path(working_dir.string()));
           auto proc = v2::process(exec, exe_path, args, start, stdio, env_init, bp::detail::posix_group_initer {group});
           return bp::child(std::move(proc));
@@ -457,7 +456,7 @@ namespace platf {
         return bp::child(std::move(proc));
       }
 #endif
-      if (!working_dir.empty()) {
+      if (!session_command && !working_dir.empty()) {
         auto start = v2::process_start_dir(v2::filesystem::path(working_dir.string()));
         auto proc = v2::process(exec, exe_path, args, start, stdio, env_init);
         return bp::child(std::move(proc));

src/steam_integration.cpp
@@ -1116,7 +1116,7 @@ namespace platf::steam {
   #else
       const auto app_id_string = std::to_string(app_id);
       if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
-        execl("/usr/libexec/vibeshine/vibepollo-session-exec", "vibepollo-session-exec", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
+        execl("/usr/libexec/vibeshine/vibepollo-session-exec", "vibepollo-session-exec", "steam", app_id_string.c_str(), static_cast<char *>(nullptr));
       } else {
         execlp("steam", "steam", "-applaunch", app_id_string.c_str(), static_cast<char *>(nullptr));
       }

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -66,10 +66,12 @@ require(handoff, '/usr/bin/timeout --signal=KILL "$socket_timeout"', "control so
 require(handoff, "rollback_to_prelogin", "transactional login handoff")
 require(handoff, "another desktop session owns the stream handoff", "multi-user exclusion")
 require(handoff, '[[ "$state_token" != "$token" ]]', "stale close rejection")
-require(handoff, 'systemctl start --no-block "$prelogin_service"', "pre-login restoration")
-require(handoff, 'user_systemctl "$desktop_user" stop "$desktop_target"', "desktop shutdown")
+require(handoff, 'systemctl start --no-block "$host_service"', "machine-host restoration")
 require(handoff, "stream_ports_are_free", "port ownership handoff")
 require(handoff, "release_connector", "connector ownership handoff")
+require(handoff, "/usr/bin/setpriv --reuid", "non-PAM user-manager transition")
+if "/usr/bin/runuser" in handoff:
+    raise AssertionError("restricted handoff must not invoke PAM through runuser")
 require(handoff, "wait-prelogin", "pre-login readiness gate")
 require(handoff, "attempt & (attempt - 1)", "rate-limited status logging")
 if "PAM_TYPE" in handoff or "PAM_USER" in handoff:
@@ -118,7 +120,7 @@ if "source \"$settings_file\"" in prelogin_sync or ". \"$settings_file\"" in pre
     raise AssertionError("root helper must parse rather than source administrator settings")
 require(prelogin_config, "capture = kms", "pre-login KMS policy")
 require(prelogin_config, "virtual_display_layout = exclusive", "exclusive greeter layout")
-require(prelogin_apps, "/usr/libexec/vibeshine/vibepollo-prelogin-sync activate", "greeter activation command")
+require(prelogin_apps, "/usr/libexec/vibeshine/vibepollo-machine-host activate", "greeter activation command")
 
 require(packaging, "add_library(pam_vibepollo_session MODULE", "native PAM build")
 require(packaging, "install(TARGETS pam_vibepollo_session", "native PAM install")
@@ -129,10 +131,9 @@ require(packaging, "util-linux", "native flock/runuser dependency")
 require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"',
         "versioned kernel source fingerprint")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-handoff"', "native install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"', "native install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin-sync"', "pre-login helper install")
-require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin.service"', "pre-login unit install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-machine-host"', "machine-host helper install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo.service"', "machine-host unit install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"', "pre-login profile install")
 
 require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")