Nonary/vibeshine

fix(linux): preserve Steam stream frame limits
Delegate direct Steam launches to a capability-free desktop helper so the machine-scoped host can apply per-stream policy without parsing user paths.

Pass a canonical fixed-point policy through the broker, resolve UMU runtimes beside the game, and write a symlink-safe user runtime handoff so 120 FPS streams receive the 116 FPS limit.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol X-High

src/platform/linux/mangohud_state.h
@@ -5,16 +5,47 @@
 #pragma once
 
 #include <chrono>
+#include <cerrno>
 #include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <string>
 #include <string_view>
 #include <system_error>
+#include <utility>
 
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 namespace platf::mangohud {
+
+  class owned_fd final {
+  public:
+    owned_fd() = default;
+    explicit owned_fd(int value): value_ {value} {}
+    owned_fd(const owned_fd &) = delete;
+    owned_fd &operator=(const owned_fd &) = delete;
+    owned_fd(owned_fd &&other) noexcept: value_ {other.value_} {
+      other.value_ = -1;
+    }
+    owned_fd &operator=(owned_fd &&other) noexcept {
+      if (this != &other) {
+        if (value_ >= 0) close(value_);
+        value_ = other.value_;
+        other.value_ = -1;
+      }
+      return *this;
+    }
+    ~owned_fd() {
+      if (value_ >= 0) close(value_);
+    }
+    [[nodiscard]] int get() const { return value_; }
+    [[nodiscard]] explicit operator bool() const { return value_ >= 0; }
+
+  private:
+    int value_;
+  };
 
   inline std::filesystem::path state_directory() {
     if (const char *override_dir = std::getenv("VIBESHINE_MANGOHUD_STATE_DIR");
@@ -51,6 +82,63 @@ namespace platf::mangohud {
       return {};
     }
     return directory / (std::string(app_id) + ".state");
+  }
+
+  inline bool private_directory(int descriptor, uid_t owner) {
+    struct stat attributes {};
+    return descriptor >= 0 && fstat(descriptor, &attributes) == 0 &&
+           S_ISDIR(attributes.st_mode) && attributes.st_uid == owner &&
+           (attributes.st_mode & 0777) == 0700;
+  }
+
+  inline owned_fd open_private_directory_at(
+    int parent,
+    const char *name,
+    uid_t owner,
+    bool create
+  ) {
+    int descriptor = openat(
+      parent,
+      name,
+      O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW
+    );
+    if (descriptor < 0 && create && errno == ENOENT &&
+        mkdirat(parent, name, 0700) == 0) {
+      descriptor = openat(
+        parent,
+        name,
+        O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW
+      );
+    }
+    owned_fd result {descriptor};
+    return private_directory(result.get(), owner) ? std::move(result) : owned_fd {};
+  }
+
+  inline std::pair<owned_fd, owned_fd> open_runtime_state_directories(bool create) {
+    const char *runtime = std::getenv("XDG_RUNTIME_DIR");
+    if (!runtime || runtime[0] != '/') {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    owned_fd runtime_fd {
+      open(runtime, O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW)
+    };
+    const uid_t owner = getuid();
+    if (!private_directory(runtime_fd.get(), owner)) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    auto vibeshine_fd = open_private_directory_at(
+      runtime_fd.get(), "vibeshine", owner, create
+    );
+    if (!vibeshine_fd) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    auto state_fd = open_private_directory_at(
+      vibeshine_fd.get(), "mangohud", owner, create
+    );
+    if (!state_fd) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    return {std::move(runtime_fd), std::move(state_fd)};
   }
 
   inline std::string serialize_state(
@@ -139,6 +227,95 @@ namespace platf::mangohud {
       return {};
     }
     return path;
+  }
+
+  inline std::filesystem::path write_runtime_state(
+    std::string_view app_id,
+    std::string_view provider,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph,
+    std::string_view limiter_method = "late"
+  ) {
+    if (!valid_steam_app_id(app_id)) {
+      return {};
+    }
+    auto [runtime_fd, state_fd] = open_runtime_state_directories(true);
+    if (!runtime_fd || !state_fd) {
+      return {};
+    }
+    const auto contents = serialize_state(
+      provider,
+      limit,
+      preset,
+      always_show_graph,
+      limiter_method,
+      std::chrono::system_clock::now() + std::chrono::hours(1)
+    );
+    if (contents.empty() || contents.size() > 4096) {
+      return {};
+    }
+
+    const std::string final_name = std::string(app_id) + ".state";
+    std::string temporary_name;
+    owned_fd temporary;
+    for (unsigned int attempt = 0; attempt < 32; ++attempt) {
+      temporary_name = final_name + ".tmp-" + std::to_string(getpid()) +
+                       "-" + std::to_string(attempt);
+      temporary = owned_fd {openat(
+        state_fd.get(),
+        temporary_name.c_str(),
+        O_WRONLY | O_CREAT | O_EXCL | O_CLOEXEC | O_NOFOLLOW,
+        0600
+      )};
+      if (temporary || errno != EEXIST) {
+        break;
+      }
+    }
+    if (!temporary) {
+      return {};
+    }
+
+    std::size_t written = 0;
+    while (written < contents.size()) {
+      const auto result = write(
+        temporary.get(), contents.data() + written, contents.size() - written
+      );
+      if (result > 0) {
+        written += static_cast<std::size_t>(result);
+      } else if (result < 0 && errno == EINTR) {
+        continue;
+      } else {
+        (void) unlinkat(state_fd.get(), temporary_name.c_str(), 0);
+        return {};
+      }
+    }
+    struct stat attributes {};
+    if (fchmod(temporary.get(), 0600) != 0 ||
+        fstat(temporary.get(), &attributes) != 0 ||
+        !S_ISREG(attributes.st_mode) || attributes.st_uid != getuid() ||
+        (attributes.st_mode & 0777) != 0600 || fsync(temporary.get()) != 0 ||
+        renameat(state_fd.get(), temporary_name.c_str(),
+                 state_fd.get(), final_name.c_str()) != 0) {
+      (void) unlinkat(state_fd.get(), temporary_name.c_str(), 0);
+      return {};
+    }
+    (void) fsync(state_fd.get());
+    const char *runtime = std::getenv("XDG_RUNTIME_DIR");
+    return std::filesystem::path(runtime) / "vibeshine" / "mangohud" /
+           final_name;
+  }
+
+  inline void remove_runtime_state(std::string_view app_id) {
+    if (!valid_steam_app_id(app_id)) {
+      return;
+    }
+    auto [runtime_fd, state_fd] = open_runtime_state_directories(false);
+    if (!runtime_fd || !state_fd) {
+      return;
+    }
+    const std::string final_name = std::string(app_id) + ".state";
+    (void) unlinkat(state_fd.get(), final_name.c_str(), 0);
   }
 
   inline void remove_state(std::string_view app_id) {

tests/test_steam_integration.cpp
@@ -329,13 +329,15 @@ TEST(SteamDiscovery, ResolvesDirectLaunchMetadataOptionsAndExistingProton) {
   EXPECT_EQ(games[0].proton_path, proton);
   EXPECT_EQ(games[0].proton_runtime_path, runtime);
   EXPECT_EQ(games[0].steam_client_path, base);
-  EXPECT_NE(launch_command(games[0]).find("mangohud vibeshine-mangohud --appid 42 -- env"), std::string::npos);
+  EXPECT_NE(launch_command(games[0]).find("mangohud /usr/bin/vibeshine-mangohud --appid 42 -- env"), std::string::npos);
 
   // Custom compatibility tools live outside steamapps. Modern config_info
   // records the Steam client root explicitly, which must be used to resolve
   // the required pressure-vessel runtime.
-  const auto external_proton = base / "compatibilitytools.d/ExternalProton";
+  const auto client = base / "client";
+  const auto external_proton = client / "compatibilitytools.d/ExternalProton";
   fs::create_directories(external_proton / "files/share/fonts", ec);
+  fs::create_directories(client / "steamapps", ec);
   {
     std::ofstream output(external_proton / "toolmanifest.vdf");
     output << R"VDF("manifest" { "require_tool_appid" "99" })VDF";
@@ -345,12 +347,12 @@ TEST(SteamDiscovery, ResolvesDirectLaunchMetadataOptionsAndExistingProton) {
     output << "External Proton\n"
            << (external_proton / "files/share/fonts").string() << "/\n"
            << (external_proton / "files/lib").string() << "/\n"
-           << base.string() << "\n";
+           << client.string() << "\n";
   }
   const auto custom_tool_games = discover({base});
   ASSERT_GE(custom_tool_games.size(), 1U);
   EXPECT_EQ(custom_tool_games[0].proton_path, external_proton);
-  EXPECT_EQ(custom_tool_games[0].steam_client_path, base);
+  EXPECT_EQ(custom_tool_games[0].steam_client_path, client);
   EXPECT_EQ(custom_tool_games[0].proton_runtime_path, runtime);
   EXPECT_NE(launch_command(custom_tool_games[0]).find("ExternalProton/proton"), std::string::npos);
 
@@ -391,6 +393,41 @@ TEST(SteamLaunch, StreamOwnedEnvironmentFeaturesRequireDirectLaunch) {
 }
 
 #ifdef __linux__
+TEST(SteamLaunch, MachineSessionLaunchUsesCanonicalSemanticArguments) {
+  session_launch_policy_t policy {
+    .provider = "mangohud-proton",
+    .limit_millihz = 116000,
+    .preset = "3",
+    .always_show_graph = true,
+    .limiter_method = "late",
+    .smooth_motion = true,
+    .smooth_motion_graphics_queue = true,
+  };
+  const auto command = session_launch_command(1182900, policy);
+  EXPECT_EQ(
+    command,
+    "/usr/libexec/vibeshine/vibeshine-session-exec steam-direct "
+    "1182900 mangohud-proton 116000 3 1 late 1 1"
+  );
+  const auto arguments = session_launch_arguments(command);
+  ASSERT_TRUE(arguments);
+  EXPECT_EQ(
+    *arguments,
+    (std::vector<std::string> {
+      "steam-direct", "1182900", "mangohud-proton", "116000", "3",
+      "1", "late", "1", "1"
+    })
+  );
+
+  policy.provider = "disabled";
+  EXPECT_TRUE(session_launch_command(1182900, policy).empty());
+  EXPECT_FALSE(session_launch_arguments(command + " trailing"));
+  EXPECT_FALSE(session_launch_arguments(
+    "/usr/libexec/vibeshine/vibeshine-session-exec steam-direct "
+    "1182900 proton 116000 custom 1 late 0 0"
+  ));
+}
+
 TEST(SteamLaunch, DirectLaunchPlacesVibeshineInsideInheritedSteamOptions) {
   game_t game;
   game.app_id = 1182900;
@@ -407,7 +444,7 @@ TEST(SteamLaunch, DirectLaunchPlacesVibeshineInsideInheritedSteamOptions) {
 
   const auto command = launch_command(game);
   EXPECT_TRUE(command.starts_with("/bin/sh -c 'PROTON_DLSS_UPGRADE=3.7 mangohud "));
-  EXPECT_NE(command.find("vibeshine-mangohud --appid 1182900 -- env"), std::string::npos);
+  EXPECT_NE(command.find("/usr/bin/vibeshine-mangohud --appid 1182900 -- env"), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_APP_ID=1182900"), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_SHADER_PATH="), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_MEDIA_PATH="), std::string::npos);
@@ -429,7 +466,7 @@ TEST(SteamLaunch, DirectNativeLaunchTreatsOptionsWithoutPlaceholderAsArguments) 
 
   EXPECT_EQ(
     launch_command(game),
-    "/bin/sh -c 'vibeshine-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
+    "/bin/sh -c '/usr/bin/vibeshine-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
     "'\\''/games/Spacewar/spacewar'\\'' -default -user-option'"
   );
 }

tests/test_steam_sync.cpp
@@ -60,7 +60,7 @@ TEST(SteamSync, PublishesResolvedDirectCommandAndWorkingDirectory) {
   ASSERT_TRUE(platf::steam::sync::policy::reconcile(root, {game}));
   ASSERT_EQ(root["apps"].size(), 1U);
   EXPECT_EQ(root["apps"][0]["cmd"],
-            "/bin/sh -c 'mangohud vibeshine-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
+            "/bin/sh -c 'mangohud /usr/bin/vibeshine-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
             "'\\''/games/Direct/direct'\\'''");
   EXPECT_EQ(root["apps"][0]["working-dir"], "/games/Direct");
 }

tests/unit/platform/linux/test_machine_service_contract.py
@@ -27,6 +27,7 @@ host = (linux / "vibeshine-machine-host").read_text()
 host_unit = (linux / "vibeshine.service").read_text()
 launcher = (linux / "vibeshine-session-exec.c").read_text()
 broker = (linux / "vibeshine-session-broker.c").read_text()
+steam_launcher = (linux / "vibeshine-steam-launch.cpp").read_text()
 session_execution = launcher + "\n" + broker
 private_display = (root / "src/platform/linux/private_display.cpp").read_text()
 audio = (root / "src/platform/linux/audio.cpp").read_text()
@@ -546,6 +547,7 @@ rpm_entries = (
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-session-exec",
     "%attr(0700,root,root) %caps(cap_kill,cap_setgid,cap_setuid+p) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-session-broker",
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-app-supervisor",
+    "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-steam-launch",
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-kwin-session-environment",
     "%attr(0750,root,vibeshine) %caps(cap_sys_admin,cap_sys_nice+p) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-host",
 )
@@ -557,6 +559,7 @@ for capability_free_path in (
     "${CMAKE_INSTALL_FULL_BINDIR}/vibeshine",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-session-exec",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-app-supervisor",
+    "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-steam-launch",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibeshine-kwin-session-environment",
 ):
     filelist_line = next(line for line in rpm_filelist.splitlines() if capability_free_path in line)
@@ -736,6 +739,7 @@ for unsafe_stop in (
 
 require(rpm, "%{_bindir}/vibeshine-mangohud", "RPM deterministic manifest")
 require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibeshine-app-supervisor", "RPM deterministic manifest")
+require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibeshine-steam-launch", "RPM deterministic manifest")
 require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibeshine-kwin-session-environment", "RPM deterministic manifest")
 require(rpm, "%attr(0750,root,vibeshine) %caps(cap_sys_admin,cap_sys_nice+p) %{_prefix}/libexec/vibeshine/vibeshine-host", "RPM deterministic manifest")
 require(rpm, "%attr(0700,root,root) %caps(cap_kill,cap_setgid,cap_setuid+p) %{_prefix}/libexec/vibeshine/vibeshine-session-broker", "RPM deterministic manifest")

Nonary/Vibepollo

(cherry picked from commit 4e446636589276b64e508048147f753362c06218)

src/platform/linux/mangohud_state.h
@@ -5,16 +5,47 @@
 #pragma once
 
 #include <chrono>
+#include <cerrno>
 #include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <string>
 #include <string_view>
 #include <system_error>
+#include <utility>
 
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 namespace platf::mangohud {
+
+  class owned_fd final {
+  public:
+    owned_fd() = default;
+    explicit owned_fd(int value): value_ {value} {}
+    owned_fd(const owned_fd &) = delete;
+    owned_fd &operator=(const owned_fd &) = delete;
+    owned_fd(owned_fd &&other) noexcept: value_ {other.value_} {
+      other.value_ = -1;
+    }
+    owned_fd &operator=(owned_fd &&other) noexcept {
+      if (this != &other) {
+        if (value_ >= 0) close(value_);
+        value_ = other.value_;
+        other.value_ = -1;
+      }
+      return *this;
+    }
+    ~owned_fd() {
+      if (value_ >= 0) close(value_);
+    }
+    [[nodiscard]] int get() const { return value_; }
+    [[nodiscard]] explicit operator bool() const { return value_ >= 0; }
+
+  private:
+    int value_;
+  };
 
   inline std::filesystem::path state_directory() {
     if (const char *override_dir = std::getenv("VIBEPOLLO_MANGOHUD_STATE_DIR");
@@ -51,6 +82,63 @@ namespace platf::mangohud {
       return {};
     }
     return directory / (std::string(app_id) + ".state");
+  }
+
+  inline bool private_directory(int descriptor, uid_t owner) {
+    struct stat attributes {};
+    return descriptor >= 0 && fstat(descriptor, &attributes) == 0 &&
+           S_ISDIR(attributes.st_mode) && attributes.st_uid == owner &&
+           (attributes.st_mode & 0777) == 0700;
+  }
+
+  inline owned_fd open_private_directory_at(
+    int parent,
+    const char *name,
+    uid_t owner,
+    bool create
+  ) {
+    int descriptor = openat(
+      parent,
+      name,
+      O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW
+    );
+    if (descriptor < 0 && create && errno == ENOENT &&
+        mkdirat(parent, name, 0700) == 0) {
+      descriptor = openat(
+        parent,
+        name,
+        O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW
+      );
+    }
+    owned_fd result {descriptor};
+    return private_directory(result.get(), owner) ? std::move(result) : owned_fd {};
+  }
+
+  inline std::pair<owned_fd, owned_fd> open_runtime_state_directories(bool create) {
+    const char *runtime = std::getenv("XDG_RUNTIME_DIR");
+    if (!runtime || runtime[0] != '/') {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    owned_fd runtime_fd {
+      open(runtime, O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW)
+    };
+    const uid_t owner = getuid();
+    if (!private_directory(runtime_fd.get(), owner)) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    auto vibepollo_fd = open_private_directory_at(
+      runtime_fd.get(), "vibepollo", owner, create
+    );
+    if (!vibepollo_fd) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    auto state_fd = open_private_directory_at(
+      vibepollo_fd.get(), "mangohud", owner, create
+    );
+    if (!state_fd) {
+      return std::pair<owned_fd, owned_fd> {};
+    }
+    return {std::move(runtime_fd), std::move(state_fd)};
   }
 
   inline std::string serialize_state(
@@ -139,6 +227,95 @@ namespace platf::mangohud {
       return {};
     }
     return path;
+  }
+
+  inline std::filesystem::path write_runtime_state(
+    std::string_view app_id,
+    std::string_view provider,
+    std::string_view limit,
+    std::string_view preset,
+    bool always_show_graph,
+    std::string_view limiter_method = "late"
+  ) {
+    if (!valid_steam_app_id(app_id)) {
+      return {};
+    }
+    auto [runtime_fd, state_fd] = open_runtime_state_directories(true);
+    if (!runtime_fd || !state_fd) {
+      return {};
+    }
+    const auto contents = serialize_state(
+      provider,
+      limit,
+      preset,
+      always_show_graph,
+      limiter_method,
+      std::chrono::system_clock::now() + std::chrono::hours(1)
+    );
+    if (contents.empty() || contents.size() > 4096) {
+      return {};
+    }
+
+    const std::string final_name = std::string(app_id) + ".state";
+    std::string temporary_name;
+    owned_fd temporary;
+    for (unsigned int attempt = 0; attempt < 32; ++attempt) {
+      temporary_name = final_name + ".tmp-" + std::to_string(getpid()) +
+                       "-" + std::to_string(attempt);
+      temporary = owned_fd {openat(
+        state_fd.get(),
+        temporary_name.c_str(),
+        O_WRONLY | O_CREAT | O_EXCL | O_CLOEXEC | O_NOFOLLOW,
+        0600
+      )};
+      if (temporary || errno != EEXIST) {
+        break;
+      }
+    }
+    if (!temporary) {
+      return {};
+    }
+
+    std::size_t written = 0;
+    while (written < contents.size()) {
+      const auto result = write(
+        temporary.get(), contents.data() + written, contents.size() - written
+      );
+      if (result > 0) {
+        written += static_cast<std::size_t>(result);
+      } else if (result < 0 && errno == EINTR) {
+        continue;
+      } else {
+        (void) unlinkat(state_fd.get(), temporary_name.c_str(), 0);
+        return {};
+      }
+    }
+    struct stat attributes {};
+    if (fchmod(temporary.get(), 0600) != 0 ||
+        fstat(temporary.get(), &attributes) != 0 ||
+        !S_ISREG(attributes.st_mode) || attributes.st_uid != getuid() ||
+        (attributes.st_mode & 0777) != 0600 || fsync(temporary.get()) != 0 ||
+        renameat(state_fd.get(), temporary_name.c_str(),
+                 state_fd.get(), final_name.c_str()) != 0) {
+      (void) unlinkat(state_fd.get(), temporary_name.c_str(), 0);
+      return {};
+    }
+    (void) fsync(state_fd.get());
+    const char *runtime = std::getenv("XDG_RUNTIME_DIR");
+    return std::filesystem::path(runtime) / "vibepollo" / "mangohud" /
+           final_name;
+  }
+
+  inline void remove_runtime_state(std::string_view app_id) {
+    if (!valid_steam_app_id(app_id)) {
+      return;
+    }
+    auto [runtime_fd, state_fd] = open_runtime_state_directories(false);
+    if (!runtime_fd || !state_fd) {
+      return;
+    }
+    const std::string final_name = std::string(app_id) + ".state";
+    (void) unlinkat(state_fd.get(), final_name.c_str(), 0);
   }
 
   inline void remove_state(std::string_view app_id) {

tests/test_steam_integration.cpp
@@ -329,13 +329,15 @@ TEST(SteamDiscovery, ResolvesDirectLaunchMetadataOptionsAndExistingProton) {
   EXPECT_EQ(games[0].proton_path, proton);
   EXPECT_EQ(games[0].proton_runtime_path, runtime);
   EXPECT_EQ(games[0].steam_client_path, base);
-  EXPECT_NE(launch_command(games[0]).find("mangohud vibepollo-mangohud --appid 42 -- env"), std::string::npos);
+  EXPECT_NE(launch_command(games[0]).find("mangohud /usr/bin/vibepollo-mangohud --appid 42 -- env"), std::string::npos);
 
   // Custom compatibility tools live outside steamapps. Modern config_info
   // records the Steam client root explicitly, which must be used to resolve
   // the required pressure-vessel runtime.
-  const auto external_proton = base / "compatibilitytools.d/ExternalProton";
+  const auto client = base / "client";
+  const auto external_proton = client / "compatibilitytools.d/ExternalProton";
   fs::create_directories(external_proton / "files/share/fonts", ec);
+  fs::create_directories(client / "steamapps", ec);
   {
     std::ofstream output(external_proton / "toolmanifest.vdf");
     output << R"VDF("manifest" { "require_tool_appid" "99" })VDF";
@@ -345,12 +347,12 @@ TEST(SteamDiscovery, ResolvesDirectLaunchMetadataOptionsAndExistingProton) {
     output << "External Proton\n"
            << (external_proton / "files/share/fonts").string() << "/\n"
            << (external_proton / "files/lib").string() << "/\n"
-           << base.string() << "\n";
+           << client.string() << "\n";
   }
   const auto custom_tool_games = discover({base});
   ASSERT_GE(custom_tool_games.size(), 1U);
   EXPECT_EQ(custom_tool_games[0].proton_path, external_proton);
-  EXPECT_EQ(custom_tool_games[0].steam_client_path, base);
+  EXPECT_EQ(custom_tool_games[0].steam_client_path, client);
   EXPECT_EQ(custom_tool_games[0].proton_runtime_path, runtime);
   EXPECT_NE(launch_command(custom_tool_games[0]).find("ExternalProton/proton"), std::string::npos);
 
@@ -391,6 +393,41 @@ TEST(SteamLaunch, StreamOwnedEnvironmentFeaturesRequireDirectLaunch) {
 }
 
 #ifdef __linux__
+TEST(SteamLaunch, MachineSessionLaunchUsesCanonicalSemanticArguments) {
+  session_launch_policy_t policy {
+    .provider = "mangohud-proton",
+    .limit_millihz = 116000,
+    .preset = "3",
+    .always_show_graph = true,
+    .limiter_method = "late",
+    .smooth_motion = true,
+    .smooth_motion_graphics_queue = true,
+  };
+  const auto command = session_launch_command(1182900, policy);
+  EXPECT_EQ(
+    command,
+    "/usr/libexec/vibeshine/vibepollo-session-exec steam-direct "
+    "1182900 mangohud-proton 116000 3 1 late 1 1"
+  );
+  const auto arguments = session_launch_arguments(command);
+  ASSERT_TRUE(arguments);
+  EXPECT_EQ(
+    *arguments,
+    (std::vector<std::string> {
+      "steam-direct", "1182900", "mangohud-proton", "116000", "3",
+      "1", "late", "1", "1"
+    })
+  );
+
+  policy.provider = "disabled";
+  EXPECT_TRUE(session_launch_command(1182900, policy).empty());
+  EXPECT_FALSE(session_launch_arguments(command + " trailing"));
+  EXPECT_FALSE(session_launch_arguments(
+    "/usr/libexec/vibeshine/vibepollo-session-exec steam-direct "
+    "1182900 proton 116000 custom 1 late 0 0"
+  ));
+}
+
 TEST(SteamLaunch, DirectLaunchPlacesVibepolloInsideInheritedSteamOptions) {
   game_t game;
   game.app_id = 1182900;
@@ -407,7 +444,7 @@ TEST(SteamLaunch, DirectLaunchPlacesVibepolloInsideInheritedSteamOptions) {
 
   const auto command = launch_command(game);
   EXPECT_TRUE(command.starts_with("/bin/sh -c 'PROTON_DLSS_UPGRADE=3.7 mangohud "));
-  EXPECT_NE(command.find("vibepollo-mangohud --appid 1182900 -- env"), std::string::npos);
+  EXPECT_NE(command.find("/usr/bin/vibepollo-mangohud --appid 1182900 -- env"), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_APP_ID=1182900"), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_SHADER_PATH="), std::string::npos);
   EXPECT_NE(command.find("STEAM_COMPAT_MEDIA_PATH="), std::string::npos);
@@ -429,7 +466,7 @@ TEST(SteamLaunch, DirectNativeLaunchTreatsOptionsWithoutPlaceholderAsArguments) 
 
   EXPECT_EQ(
     launch_command(game),
-    "/bin/sh -c 'vibepollo-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
+    "/bin/sh -c '/usr/bin/vibepollo-mangohud --appid 480 -- env SteamAppId=480 SteamGameId=480 "
     "'\\''/games/Spacewar/spacewar'\\'' -default -user-option'"
   );
 }

tests/test_steam_sync.cpp
@@ -60,7 +60,7 @@ TEST(SteamSync, PublishesResolvedDirectCommandAndWorkingDirectory) {
   ASSERT_TRUE(platf::steam::sync::policy::reconcile(root, {game}));
   ASSERT_EQ(root["apps"].size(), 1U);
   EXPECT_EQ(root["apps"][0]["cmd"],
-            "/bin/sh -c 'mangohud vibepollo-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
+            "/bin/sh -c 'mangohud /usr/bin/vibepollo-mangohud --appid 42 -- env SteamAppId=42 SteamGameId=42 "
             "'\\''/games/Direct/direct'\\'''");
   EXPECT_EQ(root["apps"][0]["working-dir"], "/games/Direct");
 }

tests/unit/platform/linux/test_machine_service_contract.py
@@ -27,6 +27,7 @@ host = (linux / "vibepollo-machine-host").read_text()
 host_unit = (linux / "vibepollo.service").read_text()
 launcher = (linux / "vibepollo-session-exec.c").read_text()
 broker = (linux / "vibepollo-session-broker.c").read_text()
+steam_launcher = (linux / "vibepollo-steam-launch.cpp").read_text()
 session_execution = launcher + "\n" + broker
 private_display = (root / "src/platform/linux/private_display.cpp").read_text()
 audio = (root / "src/platform/linux/audio.cpp").read_text()
@@ -550,6 +551,7 @@ rpm_entries = (
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-session-exec",
     "%attr(0700,root,root) %caps(cap_kill,cap_setgid,cap_setuid+p) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-session-broker",
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-app-supervisor",
+    "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-steam-launch",
     "%attr(0755,root,root) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-kwin-session-environment",
     "%attr(0750,root,vibepollo) %caps(cap_sys_admin,cap_sys_nice+p) ${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-host",
 )
@@ -561,6 +563,7 @@ for capability_free_path in (
     "${CMAKE_INSTALL_FULL_BINDIR}/vibepollo",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-session-exec",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-app-supervisor",
+    "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-steam-launch",
     "${VIBESHINE_PRIVILEGED_LIBEXEC_INSTALL_DIR}/vibepollo-kwin-session-environment",
 ):
     filelist_line = next(line for line in rpm_filelist.splitlines() if capability_free_path in line)
@@ -740,6 +743,7 @@ for unsafe_stop in (
 
 require(rpm, "%{_bindir}/vibepollo-mangohud", "RPM deterministic manifest")
 require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibepollo-app-supervisor", "RPM deterministic manifest")
+require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibepollo-steam-launch", "RPM deterministic manifest")
 require(rpm, "%attr(0755,root,root) %{_prefix}/libexec/vibeshine/vibepollo-kwin-session-environment", "RPM deterministic manifest")
 require(rpm, "%attr(0750,root,vibepollo) %caps(cap_sys_admin,cap_sys_nice+p) %{_prefix}/libexec/vibeshine/vibepollo-host", "RPM deterministic manifest")
 require(rpm, "%attr(0700,root,root) %caps(cap_kill,cap_setgid,cap_setuid+p) %{_prefix}/libexec/vibeshine/vibepollo-session-broker", "RPM deterministic manifest")