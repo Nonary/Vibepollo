Nonary/vibeshine

perf(display): keep virtual capture on the GPU
Remove compatibility paths that could pin managed outputs to 60 Hz or fall back to CPU uploads. Require direct DMA-BUF import for Vibeshine displays, preserve client mode requests, and harden disconnect, topology restore, and CUDA teardown. Package the scoped KWin bridge and custom-driver-only lifecycle.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol X-High

cmake/dependencies/common.cmake
@@ -21,6 +21,8 @@ add_subdirectory("${CMAKE_SOURCE_DIR}/third-party/libdisplaydevice")
 
 # libvirtualdisplay
 include(dependencies/libvirtualdisplay_source)
+set(BUILD_VIBESHINE_KWIN_GPU_BRIDGE ON CACHE BOOL
+        "Build the Vibeshine KWin GPU-association bridge" FORCE)
 add_subdirectory("${SUNSHINE_LIBVIRTUALDISPLAY_SOURCE_DIR}" "${CMAKE_BINARY_DIR}/third-party/libvirtualdisplay")
 
 # common dependencies

Nonary/Vibepollo

(cherry picked from commit 862e2aa0f6ba393c7a652adbf95180521eb64a29)

cmake/dependencies/common.cmake
@@ -21,6 +21,8 @@ add_subdirectory("${CMAKE_SOURCE_DIR}/third-party/libdisplaydevice")
 
 # libvirtualdisplay
 include(dependencies/libvirtualdisplay_source)
+set(BUILD_VIBESHINE_KWIN_GPU_BRIDGE ON CACHE BOOL
+        "Build the Vibepollo KWin GPU-association bridge" FORCE)
 add_subdirectory("${SUNSHINE_LIBVIRTUALDISPLAY_SOURCE_DIR}" "${CMAKE_BINARY_DIR}/third-party/libvirtualdisplay")
 
 # common dependencies