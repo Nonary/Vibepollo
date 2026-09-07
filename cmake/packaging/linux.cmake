Nonary/vibeshine

feat(linux): package the pre-login streaming host
Native packages previously installed only the handoff consumer and PAM module while relying on an unowned service, sync helper, configuration, application manifest, and duplicate /usr/local binary. Ship the complete Plasma Login Manager producer around /usr/bin/vibeshine, migrate a least-privilege paired-client allowlist, discover the greeter session dynamically, and activate PAM only after provisioning succeeds.

Generated with [Codex](https://openai.com/codex/)

Model: GPT 5.6-Sol High

cmake/packaging/linux.cmake
@@ -44,6 +44,7 @@ else()
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin-sync"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-handoff"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-ready"
                 "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-install"
@@ -55,6 +56,10 @@ else()
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
+        install(FILES
+                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/vibeshine.conf"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
+                DESTINATION "${SUNSHINE_ASSETS_DIR}/prelogin")
         install(DIRECTORY "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}"
                 FILES_MATCHING
@@ -89,6 +94,7 @@ else()
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-setup.service"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control.socket"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control@.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
@@ -146,6 +152,7 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             libgbm1, \
             libevdev2, \
             iproute2, \
+            jq, \
             kmod, \
             libkscreen-bin | libkf5screen-bin, \
             make, \
@@ -168,6 +175,7 @@ set(CPACK_RPM_PACKAGE_REQUIRES "\
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \
             iproute, \
+            jq, \
             kmod, \
             libkscreen, \
             make, \

packaging/linux/copr/Sunshine.spec
@@ -141,6 +141,7 @@ Requires: miniupnpc >= 2.2.4
 Requires: which >= 2.21
 Requires: kmod
 Requires: iproute
+Requires: jq
 Requires: pam
 Requires: socat
 Requires: util-linux
@@ -361,12 +362,25 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       echo "warning: Vibeshine DRM installation failed; managed virtual displays are unavailable."
     fi
   fi
+  if %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync configure-auto; then
+    if %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync install-pam; then
+      systemctl daemon-reload || true
+      systemctl enable vibeshine-prelogin.service || \
+        echo "warning: could not enable Vibeshine pre-login streaming."
+    else
+      echo "warning: could not install the Plasma Login Manager handoff hook."
+    fi
+  else
+    echo "warning: configure a paired-client allowlist before enabling Vibeshine pre-login streaming."
+  fi
 else
   echo "rpm-ostree environment detected, skipping post install steps. Restart to apply the changes."
 fi
 
 %preun
 if [ "$1" -eq 0 ]; then
+  systemctl disable --now vibeshine-prelogin.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
   %{_prefix}/libexec/vibeshine/vibeshine-drm-install remove || \
     echo "warning: could not remove the Vibeshine HDR DRM module cleanly."
@@ -381,6 +395,7 @@ fi
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-peercred
 %{_prefix}/libexec/vibeshine/vibeshine-session-handoff
 %{_prefix}/libexec/vibeshine/vibeshine-session-ready
+%{_prefix}/libexec/vibeshine/vibeshine-prelogin-sync
 %{_prefix}/libexec/vibeshine/kwin-preload/kwin_wayland
 %{_prefix}/lib/vibeshine/libvibeshine-kwin-gpu.so
 %{_libdir}/security/pam_vibeshine_session.so
@@ -401,6 +416,7 @@ fi
 %{_unitdir}/vibeshine-vkms-control.socket
 %{_unitdir}/vibeshine-vkms-control@.service
 %{_unitdir}/vibeshine-vkms.service
+%{_unitdir}/vibeshine-prelogin.service
 %{_unitdir}/vibeshine-session-restore@.service
 
 # Udev rules

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -15,10 +15,15 @@ pam_module = (root / "packaging/linux/pam_vibeshine_session.c").read_text()
 handoff = (root / "packaging/linux/vibeshine-session-handoff").read_text()
 ready = (root / "packaging/linux/vibeshine-session-ready").read_text()
 restore_unit = (root / "packaging/linux/vibeshine-session-restore@.service").read_text()
+prelogin_unit = (root / "packaging/linux/vibeshine-prelogin.service").read_text()
+prelogin_sync = (root / "packaging/linux/vibeshine-prelogin-sync").read_text()
+prelogin_config = (root / "packaging/linux/prelogin/vibeshine.conf").read_text()
+prelogin_apps = (root / "packaging/linux/prelogin/apps.json").read_text()
 packaging = (root / "cmake/packaging/linux.cmake").read_text()
 package_configuration = (root / "cmake/prep/special_package_configuration.cmake").read_text()
 rpm_spec = (root / "packaging/linux/copr/Sunshine.spec").read_text()
 arch_package = (root / "packaging/linux/Arch/PKGBUILD").read_text()
+arch_install = (root / "packaging/linux/Arch/vibeshine.install").read_text()
 
 require(unit, "StartLimitIntervalSec=0", "user service")
 require(unit, "PartOf=graphical-session.target", "user service")
@@ -82,6 +87,28 @@ require(restore_unit, "Type=oneshot", "restore supervisor unit")
 require(restore_unit, "vibeshine-session-handoff restore %i", "restore supervisor unit")
 require(restore_unit, "TimeoutStartSec=infinity", "restore supervisor unit")
 
+require(prelogin_unit, "ConditionPathExists=/etc/vibeshine/prelogin.conf", "pre-login service")
+require(prelogin_unit, "User=plasmalogin", "pre-login service")
+require(prelogin_unit, "vibeshine-session-handoff wait-prelogin", "pre-login service")
+require(prelogin_unit, "vibeshine-prelogin-sync prepare", "pre-login service")
+require(prelogin_unit, "vibeshine-prelogin-sync run", "pre-login service")
+require(prelogin_unit, "WantedBy=graphical.target", "pre-login service")
+if "/usr/local/" in prelogin_unit:
+    raise AssertionError("pre-login service must not depend on host-only /usr/local files")
+
+require(prelogin_sync, "settings must be root:root mode 0600", "pre-login settings boundary")
+require(prelogin_sync, "allowed_client_uuid", "paired-client allowlist")
+require(prelogin_sync, "configure-auto", "legacy provisioning migration")
+require(prelogin_sync, "plasma-login-wayland.target", "greeter compositor startup")
+require(prelogin_sync, "discover_wayland_display", "dynamic greeter display discovery")
+require(prelogin_sync, 'exec /usr/bin/env "WAYLAND_DISPLAY=$display" /usr/bin/vibeshine', "packaged executable")
+require(prelogin_sync, "pam_vibeshine_session.so", "PAM hook management")
+if "source \"$settings_file\"" in prelogin_sync or ". \"$settings_file\"" in prelogin_sync:
+    raise AssertionError("root helper must parse rather than source administrator settings")
+require(prelogin_config, "capture = kms", "pre-login KMS policy")
+require(prelogin_config, "virtual_display_layout = exclusive", "exclusive greeter layout")
+require(prelogin_apps, "/usr/libexec/vibeshine/vibeshine-prelogin-sync activate", "greeter activation command")
+
 require(packaging, "add_library(pam_vibeshine_session MODULE", "native PAM build")
 require(packaging, "install(TARGETS pam_vibeshine_session", "native PAM install")
 require(packaging, "libpam0g", "Debian PAM dependency")
@@ -93,11 +120,16 @@ require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"'
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-handoff"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-session-restore@.service"', "native install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin-sync"', "pre-login helper install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibeshine-prelogin.service"', "pre-login unit install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"', "pre-login profile install")
 
 require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")
 require(rpm_spec, "Requires: pam", "RPM PAM runtime dependency")
 require(rpm_spec, "pam_vibeshine_session.so", "RPM PAM module")
-for dependency in ("'pam'", "'socat'", "'iproute2'", "'util-linux'"):
+for dependency in ("'pam'", "'socat'", "'iproute2'", "'jq'", "'util-linux'"):
     require(arch_package, dependency, "Arch runtime dependency")
+if arch_install.index('if "$helper" configure-auto') > arch_install.index('"$helper" install-pam'):
+    raise AssertionError("Arch package must not activate the PAM hook without valid pre-login settings")
 
 print("PASS: hardened Linux session handoff contract")

Nonary/Vibepollo

(cherry picked from commit 55cc4e3986584897b4699607afa0af05d50fc3fc)

cmake/packaging/linux.cmake
@@ -44,6 +44,7 @@ else()
         install(PROGRAMS
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms"
                 "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms-quiesce"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin-sync"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-handoff"
                 "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-ready"
                 "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-install"
@@ -55,6 +56,10 @@ else()
         install(FILES "${LIBVIRTUALDISPLAY_LINUX_ROOT}/packaging/vibeshine-vkms.sysusers"
                 DESTINATION "${VIBESHINE_SYSUSERS_INSTALL_DIR}"
                 RENAME vibeshine-vkms.conf)
+        install(FILES
+                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/vibepollo.conf"
+                "${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"
+                DESTINATION "${SUNSHINE_ASSETS_DIR}/prelogin")
         install(DIRECTORY "${LIBVIRTUALDISPLAY_LINUX_ROOT}/vibeshine-drm/"
                 DESTINATION "${VIBESHINE_DRM_SOURCE_INSTALL_DIR}"
                 FILES_MATCHING
@@ -89,6 +94,7 @@ else()
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-drm-setup.service"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control.socket"
                     "${CMAKE_CURRENT_BINARY_DIR}/vibeshine-vkms-control@.service"
+                    "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin.service"
                     "${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"
                     DESTINATION "${VIBESHINE_SYSTEM_UNIT_INSTALL_DIR}")
         endif()
@@ -152,6 +158,7 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "\
             libgbm1, \
             libevdev2, \
             iproute2, \
+            jq, \
             kmod, \
             libkscreen-bin | libkf5screen-bin, \
             make, \
@@ -174,6 +181,7 @@ set(CPACK_RPM_PACKAGE_REQUIRES "\
             libdrm >= 2.4.97, \
             libevdev >= 1.5.6, \
             iproute, \
+            jq, \
             kmod, \
             libkscreen, \
             make, \

packaging/linux/copr/Sunshine.spec
@@ -142,6 +142,7 @@ Requires: miniupnpc >= 2.2.4
 Requires: which >= 2.21
 Requires: kmod
 Requires: iproute
+Requires: jq
 Requires: pam
 Requires: socat
 Requires: util-linux
@@ -362,12 +363,25 @@ if [ ! -x "$(command -v rpm-ostree)" ]; then
       echo "warning: Vibepollo DRM installation failed; managed virtual displays are unavailable."
     fi
   fi
+  if %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync configure-auto; then
+    if %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync install-pam; then
+      systemctl daemon-reload || true
+      systemctl enable vibepollo-prelogin.service || \
+        echo "warning: could not enable Vibepollo pre-login streaming."
+    else
+      echo "warning: could not install the Plasma Login Manager handoff hook."
+    fi
+  else
+    echo "warning: configure a paired-client allowlist before enabling Vibepollo pre-login streaming."
+  fi
 else
   echo "rpm-ostree environment detected, skipping post install steps. Restart to apply the changes."
 fi
 
 %preun
 if [ "$1" -eq 0 ]; then
+  systemctl disable --now vibepollo-prelogin.service 2>/dev/null || true
+  %{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync remove-pam || true
   systemctl stop vibeshine-vkms.service 2>/dev/null || true
   %{_prefix}/libexec/vibeshine/vibeshine-drm-install remove || \
     echo "warning: could not remove the Vibepollo HDR DRM module cleanly."
@@ -383,6 +397,7 @@ fi
 %{_prefix}/libexec/vibeshine/vibeshine-vkms-peercred
 %{_prefix}/libexec/vibeshine/vibepollo-session-handoff
 %{_prefix}/libexec/vibeshine/vibepollo-session-ready
+%{_prefix}/libexec/vibeshine/vibepollo-prelogin-sync
 %{_prefix}/libexec/vibeshine/kwin-preload/kwin_wayland
 %{_prefix}/lib/vibeshine/libvibeshine-kwin-gpu.so
 %{_libdir}/security/pam_vibepollo_session.so
@@ -403,6 +418,7 @@ fi
 %{_unitdir}/vibeshine-vkms-control.socket
 %{_unitdir}/vibeshine-vkms-control@.service
 %{_unitdir}/vibeshine-vkms.service
+%{_unitdir}/vibepollo-prelogin.service
 %{_unitdir}/vibepollo-session-restore@.service
 
 # Udev rules

tests/unit/platform/linux/test_session_handoff_contract.py
@@ -15,10 +15,15 @@ pam_module = (root / "packaging/linux/pam_vibepollo_session.c").read_text()
 handoff = (root / "packaging/linux/vibepollo-session-handoff").read_text()
 ready = (root / "packaging/linux/vibepollo-session-ready").read_text()
 restore_unit = (root / "packaging/linux/vibepollo-session-restore@.service").read_text()
+prelogin_unit = (root / "packaging/linux/vibepollo-prelogin.service").read_text()
+prelogin_sync = (root / "packaging/linux/vibepollo-prelogin-sync").read_text()
+prelogin_config = (root / "packaging/linux/prelogin/vibepollo.conf").read_text()
+prelogin_apps = (root / "packaging/linux/prelogin/apps.json").read_text()
 packaging = (root / "cmake/packaging/linux.cmake").read_text()
 package_configuration = (root / "cmake/prep/special_package_configuration.cmake").read_text()
 rpm_spec = (root / "packaging/linux/copr/Sunshine.spec").read_text()
 arch_package = (root / "packaging/linux/Arch/PKGBUILD").read_text()
+arch_install = (root / "packaging/linux/Arch/vibepollo.install").read_text()
 
 require(unit, "StartLimitIntervalSec=0", "user service")
 require(unit, "PartOf=graphical-session.target", "user service")
@@ -82,6 +87,28 @@ require(restore_unit, "Type=oneshot", "restore supervisor unit")
 require(restore_unit, "vibepollo-session-handoff restore %i", "restore supervisor unit")
 require(restore_unit, "TimeoutStartSec=infinity", "restore supervisor unit")
 
+require(prelogin_unit, "ConditionPathExists=/etc/vibepollo/prelogin.conf", "pre-login service")
+require(prelogin_unit, "User=plasmalogin", "pre-login service")
+require(prelogin_unit, "vibepollo-session-handoff wait-prelogin", "pre-login service")
+require(prelogin_unit, "vibepollo-prelogin-sync prepare", "pre-login service")
+require(prelogin_unit, "vibepollo-prelogin-sync run", "pre-login service")
+require(prelogin_unit, "WantedBy=graphical.target", "pre-login service")
+if "/usr/local/" in prelogin_unit:
+    raise AssertionError("pre-login service must not depend on host-only /usr/local files")
+
+require(prelogin_sync, "settings must be root:root mode 0600", "pre-login settings boundary")
+require(prelogin_sync, "allowed_client_uuid", "paired-client allowlist")
+require(prelogin_sync, "configure-auto", "legacy provisioning migration")
+require(prelogin_sync, "plasma-login-wayland.target", "greeter compositor startup")
+require(prelogin_sync, "discover_wayland_display", "dynamic greeter display discovery")
+require(prelogin_sync, 'exec /usr/bin/env "WAYLAND_DISPLAY=$display" /usr/bin/vibepollo', "packaged executable")
+require(prelogin_sync, "pam_vibepollo_session.so", "PAM hook management")
+if "source \"$settings_file\"" in prelogin_sync or ". \"$settings_file\"" in prelogin_sync:
+    raise AssertionError("root helper must parse rather than source administrator settings")
+require(prelogin_config, "capture = kms", "pre-login KMS policy")
+require(prelogin_config, "virtual_display_layout = exclusive", "exclusive greeter layout")
+require(prelogin_apps, "/usr/libexec/vibeshine/vibepollo-prelogin-sync activate", "greeter activation command")
+
 require(packaging, "add_library(pam_vibepollo_session MODULE", "native PAM build")
 require(packaging, "install(TARGETS pam_vibepollo_session", "native PAM install")
 require(packaging, "libpam0g", "Debian PAM dependency")
@@ -93,11 +120,16 @@ require(package_configuration, '"package-version:${PROJECT_VERSION_NUMERIC}\\n"'
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-handoff"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-ready"', "native install")
 require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-session-restore@.service"', "native install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin-sync"', "pre-login helper install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/vibepollo-prelogin.service"', "pre-login unit install")
+require(packaging, '"${CMAKE_SOURCE_DIR}/packaging/linux/prelogin/apps.json"', "pre-login profile install")
 
 require(rpm_spec, "BuildRequires: pam-devel", "RPM PAM build dependency")
 require(rpm_spec, "Requires: pam", "RPM PAM runtime dependency")
 require(rpm_spec, "pam_vibepollo_session.so", "RPM PAM module")
-for dependency in ("'pam'", "'socat'", "'iproute2'", "'util-linux'"):
+for dependency in ("'pam'", "'socat'", "'iproute2'", "'jq'", "'util-linux'"):
     require(arch_package, dependency, "Arch runtime dependency")
+if arch_install.index('if "$helper" configure-auto') > arch_install.index('"$helper" install-pam'):
+    raise AssertionError("Arch package must not activate the PAM hook without valid pre-login settings")
 
 print("PASS: hardened Linux session handoff contract")