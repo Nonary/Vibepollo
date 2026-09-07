Nonary/vibeshine

fix(limiter): cover DXVK games with Proton limiting
The Proton provider only configured vkd3d-proton, so D3D9-11 titles could report a limiter without actually receiving one. Apply authoritative DXVK and vkd3d-proton caps after Steam launch options, and add a combined mode that keeps MangoHUD telemetry visible without competing for pacing.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol X-High

tests/unit/platform/linux/test_mangohud_wrapper.sh
@@ -8,13 +8,15 @@ trap 'rm -rf -- "$test_root"' EXIT HUP INT TERM
 write_state() {
   limit=$1
   provider=${2-mangohud}
+  preset=${3-custom}
+  always_show_graph=${4-0}
   expires=$(($(date +%s) + 60))
   printf '%s\n' \
     'version=2' \
     "provider=$provider" \
     "limit=$limit" \
-    'preset=custom' \
-    'always_show_graph=0' \
+    "preset=$preset" \
+    "always_show_graph=$always_show_graph" \
     "owner_pid=$$" \
     "expires=$expires" > "$test_root/480.state"
 }
@@ -47,15 +49,34 @@ proton=$(
   MANGOHUD=1 \
   MANGOHUD_CONFIG='fps_limit=30' \
   MANGOHUD_FPS_LIMIT=30 \
+  DXVK_CONFIG='dxgi.maxFrameRate = 30' \
   LD_PRELOAD='/usr/$LIB/mangohud/libMangoHud_shim.so' \
     "$wrapper" --appid 480 -- /bin/sh -c \
-      'printf "vkd3d=%s\nmango=%s\nconfig=%s\npreload=%s" "$VKD3D_FRAME_RATE" "${MANGOHUD-UNSET}" "${MANGOHUD_CONFIG-UNSET}" "${LD_PRELOAD-UNSET}"'
+      'printf "vkd3d=%s\ndxvk=%s\nmango=%s\nconfig=%s\npreload=%s" "$VKD3D_FRAME_RATE" "$DXVK_CONFIG" "${MANGOHUD-UNSET}" "${MANGOHUD_CONFIG-UNSET}" "${LD_PRELOAD-UNSET}"'
 )
 expected_proton='vkd3d=116
+dxvk=dxgi.maxFrameRate = 30; dxvk.maxFrameRate = 116; dxgi.maxFrameRate = 116; d3d9.maxFrameRate = 116
 mango=UNSET
 config=UNSET
 preload=UNSET'
 [ "$proton" = "$expected_proton" ]
+
+write_state 59.94 mangohud-proton 3 1
+combined=$(
+  VIBESHINE_MANGOHUD_STATE_DIR=$test_root \
+  MANGOHUD_CONFIG='position=top-right,fps_limit=30' \
+  MANGOHUD_FPS_LIMIT=30 \
+  LD_PRELOAD='/usr/lib/libz.so.1' \
+    "$wrapper" --appid 480 -- /bin/sh -c \
+      'printf "vkd3d=%s\ndxvk=%s\nmango=%s\nconfig=%s\nlimit=%s\npreload=%s" "$VKD3D_FRAME_RATE" "$DXVK_CONFIG" "$MANGOHUD" "$MANGOHUD_CONFIG" "${MANGOHUD_FPS_LIMIT-UNSET}" "$LD_PRELOAD"'
+)
+expected_combined='vkd3d=59.94
+dxvk=dxvk.maxFrameRate = 60; dxgi.maxFrameRate = 60; d3d9.maxFrameRate = 60
+mango=1
+config=read_cfg,position=top-right,fps_limit=30,preset=3,no_display=0,frame_timing=1,fps_limit=0
+limit=UNSET
+preload=/usr/lib/libz.so.1:/usr/$LIB/mangohud/libMangoHud_shim.so'
+[ "$combined" = "$expected_combined" ]
 
 rm -f -- "$test_root/480.state"
 passthrough=$(

Nonary/Vibepollo

(cherry picked from commit 967cb871f1dea44a4fa55a804a6241c780997f5b)

tests/unit/platform/linux/test_mangohud_wrapper.sh
@@ -8,13 +8,15 @@ trap 'rm -rf -- "$test_root"' EXIT HUP INT TERM
 write_state() {
   limit=$1
   provider=${2-mangohud}
+  preset=${3-custom}
+  always_show_graph=${4-0}
   expires=$(($(date +%s) + 60))
   printf '%s\n' \
     'version=2' \
     "provider=$provider" \
     "limit=$limit" \
-    'preset=custom' \
-    'always_show_graph=0' \
+    "preset=$preset" \
+    "always_show_graph=$always_show_graph" \
     "owner_pid=$$" \
     "expires=$expires" > "$test_root/480.state"
 }
@@ -47,15 +49,34 @@ proton=$(
   MANGOHUD=1 \
   MANGOHUD_CONFIG='fps_limit=30' \
   MANGOHUD_FPS_LIMIT=30 \
+  DXVK_CONFIG='dxgi.maxFrameRate = 30' \
   LD_PRELOAD='/usr/$LIB/mangohud/libMangoHud_shim.so' \
     "$wrapper" --appid 480 -- /bin/sh -c \
-      'printf "vkd3d=%s\nmango=%s\nconfig=%s\npreload=%s" "$VKD3D_FRAME_RATE" "${MANGOHUD-UNSET}" "${MANGOHUD_CONFIG-UNSET}" "${LD_PRELOAD-UNSET}"'
+      'printf "vkd3d=%s\ndxvk=%s\nmango=%s\nconfig=%s\npreload=%s" "$VKD3D_FRAME_RATE" "$DXVK_CONFIG" "${MANGOHUD-UNSET}" "${MANGOHUD_CONFIG-UNSET}" "${LD_PRELOAD-UNSET}"'
 )
 expected_proton='vkd3d=116
+dxvk=dxgi.maxFrameRate = 30; dxvk.maxFrameRate = 116; dxgi.maxFrameRate = 116; d3d9.maxFrameRate = 116
 mango=UNSET
 config=UNSET
 preload=UNSET'
 [ "$proton" = "$expected_proton" ]
+
+write_state 59.94 mangohud-proton 3 1
+combined=$(
+  VIBEPOLLO_MANGOHUD_STATE_DIR=$test_root \
+  MANGOHUD_CONFIG='position=top-right,fps_limit=30' \
+  MANGOHUD_FPS_LIMIT=30 \
+  LD_PRELOAD='/usr/lib/libz.so.1' \
+    "$wrapper" --appid 480 -- /bin/sh -c \
+      'printf "vkd3d=%s\ndxvk=%s\nmango=%s\nconfig=%s\nlimit=%s\npreload=%s" "$VKD3D_FRAME_RATE" "$DXVK_CONFIG" "$MANGOHUD" "$MANGOHUD_CONFIG" "${MANGOHUD_FPS_LIMIT-UNSET}" "$LD_PRELOAD"'
+)
+expected_combined='vkd3d=59.94
+dxvk=dxvk.maxFrameRate = 60; dxgi.maxFrameRate = 60; d3d9.maxFrameRate = 60
+mango=1
+config=read_cfg,position=top-right,fps_limit=30,preset=3,no_display=0,frame_timing=1,fps_limit=0
+limit=UNSET
+preload=/usr/lib/libz.so.1:/usr/$LIB/mangohud/libMangoHud_shim.so'
+[ "$combined" = "$expected_combined" ]
 
 rm -f -- "$test_root/480.state"
 passthrough=$(