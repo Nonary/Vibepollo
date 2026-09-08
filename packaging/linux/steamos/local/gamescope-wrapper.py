Nonary/vibeshine

feat(steamos): prepare local HDR runtime and reversible Sunshine replacement

packaging/linux/steamos/local/gamescope-wrapper.py
@@ -1,0 +1,99 @@
+#!/usr/bin/python3
+"""Select this local compositor only while its pinned SteamOS inputs match."""
+import hashlib
+import json
+import os
+import pathlib
+import platform
+import shlex
+import subprocess
+import sys
+
+
+def digest(path):
+    with path.open('rb') as stream:
+        return hashlib.file_digest(stream, 'sha256').hexdigest()
+
+
+def os_identity():
+    values = {}
+    for line in pathlib.Path('/etc/os-release').read_text().splitlines():
+        if '=' in line:
+            key, value = line.split('=', 1)
+            values[key] = shlex.split(value)[0] if value else ''
+    return {key: values.get(key) for key in ('ID', 'VERSION_ID', 'BUILD_ID')}
+
+
+def verify(root, privileged=True):
+    manifest = json.loads((root / 'local-manifest.json').read_text())
+    if not isinstance(manifest, dict) or not isinstance(manifest.get('files'), dict):
+        raise RuntimeError('Invalid local Gamescope manifest schema')
+    if (not isinstance(manifest.get('os'), dict)
+            or not all(isinstance(manifest.get(key), str) for key in (
+                'architecture', 'stock_gamescope_package', 'stock_gamescope_sha256', 'stock_session_sha256'))
+            or not all(isinstance(name, str) and isinstance(checksum, str) and len(checksum) == 64
+                       and all(char in '0123456789abcdef' for char in checksum)
+                       for name, checksum in manifest['files'].items())):
+        raise RuntimeError('Invalid local Gamescope manifest schema')
+    required = {'bin/gamescope', 'libexec/gamescope', 'libexec/gamescope-session',
+                'lib/libstdc++.so.6', 'lib/libgcc_s.so.1',
+                'share/gamescope/scripts/00-gamescope/displays/valve.steamdeck.oled.lua'}
+    if not required <= manifest['files'].keys():
+        raise RuntimeError('Incomplete local Gamescope manifest')
+    paths = list(root.rglob('*'))
+    if any(path.is_symlink() for path in paths):
+        raise RuntimeError('Symlinks are not accepted in the local Gamescope tree')
+    inventory = {str(path.relative_to(root)) for path in paths
+                 if path.is_file() and path != root / 'local-manifest.json'}
+    if inventory != manifest['files'].keys():
+        raise RuntimeError('Local Gamescope manifest does not match the file inventory')
+    if manifest['os'] != os_identity() or manifest['architecture'] != platform.machine():
+        raise RuntimeError('SteamOS version/build or architecture changed')
+    package = subprocess.check_output(['/usr/bin/pacman', '-Q', 'gamescope'], text=True).strip()
+    if package != manifest['stock_gamescope_package']:
+        raise RuntimeError('Valve Gamescope package changed')
+    if digest(pathlib.Path('/usr/bin/gamescope')) != manifest['stock_gamescope_sha256']:
+        raise RuntimeError('Valve Gamescope executable changed')
+    if digest(pathlib.Path('/usr/lib/steamos/gamescope-session')) != manifest['stock_session_sha256']:
+        raise RuntimeError('Valve Gaming Mode session launcher changed')
+    for relative, expected in manifest['files'].items():
+        path = root / relative
+        if pathlib.PurePosixPath(relative).is_absolute() or '..' in pathlib.PurePosixPath(relative).parts:
+            raise RuntimeError('Invalid manifest path')
+        if path.is_symlink() or digest(path) != expected:
+            raise RuntimeError(f'Local Gamescope file changed: {relative}')
+    if privileged:
+        for path in [root, *root.parents, *root.rglob('*')]:
+            info = path.stat()
+            if info.st_uid != 0 or info.st_mode & 0o022 or path.is_symlink():
+                raise RuntimeError(f'Gamescope path must be root-owned and unwritable by other users: {path}')
+        verify_capabilities(root / 'libexec/gamescope')
+    return manifest
+
+
+def verify_capabilities(binary):
+    capabilities = subprocess.check_output(['/usr/bin/getcap', str(binary)], text=True)
+    if capabilities.strip() != str(binary) + ' cap_sys_nice=eip':
+        raise RuntimeError('Gamescope CAP_SYS_NICE is missing or changed')
+
+
+def main():
+    root = pathlib.Path(__file__).resolve().parent.parent
+    session = sys.argv[1:] == ['--vibeshine-session']
+    try:
+        verify(root)
+    except (OSError, ValueError, KeyError, RuntimeError, subprocess.SubprocessError) as error:
+        print(f'Vibeshine local HDR compositor unavailable: {error}; using Valve Gamescope.', file=sys.stderr)
+        if session:
+            os.execv('/usr/lib/steamos/gamescope-session', ['gamescope-session'])
+        os.execv('/usr/bin/gamescope', ['gamescope', *sys.argv[1:]])
+    if session:
+        os.execv('/bin/bash', ['bash', str(root / 'libexec/gamescope-session')])
+    # Apply these only after the session script published its environment to
+    # Steam. The private C++ runtime is selected by the ELF's absolute RUNPATH.
+    os.environ['GAMESCOPE_SCRIPT_PATH'] = str(root / 'share/gamescope/scripts')
+    os.execv(str(root / 'libexec/gamescope'), ['gamescope', *sys.argv[1:]])
+
+
+if __name__ == '__main__':
+    main()

packaging/linux/steamos/tests/test-user-install.sh
@@ -54,11 +54,18 @@ test_env=(
   "STEAMOS_TEST_FAIL=$test_root/fail-restart"
 )
 
+# A dormant first install must not schedule a host alongside an existing service.
 env "${test_env[@]}" "$steamos_dir/install-user.sh" \
-  --payload "$test_root/payload" --no-start
+  --payload "$test_root/payload" --no-enable
 test -L "$test_root/home/data/vibeshine-steamos/current"
 test -x "$test_root/home/.local/bin/vibeshine-steamos-session"
 test -f "$test_root/home/config/systemd/user/vibeshine-steamos.service"
+! grep -Eq ' (enable|restart|start) ' "$test_root/systemctl.log"
+
+# --no-start retains its documented enable-on-next-session behavior.
+env "${test_env[@]}" "$steamos_dir/install-user.sh" \
+  --payload "$test_root/payload" --no-start
+grep -qx -- '--user enable vibeshine-steamos.service' "$test_root/systemctl.log"
 ! grep -q 'restart' "$test_root/systemctl.log"
 first_release=$(readlink "$test_root/home/data/vibeshine-steamos/current")
 

src/platform/linux/misc.cpp
@@ -67,6 +67,9 @@
   #include "src/steam_integration.h"
 #endif
 #include "vaapi.h"
+#ifdef SUNSHINE_BUILD_STEAMOS
+  #include "private_vaapi_environment.h"
+#endif
 #ifdef SUNSHINE_BUILD_GAMESCOPE
   #include "gamescope_session.h"
   #include "gamescopegrab.h"
@@ -367,7 +370,17 @@ namespace platf {
       stdio.err = nullptr;
     }
 
+#ifdef SUNSHINE_BUILD_STEAMOS
+    const auto child_env = linux_private_vaapi::child_environment(
+      env,
+      std::getenv("VIBESHINE_PRIVATE_VAAPI"),
+      std::getenv("LIBVA_DRIVERS_PATH"),
+      std::getenv("LIBVA_DRIVER_NAME")
+    );
+    auto env_init = child_env.to_process_environment();
+#else
     auto env_init = env.to_process_environment();
+#endif
     boost::asio::system_executor exec;
 
     try {

src/platform/linux/private_vaapi_environment.h
@@ -1,0 +1,32 @@
+/** @file Keep the SteamOS host's private VAAPI driver out of launched applications. */
+#pragma once
+
+#include "src/boost_process_shim.h"
+
+#include <string_view>
+
+namespace platf::linux_private_vaapi {
+  inline boost_process_shim::environment child_environment(
+    const boost_process_shim::environment &source,
+    const char *daemon_marker,
+    const char *daemon_driver_path,
+    const char *daemon_driver_name
+  ) {
+    if (!daemon_marker || std::string_view {daemon_marker} != "1") {
+      return source;
+    }
+
+    boost_process_shim::environment result;
+    for (const auto &entry : source) {
+      const auto &name = entry.get_name();
+      const auto value = entry.to_string();
+      if (name == "VIBESHINE_PRIVATE_VAAPI" ||
+          (name == "LIBVA_DRIVERS_PATH" && daemon_driver_path && value == daemon_driver_path) ||
+          (name == "LIBVA_DRIVER_NAME" && daemon_driver_name && value == daemon_driver_name)) {
+        continue;
+      }
+      result[name] = value;
+    }
+    return result;
+  }
+}  // namespace platf::linux_private_vaapi

tests/unit/platform/linux/test_boost_process_shim.cpp
@@ -5,8 +5,11 @@
 #include "../../../tests_common.h"
 
 #include <src/boost_process_shim.h>
+#include <src/platform/linux/private_vaapi_environment.h>
 
 #include <algorithm>
+#include <cstdio>
+#include <memory>
 #include <string>
 
 TEST(BoostProcessShim, ConvertedEnvironmentOwnsItsEntries) {
@@ -24,4 +27,55 @@ TEST(BoostProcessShim, ConvertedEnvironmentOwnsItsEntries) {
   );
 
   ASSERT_NE(owned_entry, process_environment.env_buffer.cend());
+}
+
+TEST(BoostProcessShim, PrivateVaapiDriverIsAbsentFromLaunchedApplications) {
+  boost_process_shim::environment environment;
+  environment["VIBESHINE_PRIVATE_VAAPI"] = "1";
+  environment["LIBVA_DRIVERS_PATH"] = "/opt/vibeshine/private-driver";
+  environment["LIBVA_DRIVER_NAME"] = "radeonsi";
+  environment["GAME_OPTION"] = "preserved";
+  auto child_environment = platf::linux_private_vaapi::child_environment(
+    environment, "1", "/opt/vibeshine/private-driver", "radeonsi"
+  ).to_process_environment();
+
+  const auto close_file = [](FILE *file) { std::fclose(file); };
+  std::unique_ptr<FILE, decltype(close_file)> output {std::tmpfile(), close_file};
+  ASSERT_NE(output, nullptr);
+  boost::process::v2::process_stdio stdio {};
+  stdio.out = output.get();
+  boost::process::v2::process child(
+    boost::asio::system_executor(), "/usr/bin/env", std::vector<std::string> {}, stdio, child_environment
+  );
+  EXPECT_EQ(child.wait(), 0);
+  std::rewind(output.get());
+  std::string text;
+  char line[256];
+  while (std::fgets(line, sizeof(line), output.get())) {
+    text += line;
+  }
+  EXPECT_EQ(text, "GAME_OPTION=preserved\n");
+  // Constructing the child environment leaves the daemon's source unchanged.
+  EXPECT_EQ(environment["LIBVA_DRIVER_NAME"].to_string(), "radeonsi");
+}
+
+TEST(BoostProcessShim, PrivateVaapiFilteringPreservesApplicationOverridesAndRequiresExactMarker) {
+  boost_process_shim::environment environment;
+  environment["VIBESHINE_PRIVATE_VAAPI"] = "1";
+  environment["LIBVA_DRIVERS_PATH"] = "/game/drivers";
+  environment["LIBVA_DRIVER_NAME"] = "game-driver";
+  const auto child_environment = platf::linux_private_vaapi::child_environment(
+    environment, "1", "/opt/vibeshine/private-driver", "radeonsi"
+  );
+  ASSERT_EQ(std::distance(child_environment.begin(), child_environment.end()), 2);
+  for (const auto &entry : child_environment) {
+    EXPECT_TRUE(entry.to_string() == "/game/drivers" || entry.to_string() == "game-driver");
+  }
+
+  for (const auto *marker : {static_cast<const char *>(nullptr), "", "0", "10"}) {
+    const auto unchanged = platf::linux_private_vaapi::child_environment(
+      environment, marker, "/game/drivers", "game-driver"
+    );
+    EXPECT_EQ(std::distance(unchanged.begin(), unchanged.end()), 3);
+  }
 }

Nonary/Vibepollo

(cherry picked from commit fe6f943773737c3af6a23a8efca65231a7d45b67)

packaging/linux/steamos/local/gamescope-wrapper.py
@@ -1,0 +1,99 @@
+#!/usr/bin/python3
+"""Select this local compositor only while its pinned SteamOS inputs match."""
+import hashlib
+import json
+import os
+import pathlib
+import platform
+import shlex
+import subprocess
+import sys
+
+
+def digest(path):
+    with path.open('rb') as stream:
+        return hashlib.file_digest(stream, 'sha256').hexdigest()
+
+
+def os_identity():
+    values = {}
+    for line in pathlib.Path('/etc/os-release').read_text().splitlines():
+        if '=' in line:
+            key, value = line.split('=', 1)
+            values[key] = shlex.split(value)[0] if value else ''
+    return {key: values.get(key) for key in ('ID', 'VERSION_ID', 'BUILD_ID')}
+
+
+def verify(root, privileged=True):
+    manifest = json.loads((root / 'local-manifest.json').read_text())
+    if not isinstance(manifest, dict) or not isinstance(manifest.get('files'), dict):
+        raise RuntimeError('Invalid local Gamescope manifest schema')
+    if (not isinstance(manifest.get('os'), dict)
+            or not all(isinstance(manifest.get(key), str) for key in (
+                'architecture', 'stock_gamescope_package', 'stock_gamescope_sha256', 'stock_session_sha256'))
+            or not all(isinstance(name, str) and isinstance(checksum, str) and len(checksum) == 64
+                       and all(char in '0123456789abcdef' for char in checksum)
+                       for name, checksum in manifest['files'].items())):
+        raise RuntimeError('Invalid local Gamescope manifest schema')
+    required = {'bin/gamescope', 'libexec/gamescope', 'libexec/gamescope-session',
+                'lib/libstdc++.so.6', 'lib/libgcc_s.so.1',
+                'share/gamescope/scripts/00-gamescope/displays/valve.steamdeck.oled.lua'}
+    if not required <= manifest['files'].keys():
+        raise RuntimeError('Incomplete local Gamescope manifest')
+    paths = list(root.rglob('*'))
+    if any(path.is_symlink() for path in paths):
+        raise RuntimeError('Symlinks are not accepted in the local Gamescope tree')
+    inventory = {str(path.relative_to(root)) for path in paths
+                 if path.is_file() and path != root / 'local-manifest.json'}
+    if inventory != manifest['files'].keys():
+        raise RuntimeError('Local Gamescope manifest does not match the file inventory')
+    if manifest['os'] != os_identity() or manifest['architecture'] != platform.machine():
+        raise RuntimeError('SteamOS version/build or architecture changed')
+    package = subprocess.check_output(['/usr/bin/pacman', '-Q', 'gamescope'], text=True).strip()
+    if package != manifest['stock_gamescope_package']:
+        raise RuntimeError('Valve Gamescope package changed')
+    if digest(pathlib.Path('/usr/bin/gamescope')) != manifest['stock_gamescope_sha256']:
+        raise RuntimeError('Valve Gamescope executable changed')
+    if digest(pathlib.Path('/usr/lib/steamos/gamescope-session')) != manifest['stock_session_sha256']:
+        raise RuntimeError('Valve Gaming Mode session launcher changed')
+    for relative, expected in manifest['files'].items():
+        path = root / relative
+        if pathlib.PurePosixPath(relative).is_absolute() or '..' in pathlib.PurePosixPath(relative).parts:
+            raise RuntimeError('Invalid manifest path')
+        if path.is_symlink() or digest(path) != expected:
+            raise RuntimeError(f'Local Gamescope file changed: {relative}')
+    if privileged:
+        for path in [root, *root.parents, *root.rglob('*')]:
+            info = path.stat()
+            if info.st_uid != 0 or info.st_mode & 0o022 or path.is_symlink():
+                raise RuntimeError(f'Gamescope path must be root-owned and unwritable by other users: {path}')
+        verify_capabilities(root / 'libexec/gamescope')
+    return manifest
+
+
+def verify_capabilities(binary):
+    capabilities = subprocess.check_output(['/usr/bin/getcap', str(binary)], text=True)
+    if capabilities.strip() != str(binary) + ' cap_sys_nice=eip':
+        raise RuntimeError('Gamescope CAP_SYS_NICE is missing or changed')
+
+
+def main():
+    root = pathlib.Path(__file__).resolve().parent.parent
+    session = sys.argv[1:] == ['--vibepollo-session']
+    try:
+        verify(root)
+    except (OSError, ValueError, KeyError, RuntimeError, subprocess.SubprocessError) as error:
+        print(f'Vibepollo local HDR compositor unavailable: {error}; using Valve Gamescope.', file=sys.stderr)
+        if session:
+            os.execv('/usr/lib/steamos/gamescope-session', ['gamescope-session'])
+        os.execv('/usr/bin/gamescope', ['gamescope', *sys.argv[1:]])
+    if session:
+        os.execv('/bin/bash', ['bash', str(root / 'libexec/gamescope-session')])
+    # Apply these only after the session script published its environment to
+    # Steam. The private C++ runtime is selected by the ELF's absolute RUNPATH.
+    os.environ['GAMESCOPE_SCRIPT_PATH'] = str(root / 'share/gamescope/scripts')
+    os.execv(str(root / 'libexec/gamescope'), ['gamescope', *sys.argv[1:]])
+
+
+if __name__ == '__main__':
+    main()

packaging/linux/steamos/tests/test-user-install.sh
@@ -54,11 +54,18 @@ test_env=(
   "STEAMOS_TEST_FAIL=$test_root/fail-restart"
 )
 
+# A dormant first install must not schedule a host alongside an existing service.
 env "${test_env[@]}" "$steamos_dir/install-user.sh" \
-  --payload "$test_root/payload" --no-start
+  --payload "$test_root/payload" --no-enable
 test -L "$test_root/home/data/vibepollo-steamos/current"
 test -x "$test_root/home/.local/bin/vibepollo-steamos-session"
 test -f "$test_root/home/config/systemd/user/vibepollo-steamos.service"
+! grep -Eq ' (enable|restart|start) ' "$test_root/systemctl.log"
+
+# --no-start retains its documented enable-on-next-session behavior.
+env "${test_env[@]}" "$steamos_dir/install-user.sh" \
+  --payload "$test_root/payload" --no-start
+grep -qx -- '--user enable vibepollo-steamos.service' "$test_root/systemctl.log"
 ! grep -q 'restart' "$test_root/systemctl.log"
 first_release=$(readlink "$test_root/home/data/vibepollo-steamos/current")
 

src/platform/linux/misc.cpp
@@ -67,6 +67,9 @@
   #include "src/steam_integration.h"
 #endif
 #include "vaapi.h"
+#ifdef SUNSHINE_BUILD_STEAMOS
+  #include "private_vaapi_environment.h"
+#endif
 #ifdef SUNSHINE_BUILD_GAMESCOPE
   #include "gamescope_session.h"
   #include "gamescopegrab.h"
@@ -458,7 +461,17 @@ namespace platf {
       stdio.err = nullptr;
     }
 
+#ifdef SUNSHINE_BUILD_STEAMOS
+    const auto child_env = linux_private_vaapi::child_environment(
+      env,
+      std::getenv("VIBEPOLLO_PRIVATE_VAAPI"),
+      std::getenv("LIBVA_DRIVERS_PATH"),
+      std::getenv("LIBVA_DRIVER_NAME")
+    );
+    auto env_init = child_env.to_process_environment();
+#else
     auto env_init = env.to_process_environment();
+#endif
     boost::asio::system_executor exec;
 
     try {

src/platform/linux/private_vaapi_environment.h
@@ -1,0 +1,32 @@
+/** @file Keep the SteamOS host's private VAAPI driver out of launched applications. */
+#pragma once
+
+#include "src/boost_process_shim.h"
+
+#include <string_view>
+
+namespace platf::linux_private_vaapi {
+  inline boost_process_shim::environment child_environment(
+    const boost_process_shim::environment &source,
+    const char *daemon_marker,
+    const char *daemon_driver_path,
+    const char *daemon_driver_name
+  ) {
+    if (!daemon_marker || std::string_view {daemon_marker} != "1") {
+      return source;
+    }
+
+    boost_process_shim::environment result;
+    for (const auto &entry : source) {
+      const auto &name = entry.get_name();
+      const auto value = entry.to_string();
+      if (name == "VIBEPOLLO_PRIVATE_VAAPI" ||
+          (name == "LIBVA_DRIVERS_PATH" && daemon_driver_path && value == daemon_driver_path) ||
+          (name == "LIBVA_DRIVER_NAME" && daemon_driver_name && value == daemon_driver_name)) {
+        continue;
+      }
+      result[name] = value;
+    }
+    return result;
+  }
+}  // namespace platf::linux_private_vaapi

tests/unit/platform/linux/test_boost_process_shim.cpp
@@ -5,8 +5,11 @@
 #include "../../../tests_common.h"
 
 #include <src/boost_process_shim.h>
+#include <src/platform/linux/private_vaapi_environment.h>
 
 #include <algorithm>
+#include <cstdio>
+#include <memory>
 #include <string>
 
 TEST(BoostProcessShim, ConvertedEnvironmentOwnsItsEntries) {
@@ -24,4 +27,55 @@ TEST(BoostProcessShim, ConvertedEnvironmentOwnsItsEntries) {
   );
 
   ASSERT_NE(owned_entry, process_environment.env_buffer.cend());
+}
+
+TEST(BoostProcessShim, PrivateVaapiDriverIsAbsentFromLaunchedApplications) {
+  boost_process_shim::environment environment;
+  environment["VIBEPOLLO_PRIVATE_VAAPI"] = "1";
+  environment["LIBVA_DRIVERS_PATH"] = "/opt/vibepollo/private-driver";
+  environment["LIBVA_DRIVER_NAME"] = "radeonsi";
+  environment["GAME_OPTION"] = "preserved";
+  auto child_environment = platf::linux_private_vaapi::child_environment(
+    environment, "1", "/opt/vibepollo/private-driver", "radeonsi"
+  ).to_process_environment();
+
+  const auto close_file = [](FILE *file) { std::fclose(file); };
+  std::unique_ptr<FILE, decltype(close_file)> output {std::tmpfile(), close_file};
+  ASSERT_NE(output, nullptr);
+  boost::process::v2::process_stdio stdio {};
+  stdio.out = output.get();
+  boost::process::v2::process child(
+    boost::asio::system_executor(), "/usr/bin/env", std::vector<std::string> {}, stdio, child_environment
+  );
+  EXPECT_EQ(child.wait(), 0);
+  std::rewind(output.get());
+  std::string text;
+  char line[256];
+  while (std::fgets(line, sizeof(line), output.get())) {
+    text += line;
+  }
+  EXPECT_EQ(text, "GAME_OPTION=preserved\n");
+  // Constructing the child environment leaves the daemon's source unchanged.
+  EXPECT_EQ(environment["LIBVA_DRIVER_NAME"].to_string(), "radeonsi");
+}
+
+TEST(BoostProcessShim, PrivateVaapiFilteringPreservesApplicationOverridesAndRequiresExactMarker) {
+  boost_process_shim::environment environment;
+  environment["VIBEPOLLO_PRIVATE_VAAPI"] = "1";
+  environment["LIBVA_DRIVERS_PATH"] = "/game/drivers";
+  environment["LIBVA_DRIVER_NAME"] = "game-driver";
+  const auto child_environment = platf::linux_private_vaapi::child_environment(
+    environment, "1", "/opt/vibepollo/private-driver", "radeonsi"
+  );
+  ASSERT_EQ(std::distance(child_environment.begin(), child_environment.end()), 2);
+  for (const auto &entry : child_environment) {
+    EXPECT_TRUE(entry.to_string() == "/game/drivers" || entry.to_string() == "game-driver");
+  }
+
+  for (const auto *marker : {static_cast<const char *>(nullptr), "", "0", "10"}) {
+    const auto unchanged = platf::linux_private_vaapi::child_environment(
+      environment, marker, "/game/drivers", "game-driver"
+    );
+    EXPECT_EQ(std::distance(unchanged.begin(), unchanged.end()), 3);
+  }
 }