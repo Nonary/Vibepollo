Nonary/vibeshine

fix(steamos): admit client-requested private display modes

packaging/linux/steamos/local/update-user.sh
@@ -8,14 +8,16 @@ die() { printf 'update-user.sh: %s\n' "$*" >&2; exit 1; }
 payload=
 check=no
 private_display=no
+defer_start=no
 while (($#)); do
   case "$1" in
     --payload) (($# >= 2)) || die '--payload requires a directory'; payload=$2; shift 2 ;;
     --check) check=yes; shift ;;
     --enable-private-display) private_display=yes; shift ;;
+    --defer-start) defer_start=yes; shift ;;
     -h|--help)
       cat <<'EOF'
-Usage: update-user.sh --payload DIR [--check] [--enable-private-display]
+Usage: update-user.sh --payload DIR [--check] [--enable-private-display] [--defer-start]
 
 Updates the existing Vibeshine user installation, preserving its profile and
 paired clients. Execution restarts the service and disconnects active streams.
@@ -24,6 +26,9 @@ when the updated host can be stopped; otherwise backups are retained untouched.
 --check performs preflight only; it does not install or restart anything.
 --enable-private-display enables client-driven private displays. The kernel
 pool and privileged capture helper must already have been provisioned.
+--defer-start stops the previous host and prepares the update for the next
+boot without starting it. The existing service must be persistently enabled.
+Startup and streaming remain unverified until the machine is rebooted.
 EOF
       exit 0 ;;
     *) die "unknown option: $1" ;;
@@ -146,6 +151,9 @@ gamestream_port=$(cat "$work/port")
 https_port=$((gamestream_port + 1))
 enabled=$(systemctl --user is-enabled "$unit" 2>/dev/null || true)
 case "$enabled" in enabled|enabled-runtime|disabled|static|indirect) ;; *) die 'service is missing, masked, or has an unsupported enable state' ;; esac
+if [[ "$defer_start" == yes && "$enabled" != enabled ]]; then
+  die '--defer-start requires the existing service to be persistently enabled for next boot'
+fi
 was_active=no
 systemctl --user is-active --quiet "$unit" && was_active=yes
 if [[ "$check" == yes ]]; then
@@ -284,6 +292,20 @@ with temporary.open('x') as stream:
     os.fsync(stream.fileno())
 os.replace(temporary, path)
 PY
+fi
+if [[ "$defer_start" == yes ]]; then
+  # Keep reconnecting clients away from a new host paired with the old kernel
+  # until the caller performs its explicit reboot. Existing rollback still
+  # restores and restarts the previous host on any error before this point.
+  remaining_pid=$(systemctl --user show "$unit" --property=MainPID --value)
+  remaining_state=$(systemctl --user show "$unit" --property=ActiveState --value)
+  [[ "$remaining_pid" == 0 && ( "$remaining_state" == inactive || "$remaining_state" == failed ) ]] ||
+    die 'the previous host could not be confirmed stopped for the deferred update'
+  [[ $(systemctl --user is-enabled "$unit") == enabled ]] || die 'the updated service is not enabled for next boot'
+  success=yes
+  printf 'Vibeshine update prepared for next boot; the host remains stopped. Reboot to start it and verify streaming.\n'
+  printf 'Rollback backup: %s\n' "$backup"
+  exit 0
 fi
 service_started_at=$(date -u +'%Y-%m-%d %H:%M:%S UTC')
 systemctl --user restart "$unit"

packaging/linux/steamos/tests/test-update-user.sh
@@ -85,6 +85,10 @@ if [[ ${1:-} == - ]]; then
     [[ -f "$TEST_CASE/private-ready" ]]
     exit
   fi
+  if [[ -f "$TEST_CASE/fail-config" && "$code" == *"updates = {'virtual_display_mode': 'per_client'}"* ]]; then
+    /usr/bin/python3 "$@" <<< "$code"
+    exit 1
+  fi
   exec /usr/bin/python3 "$@" <<< "$code"
 fi
 exec /usr/bin/python3 "$@"
@@ -200,4 +204,32 @@ grep -Fx 'virtual_display_layout = extended_isolated' "$config/vibeshine.conf"
 grep -Fx 'dd_resolution_option = auto' "$config/vibeshine.conf"
 [[ $(grep -c '^virtual_display_mode =' "$config/vibeshine.conf") == 1 ]]
 grep -Fx 'virtual_display_mode = per_client' "$config/vibeshine.conf"
-printf 'SteamOS update transaction: 10 scenarios PASS\n'
+
+new_case deferred_private
+touch "$case_dir/private-ready"
+run_update --enable-private-display --defer-start > "$case_dir/result" 2>&1
+[[ $(readlink "$install_root/current") != releases/original ]]
+[[ $(cat "$case_dir/active") == no && $(cat "$case_dir/enabled") == enabled ]]
+grep -Fx 'virtual_display_mode = per_client' "$config/vibeshine.conf"
+cmp "$case_dir/original-profile/apps.json" "$config/apps.json"
+cmp "$case_dir/original-profile/pairings.json" "$config/pairings.json"
+[[ ! -e "$case_dir/http-calls" && ! -e "$case_dir/restarted" ]]
+if grep -Eq -- '--user (start|restart) ' "$case_dir/calls"; then exit 1; fi
+grep -q 'host remains stopped' "$case_dir/result"
+
+new_case deferred_disabled
+printf 'disabled\n' > "$case_dir/enabled"
+if run_update --defer-start > "$case_dir/result" 2>&1; then exit 1; fi
+[[ $(readlink "$install_root/current") == releases/original ]]
+[[ $(cat "$case_dir/active") == yes && $(cat "$case_dir/enabled") == disabled ]]
+if grep -Eq -- '--user (stop|start|restart|daemon-reload|enable|disable)' "$case_dir/calls"; then exit 1; fi
+diff -r "$case_dir/original-profile" "$config"
+grep -q 'persistently enabled' "$case_dir/result"
+
+new_case deferred_failed_config
+touch "$case_dir/private-ready" "$case_dir/fail-config"
+if run_update --enable-private-display --defer-start > "$case_dir/result" 2>&1; then exit 1; fi
+assert_rollback
+[[ ! -e "$case_dir/restarted" ]]
+grep -q -- '--user start vibeshine-steamos.service' "$case_dir/calls"
+printf 'SteamOS update transaction: 13 scenarios PASS\n'

Nonary/Vibepollo

(cherry picked from commit 7d75cd6c3ab40aab70d1967de02abe1ba89972d8)

packaging/linux/steamos/local/update-user.sh
@@ -8,14 +8,16 @@ die() { printf 'update-user.sh: %s\n' "$*" >&2; exit 1; }
 payload=
 check=no
 private_display=no
+defer_start=no
 while (($#)); do
   case "$1" in
     --payload) (($# >= 2)) || die '--payload requires a directory'; payload=$2; shift 2 ;;
     --check) check=yes; shift ;;
     --enable-private-display) private_display=yes; shift ;;
+    --defer-start) defer_start=yes; shift ;;
     -h|--help)
       cat <<'EOF'
-Usage: update-user.sh --payload DIR [--check] [--enable-private-display]
+Usage: update-user.sh --payload DIR [--check] [--enable-private-display] [--defer-start]
 
 Updates the existing Vibepollo user installation, preserving its profile and
 paired clients. Execution restarts the service and disconnects active streams.
@@ -24,6 +26,9 @@ when the updated host can be stopped; otherwise backups are retained untouched.
 --check performs preflight only; it does not install or restart anything.
 --enable-private-display enables client-driven private displays. The kernel
 pool and privileged capture helper must already have been provisioned.
+--defer-start stops the previous host and prepares the update for the next
+boot without starting it. The existing service must be persistently enabled.
+Startup and streaming remain unverified until the machine is rebooted.
 EOF
       exit 0 ;;
     *) die "unknown option: $1" ;;
@@ -146,6 +151,9 @@ gamestream_port=$(cat "$work/port")
 https_port=$((gamestream_port + 1))
 enabled=$(systemctl --user is-enabled "$unit" 2>/dev/null || true)
 case "$enabled" in enabled|enabled-runtime|disabled|static|indirect) ;; *) die 'service is missing, masked, or has an unsupported enable state' ;; esac
+if [[ "$defer_start" == yes && "$enabled" != enabled ]]; then
+  die '--defer-start requires the existing service to be persistently enabled for next boot'
+fi
 was_active=no
 systemctl --user is-active --quiet "$unit" && was_active=yes
 if [[ "$check" == yes ]]; then
@@ -284,6 +292,20 @@ with temporary.open('x') as stream:
     os.fsync(stream.fileno())
 os.replace(temporary, path)
 PY
+fi
+if [[ "$defer_start" == yes ]]; then
+  # Keep reconnecting clients away from a new host paired with the old kernel
+  # until the caller performs its explicit reboot. Existing rollback still
+  # restores and restarts the previous host on any error before this point.
+  remaining_pid=$(systemctl --user show "$unit" --property=MainPID --value)
+  remaining_state=$(systemctl --user show "$unit" --property=ActiveState --value)
+  [[ "$remaining_pid" == 0 && ( "$remaining_state" == inactive || "$remaining_state" == failed ) ]] ||
+    die 'the previous host could not be confirmed stopped for the deferred update'
+  [[ $(systemctl --user is-enabled "$unit") == enabled ]] || die 'the updated service is not enabled for next boot'
+  success=yes
+  printf 'Vibepollo update prepared for next boot; the host remains stopped. Reboot to start it and verify streaming.\n'
+  printf 'Rollback backup: %s\n' "$backup"
+  exit 0
 fi
 service_started_at=$(date -u +'%Y-%m-%d %H:%M:%S UTC')
 systemctl --user restart "$unit"

packaging/linux/steamos/tests/test-update-user.sh
@@ -85,6 +85,10 @@ if [[ ${1:-} == - ]]; then
     [[ -f "$TEST_CASE/private-ready" ]]
     exit
   fi
+  if [[ -f "$TEST_CASE/fail-config" && "$code" == *"updates = {'virtual_display_mode': 'per_client'}"* ]]; then
+    /usr/bin/python3 "$@" <<< "$code"
+    exit 1
+  fi
   exec /usr/bin/python3 "$@" <<< "$code"
 fi
 exec /usr/bin/python3 "$@"
@@ -200,4 +204,32 @@ grep -Fx 'virtual_display_layout = extended_isolated' "$config/vibepollo.conf"
 grep -Fx 'dd_resolution_option = auto' "$config/vibepollo.conf"
 [[ $(grep -c '^virtual_display_mode =' "$config/vibepollo.conf") == 1 ]]
 grep -Fx 'virtual_display_mode = per_client' "$config/vibepollo.conf"
-printf 'SteamOS update transaction: 10 scenarios PASS\n'
+
+new_case deferred_private
+touch "$case_dir/private-ready"
+run_update --enable-private-display --defer-start > "$case_dir/result" 2>&1
+[[ $(readlink "$install_root/current") != releases/original ]]
+[[ $(cat "$case_dir/active") == no && $(cat "$case_dir/enabled") == enabled ]]
+grep -Fx 'virtual_display_mode = per_client' "$config/vibepollo.conf"
+cmp "$case_dir/original-profile/apps.json" "$config/apps.json"
+cmp "$case_dir/original-profile/pairings.json" "$config/pairings.json"
+[[ ! -e "$case_dir/http-calls" && ! -e "$case_dir/restarted" ]]
+if grep -Eq -- '--user (start|restart) ' "$case_dir/calls"; then exit 1; fi
+grep -q 'host remains stopped' "$case_dir/result"
+
+new_case deferred_disabled
+printf 'disabled\n' > "$case_dir/enabled"
+if run_update --defer-start > "$case_dir/result" 2>&1; then exit 1; fi
+[[ $(readlink "$install_root/current") == releases/original ]]
+[[ $(cat "$case_dir/active") == yes && $(cat "$case_dir/enabled") == disabled ]]
+if grep -Eq -- '--user (stop|start|restart|daemon-reload|enable|disable)' "$case_dir/calls"; then exit 1; fi
+diff -r "$case_dir/original-profile" "$config"
+grep -q 'persistently enabled' "$case_dir/result"
+
+new_case deferred_failed_config
+touch "$case_dir/private-ready" "$case_dir/fail-config"
+if run_update --enable-private-display --defer-start > "$case_dir/result" 2>&1; then exit 1; fi
+assert_rollback
+[[ ! -e "$case_dir/restarted" ]]
+grep -q -- '--user start vibepollo-steamos.service' "$case_dir/calls"
+printf 'SteamOS update transaction: 13 scenarios PASS\n'