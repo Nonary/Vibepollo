Nonary/vibeshine

feat(steamos): add user bundle and Gamescope SDR and HDR capture

cmake/prep/options.cmake
@@ -42,6 +42,20 @@ if(APPLE)
     option(SUNSHINE_CONFIGURE_PORTFILE
             "Configure macOS Portfile. Recommended to use with SUNSHINE_CONFIGURE_ONLY" OFF)
 elseif(UNIX)  # Linux
+    option(SUNSHINE_BUILD_STEAMOS "Build a relocatable SteamOS user bundle." OFF)
+    if(SUNSHINE_BUILD_STEAMOS)
+        if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            message(FATAL_ERROR "The SteamOS bundle requires Linux")
+        endif()
+        if(SUNSHINE_BUILD_APPIMAGE OR SUNSHINE_BUILD_FLATPAK OR SUNSHINE_CONFIGURE_PKGBUILD)
+            message(FATAL_ERROR "The SteamOS bundle cannot be combined with other packaging profiles")
+        endif()
+        # These defaults apply only to a fresh build directory. An explicit
+        # backend choice remains available for other Gamescope hardware.
+        option(SUNSHINE_ENABLE_CUDA "Enable cuda specific code." OFF)
+        option(SUNSHINE_ENABLE_DRM "Enable KMS grab if available." OFF)
+        set(SUNSHINE_ASSETS_DIR "share/vibeshine")
+    endif()
     option(SUNSHINE_BUILD_APPIMAGE
             "Enable an AppImage build." OFF)
     option(SUNSHINE_BUILD_FLATPAK
@@ -68,4 +82,8 @@ elseif(UNIX)  # Linux
             "Enable KWin ScreenCast grab if available" ON)
     option(SUNSHINE_ENABLE_PORTAL
             "Enable XDG portal grab if available" ON)
+    option(SUNSHINE_ENABLE_GAMESCOPE "Enable Gamescope PipeWire capture" ${SUNSHINE_ENABLE_WAYLAND})
+    if(SUNSHINE_BUILD_STEAMOS AND NOT SUNSHINE_ENABLE_GAMESCOPE)
+        message(FATAL_ERROR "The SteamOS bundle requires Gamescope capture")
+    endif()
 endif()

packaging/linux/steamos/gamescope/build-gamescope.py
@@ -1,0 +1,51 @@
+#!/usr/bin/env python3
+"""Fetch the locked Gamescope revision, apply the HDR patch, build and stage it.
+
+Run in a SteamOS-compatible SDK. Does not install packages or change a session.
+"""
+import argparse
+import hashlib
+import json
+import os
+import pathlib
+import shutil
+import subprocess
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('work', type=pathlib.Path, help='New, empty build workspace')
+parser.add_argument('stage', type=pathlib.Path, help='New staging root (DESTDIR)')
+parser.add_argument('--jobs', type=int, default=4)
+parser.add_argument('--meson-option', action='append', default=[])
+args = parser.parse_args()
+here = pathlib.Path(__file__).resolve().parent
+lock = json.loads((here / 'source-lock.json').read_text())
+patch = here / lock['patch']
+if hashlib.sha256(patch.read_bytes()).hexdigest() != lock['patch_sha256']:
+    raise SystemExit('Patch checksum does not match source-lock.json')
+work, stage = args.work.resolve(), args.stage.resolve()
+for directory in [work, stage]:
+    if directory.exists():
+        raise SystemExit(f'Refusing existing directory: {directory}')
+    directory.mkdir(parents=True)
+source, build = work / 'source', work / 'build'
+
+def run(command, **kwargs):
+    subprocess.run(command, check=True, **kwargs)
+
+run(['git', 'clone', '--branch', lock['tag'], '--depth', '1', lock['upstream'], str(source)])
+actual = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=source, text=True).strip()
+if actual != lock['commit']:
+    raise SystemExit(f'Upstream tag moved: {actual}')
+run(['git', 'submodule', 'update', '--init', '--recursive', '--depth', '1', '--',
+     'subprojects/wlroots', 'subprojects/libliftoff', 'subprojects/vkroots', 'src/reshade', 'thirdparty/SPIRV-Headers'], cwd=source)
+run(['git', 'apply', '--check', str(patch)], cwd=source)
+run(['git', 'apply', str(patch)], cwd=source)
+run(['meson', 'setup', str(build), str(source), '--buildtype=release', '--prefix=/opt/vibeshine-gamescope',
+     '-Denable_openvr_support=false', '-Dpipewire=enabled', '-Drt_cap=enabled', '-Ddrm_backend=enabled',
+     '-Dsdl2_backend=enabled', '-Dinput_emulation=enabled', '-Davif_screenshots=enabled',
+     '-Dwlroots:werror=false'] + args.meson_option)
+run(['meson', 'compile', '-C', str(build), '-j', str(args.jobs)])
+run(['meson', 'install', '-C', str(build), '--no-rebuild'], env={**os.environ, 'DESTDIR': str(stage)})
+shutil.copytree(source / 'scripts', stage / 'opt/vibeshine-gamescope/share/gamescope/scripts', dirs_exist_ok=True)
+(stage / 'source-lock.json').write_text(json.dumps(lock, indent=2) + '\n')
+print(f'Built and staged {lock["tag"]} + HDR capture profile 1 at {stage}')

packaging/linux/steamos/tests/build-hdr-probe.py
@@ -1,0 +1,35 @@
+#!/usr/bin/env python3
+"""Build a native integration probe from a completed Ninja Vibeshine build.
+Usage: build-hdr-probe.py BUILD_DIRECTORY OUTPUT_DIRECTORY
+"""
+import json
+import pathlib
+import shlex
+import subprocess
+import sys
+
+build = pathlib.Path(sys.argv[1]).resolve()
+out = pathlib.Path(sys.argv[2]).resolve()
+out.mkdir(parents=True, exist_ok=True)
+source = pathlib.Path(__file__).with_name('hdr-capture-probe.cpp').resolve()
+commands = json.loads((build / 'compile_commands.json').read_text())
+command = next(c for c in commands if c['file'].endswith('/gamescopegrab.cpp'))
+args = shlex.split(command['command'])
+args[args.index('-o') + 1] = str(out / 'probe.o')
+args[args.index('-c') + 1] = str(source)
+args = ['-O0' if x == '-O3' else x for x in args]
+subprocess.run(args, cwd=build, check=True)
+main_object = out / 'main-for-probe.o'
+subprocess.run(['objcopy', '--redefine-sym', 'main=vibeshine_application_main',
+                str(build / 'CMakeFiles/sunshine.dir/src/main.cpp.o'), str(main_object)], check=True)
+lines = subprocess.check_output(['ninja', '-t', 'commands', 'sunshine'], cwd=build, text=True).splitlines()
+args = shlex.split(next(line for line in reversed(lines) if ' -o vibeshine ' in line))
+start = next(i for i, x in enumerate(args) if x.endswith('/c++'))
+args = args[start:]
+if '&&' in args:
+    args = args[:args.index('&&')]
+args = [str(main_object) if x == 'CMakeFiles/sunshine.dir/src/main.cpp.o' else x for x in args]
+args[args.index('-o') + 1] = str(out / 'hdr-capture-probe')
+args.insert(1, str(out / 'probe.o'))
+subprocess.run(args, cwd=build, check=True)
+print(out / 'hdr-capture-probe')

packaging/linux/steamos/tests/hdr-pattern.c
@@ -1,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+// Deterministic HDR10 source using the same Gamescope swapchain color contract
+// as Vulkan WSI. Run only inside the isolated test compositor.
+#define _GNU_SOURCE
+#include "gamescope-swapchain-client-protocol.h"
+
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <unistd.h>
+#include <vulkan/vulkan.h>
+#include <wayland-client.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+
+static struct wl_compositor *compositor;
+static struct wl_shm *shm;
+static struct gamescope_swapchain_factory_v2 *factory;
+static bool released[2] = {true, true};
+static bool rgb10_supported;
+
+static void shm_format(void *data, struct wl_shm *object, uint32_t format) {
+  (void) data;
+  (void) object;
+  if (format == WL_SHM_FORMAT_XBGR2101010) {
+    rgb10_supported = true;
+  }
+}
+static const struct wl_shm_listener shm_listener = {shm_format};
+
+static void global(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version) {
+  (void) data;
+  (void) version;
+  if (!strcmp(interface, "wl_compositor")) {
+    compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
+  } else if (!strcmp(interface, "wl_shm")) {
+    shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
+    wl_shm_add_listener(shm, &shm_listener, NULL);
+  } else if (!strcmp(interface, "gamescope_swapchain_factory_v2")) {
+    factory = wl_registry_bind(registry, name, &gamescope_swapchain_factory_v2_interface, 1);
+  }
+}
+
+static void removed(void *data, struct wl_registry *registry, uint32_t name) {
+  (void) data;
+  (void) registry;
+  (void) name;
+}
+static const struct wl_registry_listener registry_listener = {global, removed};
+
+static void release(void *data, struct wl_buffer *buffer) {
+  (void) buffer;
+  *(bool *) data = true;
+}
+static const struct wl_buffer_listener buffer_listener = {release};
+
+static uint32_t pq(double nits) {
+  const double m1 = 2610.0 / 16384.0, m2 = 2523.0 / 32.0;
+  const double c1 = 3424.0 / 4096.0, c2 = 2413.0 / 128.0, c3 = 2392.0 / 128.0;
+  double p = pow(nits / 10000.0, m1);
+  return (uint32_t) lround(1023.0 * pow((c1 + c2 * p) / (1.0 + c3 * p), m2));
+}
+
+int main(int argc, char **argv) {
+  const bool sdr = argc > 1 && !strcmp(argv[1], "--sdr");
+  struct wl_display *display = wl_display_connect(NULL);
+  assert(display);
+  struct wl_registry *registry = wl_display_get_registry(display);
+  wl_registry_add_listener(registry, &registry_listener, NULL);
+  assert(wl_display_roundtrip(display) >= 0);
+  assert(wl_display_roundtrip(display) >= 0);
+  assert(compositor && shm && factory && rgb10_supported);
+  Display *xdisplay = XOpenDisplay(NULL);
+  assert(xdisplay);
+  Window root = DefaultRootWindow(xdisplay);
+  Window window = XCreateSimpleWindow(xdisplay, root, 0, 0, 1280, 800, 0, 0, 0);
+  XStoreName(xdisplay, window, "Vibeshine HDR validation pattern");
+  XMapWindow(xdisplay, window);
+  XSync(xdisplay, False);
+  Atom actual_type;
+  int actual_format;
+  unsigned long count, remaining;
+  unsigned char *value = NULL;
+  assert(XGetWindowProperty(xdisplay, root, XInternAtom(xdisplay, "GAMESCOPE_XWAYLAND_SERVER_ID", False), 0, 1, False, XA_CARDINAL, &actual_type, &actual_format, &count, &remaining, &value) == Success);
+  assert(count == 1 && actual_format == 32);
+  unsigned server = *(unsigned long *) value;
+  XFree(value);
+  struct wl_surface *surface = wl_compositor_create_surface(compositor);
+  struct gamescope_swapchain *swapchain = gamescope_swapchain_factory_v2_create_swapchain(factory, surface);
+  gamescope_swapchain_override_window_content(swapchain, server, window);
+  gamescope_swapchain_swapchain_feedback(swapchain, 2, VK_FORMAT_A2B10G10R10_UNORM_PACK32, sdr ? VK_COLOR_SPACE_SRGB_NONLINEAR_KHR : VK_COLOR_SPACE_HDR10_ST2084_EXT, VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 1, "Vibeshine HDR validation");
+  gamescope_swapchain_set_present_mode(swapchain, VK_PRESENT_MODE_FIFO_KHR);
+  const size_t bytes = 1280 * 800 * 4;
+  int fd = memfd_create("vibeshine-hdr-pattern", MFD_CLOEXEC);
+  assert(fd >= 0);
+  assert(ftruncate(fd, bytes * 2) == 0);
+  uint32_t *pixels = mmap(NULL, bytes * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  assert(pixels != MAP_FAILED);
+  uint32_t level[] = {0, pq(100), pq(203), pq(1000), pq(4000), pq(1000), pq(1000), pq(1000)};
+  for (int b = 0; b < 2; ++b) {
+    for (int y = 0; y < 800; ++y) {
+      for (int x = 0; x < 1280; ++x) {
+        int band = x / 160;
+        uint32_t n = sdr ? (band == 0 ? 0 : 1023) : level[band];
+        uint32_t r = n, g = n, bl = n;
+        if (band == 5) {
+          g = bl = 0;
+        }
+        if (band == 6) {
+          r = bl = 0;
+        }
+        if (band == 7) {
+          r = g = 0;
+        }
+        pixels[b * (bytes / 4) + y * 1280 + x] = r | (g << 10) | (bl << 20) | (3u << 30);
+      }
+    }
+  }
+  struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, bytes * 2);
+  struct wl_buffer *buffers[2];
+  for (int b = 0; b < 2; ++b) {
+    buffers[b] = wl_shm_pool_create_buffer(pool, b * bytes, 1280, 800, 1280 * 4, WL_SHM_FORMAT_XBGR2101010);
+    wl_buffer_add_listener(buffers[b], &buffer_listener, &released[b]);
+  }
+  close(fd);
+  printf("PATTERN %s levels: %u %u %u %u\n", sdr ? "SDR" : "HDR", level[1], level[2], level[3], level[4]);
+  fflush(stdout);
+  time_t end = time(NULL) + 300;
+  while (time(NULL) < end) {
+    for (int b = 0; b < 2; ++b) {
+      if (released[b]) {
+        released[b] = false;
+        wl_surface_attach(surface, buffers[b], 0, 0);
+        wl_surface_damage_buffer(surface, 0, 0, 1280, 800);
+        wl_surface_commit(surface);
+        break;
+      }
+    }
+    if (wl_display_roundtrip(display) < 0) {
+      break;
+    }
+    usleep(33333);
+  }
+  XDestroyWindow(xdisplay, window);
+  XCloseDisplay(xdisplay);
+  wl_display_disconnect(display);
+  munmap(pixels, bytes * 2);
+  return 0;
+}

packaging/linux/steamos/tests/test-profile.sh
@@ -1,0 +1,46 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+repository=$(CDPATH= cd -- "$(dirname -- "$0")/../../../.." && pwd -P)
+# tests/ -> steamos/ -> linux/ -> packaging/ -> repository
+build_root=$(mktemp -d /tmp/vibeshine-steamos-profile.XXXXXXXX)
+trap 'rm -rf -- "$build_root"' EXIT
+
+cmake -S "$repository" -B "$build_root/profile" \
+  -DSUNSHINE_BUILD_STEAMOS=ON -DSUNSHINE_CONFIGURE_ONLY=ON > "$build_root/configure.log" 2>&1 || {
+  cat "$build_root/configure.log"
+  exit 1
+}
+test ! -e "$build_root/profile/vibeshine-vkms.service"
+test ! -e "$build_root/profile/vibeshine-drm-install"
+test ! -e "$build_root/profile/postinst"
+grep -qx 'BUILD_VIBESHINE_KWIN_GPU_BRIDGE:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+grep -qx 'SUNSHINE_ENABLE_CUDA:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+grep -qx 'SUNSHINE_ENABLE_DRM:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+
+# Exercise the shared Unix path handling: an absolute build prefix must not
+# leak into either the installed assets or the SteamOS runtime definition.
+cat > "$build_root/assets.cmake" <<'EOF'
+set(CMAKE_INSTALL_PREFIX "/temporary/build/prefix")
+set(SUNSHINE_ASSETS_DIR "share/vibeshine")
+set(SUNSHINE_BUILD_STEAMOS ON)
+include("${REPOSITORY}/cmake/compile_definitions/unix.cmake")
+if(NOT SUNSHINE_ASSETS_DIR STREQUAL "share/vibeshine")
+    message(FATAL_ERROR "SteamOS assets must remain bundle-relative")
+endif()
+set(SUNSHINE_BUILD_STEAMOS OFF)
+include("${REPOSITORY}/cmake/compile_definitions/unix.cmake")
+if(NOT SUNSHINE_ASSETS_DIR STREQUAL "/temporary/build/prefix/share/vibeshine")
+    message(FATAL_ERROR "Native Linux assets must retain the install prefix")
+endif()
+EOF
+cmake -DREPOSITORY="$repository" -P "$build_root/assets.cmake"
+
+if cmake -S "$repository" -B "$build_root/conflicting" \
+  -DSUNSHINE_BUILD_STEAMOS=ON -DSUNSHINE_BUILD_APPIMAGE=ON \
+  -DSUNSHINE_CONFIGURE_ONLY=ON > "$build_root/conflicting.log" 2>&1; then
+  printf 'ERROR: incompatible packaging profiles were accepted\n' >&2
+  exit 1
+fi
+grep -q 'cannot be combined' "$build_root/conflicting.log"
+printf 'SteamOS profile tests passed\n'

Nonary/Vibepollo

(cherry picked from commit 322a75226e1c80e55e8d7fdf8febc823dfdb1768)

cmake/prep/options.cmake
@@ -43,6 +43,20 @@ if(APPLE)
     option(SUNSHINE_CONFIGURE_PORTFILE
             "Configure macOS Portfile. Recommended to use with SUNSHINE_CONFIGURE_ONLY" OFF)
 elseif(UNIX)  # Linux
+    option(SUNSHINE_BUILD_STEAMOS "Build a relocatable SteamOS user bundle." OFF)
+    if(SUNSHINE_BUILD_STEAMOS)
+        if(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+            message(FATAL_ERROR "The SteamOS bundle requires Linux")
+        endif()
+        if(SUNSHINE_BUILD_APPIMAGE OR SUNSHINE_BUILD_FLATPAK OR SUNSHINE_CONFIGURE_PKGBUILD)
+            message(FATAL_ERROR "The SteamOS bundle cannot be combined with other packaging profiles")
+        endif()
+        # These defaults apply only to a fresh build directory. An explicit
+        # backend choice remains available for other Gamescope hardware.
+        option(SUNSHINE_ENABLE_CUDA "Enable cuda specific code." OFF)
+        option(SUNSHINE_ENABLE_DRM "Enable KMS grab if available." OFF)
+        set(SUNSHINE_ASSETS_DIR "share/vibepollo")
+    endif()
     option(SUNSHINE_BUILD_APPIMAGE
             "Enable an AppImage build." OFF)
     option(SUNSHINE_BUILD_FLATPAK
@@ -69,4 +83,8 @@ elseif(UNIX)  # Linux
             "Enable KWin ScreenCast grab if available" ON)
     option(SUNSHINE_ENABLE_PORTAL
             "Enable XDG portal grab if available" ON)
+    option(SUNSHINE_ENABLE_GAMESCOPE "Enable Gamescope PipeWire capture" ${SUNSHINE_ENABLE_WAYLAND})
+    if(SUNSHINE_BUILD_STEAMOS AND NOT SUNSHINE_ENABLE_GAMESCOPE)
+        message(FATAL_ERROR "The SteamOS bundle requires Gamescope capture")
+    endif()
 endif()

packaging/linux/steamos/gamescope/build-gamescope.py
@@ -1,0 +1,51 @@
+#!/usr/bin/env python3
+"""Fetch the locked Gamescope revision, apply the HDR patch, build and stage it.
+
+Run in a SteamOS-compatible SDK. Does not install packages or change a session.
+"""
+import argparse
+import hashlib
+import json
+import os
+import pathlib
+import shutil
+import subprocess
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('work', type=pathlib.Path, help='New, empty build workspace')
+parser.add_argument('stage', type=pathlib.Path, help='New staging root (DESTDIR)')
+parser.add_argument('--jobs', type=int, default=4)
+parser.add_argument('--meson-option', action='append', default=[])
+args = parser.parse_args()
+here = pathlib.Path(__file__).resolve().parent
+lock = json.loads((here / 'source-lock.json').read_text())
+patch = here / lock['patch']
+if hashlib.sha256(patch.read_bytes()).hexdigest() != lock['patch_sha256']:
+    raise SystemExit('Patch checksum does not match source-lock.json')
+work, stage = args.work.resolve(), args.stage.resolve()
+for directory in [work, stage]:
+    if directory.exists():
+        raise SystemExit(f'Refusing existing directory: {directory}')
+    directory.mkdir(parents=True)
+source, build = work / 'source', work / 'build'
+
+def run(command, **kwargs):
+    subprocess.run(command, check=True, **kwargs)
+
+run(['git', 'clone', '--branch', lock['tag'], '--depth', '1', lock['upstream'], str(source)])
+actual = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=source, text=True).strip()
+if actual != lock['commit']:
+    raise SystemExit(f'Upstream tag moved: {actual}')
+run(['git', 'submodule', 'update', '--init', '--recursive', '--depth', '1', '--',
+     'subprojects/wlroots', 'subprojects/libliftoff', 'subprojects/vkroots', 'src/reshade', 'thirdparty/SPIRV-Headers'], cwd=source)
+run(['git', 'apply', '--check', str(patch)], cwd=source)
+run(['git', 'apply', str(patch)], cwd=source)
+run(['meson', 'setup', str(build), str(source), '--buildtype=release', '--prefix=/opt/vibepollo-gamescope',
+     '-Denable_openvr_support=false', '-Dpipewire=enabled', '-Drt_cap=enabled', '-Ddrm_backend=enabled',
+     '-Dsdl2_backend=enabled', '-Dinput_emulation=enabled', '-Davif_screenshots=enabled',
+     '-Dwlroots:werror=false'] + args.meson_option)
+run(['meson', 'compile', '-C', str(build), '-j', str(args.jobs)])
+run(['meson', 'install', '-C', str(build), '--no-rebuild'], env={**os.environ, 'DESTDIR': str(stage)})
+shutil.copytree(source / 'scripts', stage / 'opt/vibepollo-gamescope/share/gamescope/scripts', dirs_exist_ok=True)
+(stage / 'source-lock.json').write_text(json.dumps(lock, indent=2) + '\n')
+print(f'Built and staged {lock["tag"]} + HDR capture profile 1 at {stage}')

packaging/linux/steamos/tests/build-hdr-probe.py
@@ -1,0 +1,35 @@
+#!/usr/bin/env python3
+"""Build a native integration probe from a completed Ninja Vibepollo build.
+Usage: build-hdr-probe.py BUILD_DIRECTORY OUTPUT_DIRECTORY
+"""
+import json
+import pathlib
+import shlex
+import subprocess
+import sys
+
+build = pathlib.Path(sys.argv[1]).resolve()
+out = pathlib.Path(sys.argv[2]).resolve()
+out.mkdir(parents=True, exist_ok=True)
+source = pathlib.Path(__file__).with_name('hdr-capture-probe.cpp').resolve()
+commands = json.loads((build / 'compile_commands.json').read_text())
+command = next(c for c in commands if c['file'].endswith('/gamescopegrab.cpp'))
+args = shlex.split(command['command'])
+args[args.index('-o') + 1] = str(out / 'probe.o')
+args[args.index('-c') + 1] = str(source)
+args = ['-O0' if x == '-O3' else x for x in args]
+subprocess.run(args, cwd=build, check=True)
+main_object = out / 'main-for-probe.o'
+subprocess.run(['objcopy', '--redefine-sym', 'main=vibepollo_application_main',
+                str(build / 'CMakeFiles/sunshine.dir/src/main.cpp.o'), str(main_object)], check=True)
+lines = subprocess.check_output(['ninja', '-t', 'commands', 'sunshine'], cwd=build, text=True).splitlines()
+args = shlex.split(next(line for line in reversed(lines) if ' -o vibepollo ' in line))
+start = next(i for i, x in enumerate(args) if x.endswith('/c++'))
+args = args[start:]
+if '&&' in args:
+    args = args[:args.index('&&')]
+args = [str(main_object) if x == 'CMakeFiles/sunshine.dir/src/main.cpp.o' else x for x in args]
+args[args.index('-o') + 1] = str(out / 'hdr-capture-probe')
+args.insert(1, str(out / 'probe.o'))
+subprocess.run(args, cwd=build, check=True)
+print(out / 'hdr-capture-probe')

packaging/linux/steamos/tests/hdr-pattern.c
@@ -1,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+// Deterministic HDR10 source using the same Gamescope swapchain color contract
+// as Vulkan WSI. Run only inside the isolated test compositor.
+#define _GNU_SOURCE
+#include "gamescope-swapchain-client-protocol.h"
+
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <unistd.h>
+#include <vulkan/vulkan.h>
+#include <wayland-client.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+
+static struct wl_compositor *compositor;
+static struct wl_shm *shm;
+static struct gamescope_swapchain_factory_v2 *factory;
+static bool released[2] = {true, true};
+static bool rgb10_supported;
+
+static void shm_format(void *data, struct wl_shm *object, uint32_t format) {
+  (void) data;
+  (void) object;
+  if (format == WL_SHM_FORMAT_XBGR2101010) {
+    rgb10_supported = true;
+  }
+}
+static const struct wl_shm_listener shm_listener = {shm_format};
+
+static void global(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version) {
+  (void) data;
+  (void) version;
+  if (!strcmp(interface, "wl_compositor")) {
+    compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
+  } else if (!strcmp(interface, "wl_shm")) {
+    shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
+    wl_shm_add_listener(shm, &shm_listener, NULL);
+  } else if (!strcmp(interface, "gamescope_swapchain_factory_v2")) {
+    factory = wl_registry_bind(registry, name, &gamescope_swapchain_factory_v2_interface, 1);
+  }
+}
+
+static void removed(void *data, struct wl_registry *registry, uint32_t name) {
+  (void) data;
+  (void) registry;
+  (void) name;
+}
+static const struct wl_registry_listener registry_listener = {global, removed};
+
+static void release(void *data, struct wl_buffer *buffer) {
+  (void) buffer;
+  *(bool *) data = true;
+}
+static const struct wl_buffer_listener buffer_listener = {release};
+
+static uint32_t pq(double nits) {
+  const double m1 = 2610.0 / 16384.0, m2 = 2523.0 / 32.0;
+  const double c1 = 3424.0 / 4096.0, c2 = 2413.0 / 128.0, c3 = 2392.0 / 128.0;
+  double p = pow(nits / 10000.0, m1);
+  return (uint32_t) lround(1023.0 * pow((c1 + c2 * p) / (1.0 + c3 * p), m2));
+}
+
+int main(int argc, char **argv) {
+  const bool sdr = argc > 1 && !strcmp(argv[1], "--sdr");
+  struct wl_display *display = wl_display_connect(NULL);
+  assert(display);
+  struct wl_registry *registry = wl_display_get_registry(display);
+  wl_registry_add_listener(registry, &registry_listener, NULL);
+  assert(wl_display_roundtrip(display) >= 0);
+  assert(wl_display_roundtrip(display) >= 0);
+  assert(compositor && shm && factory && rgb10_supported);
+  Display *xdisplay = XOpenDisplay(NULL);
+  assert(xdisplay);
+  Window root = DefaultRootWindow(xdisplay);
+  Window window = XCreateSimpleWindow(xdisplay, root, 0, 0, 1280, 800, 0, 0, 0);
+  XStoreName(xdisplay, window, "Vibepollo HDR validation pattern");
+  XMapWindow(xdisplay, window);
+  XSync(xdisplay, False);
+  Atom actual_type;
+  int actual_format;
+  unsigned long count, remaining;
+  unsigned char *value = NULL;
+  assert(XGetWindowProperty(xdisplay, root, XInternAtom(xdisplay, "GAMESCOPE_XWAYLAND_SERVER_ID", False), 0, 1, False, XA_CARDINAL, &actual_type, &actual_format, &count, &remaining, &value) == Success);
+  assert(count == 1 && actual_format == 32);
+  unsigned server = *(unsigned long *) value;
+  XFree(value);
+  struct wl_surface *surface = wl_compositor_create_surface(compositor);
+  struct gamescope_swapchain *swapchain = gamescope_swapchain_factory_v2_create_swapchain(factory, surface);
+  gamescope_swapchain_override_window_content(swapchain, server, window);
+  gamescope_swapchain_swapchain_feedback(swapchain, 2, VK_FORMAT_A2B10G10R10_UNORM_PACK32, sdr ? VK_COLOR_SPACE_SRGB_NONLINEAR_KHR : VK_COLOR_SPACE_HDR10_ST2084_EXT, VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 1, "Vibepollo HDR validation");
+  gamescope_swapchain_set_present_mode(swapchain, VK_PRESENT_MODE_FIFO_KHR);
+  const size_t bytes = 1280 * 800 * 4;
+  int fd = memfd_create("vibepollo-hdr-pattern", MFD_CLOEXEC);
+  assert(fd >= 0);
+  assert(ftruncate(fd, bytes * 2) == 0);
+  uint32_t *pixels = mmap(NULL, bytes * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  assert(pixels != MAP_FAILED);
+  uint32_t level[] = {0, pq(100), pq(203), pq(1000), pq(4000), pq(1000), pq(1000), pq(1000)};
+  for (int b = 0; b < 2; ++b) {
+    for (int y = 0; y < 800; ++y) {
+      for (int x = 0; x < 1280; ++x) {
+        int band = x / 160;
+        uint32_t n = sdr ? (band == 0 ? 0 : 1023) : level[band];
+        uint32_t r = n, g = n, bl = n;
+        if (band == 5) {
+          g = bl = 0;
+        }
+        if (band == 6) {
+          r = bl = 0;
+        }
+        if (band == 7) {
+          r = g = 0;
+        }
+        pixels[b * (bytes / 4) + y * 1280 + x] = r | (g << 10) | (bl << 20) | (3u << 30);
+      }
+    }
+  }
+  struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, bytes * 2);
+  struct wl_buffer *buffers[2];
+  for (int b = 0; b < 2; ++b) {
+    buffers[b] = wl_shm_pool_create_buffer(pool, b * bytes, 1280, 800, 1280 * 4, WL_SHM_FORMAT_XBGR2101010);
+    wl_buffer_add_listener(buffers[b], &buffer_listener, &released[b]);
+  }
+  close(fd);
+  printf("PATTERN %s levels: %u %u %u %u\n", sdr ? "SDR" : "HDR", level[1], level[2], level[3], level[4]);
+  fflush(stdout);
+  time_t end = time(NULL) + 300;
+  while (time(NULL) < end) {
+    for (int b = 0; b < 2; ++b) {
+      if (released[b]) {
+        released[b] = false;
+        wl_surface_attach(surface, buffers[b], 0, 0);
+        wl_surface_damage_buffer(surface, 0, 0, 1280, 800);
+        wl_surface_commit(surface);
+        break;
+      }
+    }
+    if (wl_display_roundtrip(display) < 0) {
+      break;
+    }
+    usleep(33333);
+  }
+  XDestroyWindow(xdisplay, window);
+  XCloseDisplay(xdisplay);
+  wl_display_disconnect(display);
+  munmap(pixels, bytes * 2);
+  return 0;
+}

packaging/linux/steamos/tests/test-profile.sh
@@ -1,0 +1,46 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+repository=$(CDPATH= cd -- "$(dirname -- "$0")/../../../.." && pwd -P)
+# tests/ -> steamos/ -> linux/ -> packaging/ -> repository
+build_root=$(mktemp -d /tmp/vibepollo-steamos-profile.XXXXXXXX)
+trap 'rm -rf -- "$build_root"' EXIT
+
+cmake -S "$repository" -B "$build_root/profile" \
+  -DSUNSHINE_BUILD_STEAMOS=ON -DSUNSHINE_CONFIGURE_ONLY=ON > "$build_root/configure.log" 2>&1 || {
+  cat "$build_root/configure.log"
+  exit 1
+}
+test ! -e "$build_root/profile/vibeshine-vkms.service"
+test ! -e "$build_root/profile/vibeshine-drm-install"
+test ! -e "$build_root/profile/postinst"
+grep -qx 'BUILD_VIBESHINE_KWIN_GPU_BRIDGE:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+grep -qx 'SUNSHINE_ENABLE_CUDA:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+grep -qx 'SUNSHINE_ENABLE_DRM:BOOL=OFF' "$build_root/profile/CMakeCache.txt"
+
+# Exercise the shared Unix path handling: an absolute build prefix must not
+# leak into either the installed assets or the SteamOS runtime definition.
+cat > "$build_root/assets.cmake" <<'EOF'
+set(CMAKE_INSTALL_PREFIX "/temporary/build/prefix")
+set(SUNSHINE_ASSETS_DIR "share/vibepollo")
+set(SUNSHINE_BUILD_STEAMOS ON)
+include("${REPOSITORY}/cmake/compile_definitions/unix.cmake")
+if(NOT SUNSHINE_ASSETS_DIR STREQUAL "share/vibepollo")
+    message(FATAL_ERROR "SteamOS assets must remain bundle-relative")
+endif()
+set(SUNSHINE_BUILD_STEAMOS OFF)
+include("${REPOSITORY}/cmake/compile_definitions/unix.cmake")
+if(NOT SUNSHINE_ASSETS_DIR STREQUAL "/temporary/build/prefix/share/vibepollo")
+    message(FATAL_ERROR "Native Linux assets must retain the install prefix")
+endif()
+EOF
+cmake -DREPOSITORY="$repository" -P "$build_root/assets.cmake"
+
+if cmake -S "$repository" -B "$build_root/conflicting" \
+  -DSUNSHINE_BUILD_STEAMOS=ON -DSUNSHINE_BUILD_APPIMAGE=ON \
+  -DSUNSHINE_CONFIGURE_ONLY=ON > "$build_root/conflicting.log" 2>&1; then
+  printf 'ERROR: incompatible packaging profiles were accepted\n' >&2
+  exit 1
+fi
+grep -q 'cannot be combined' "$build_root/conflicting.log"
+printf 'SteamOS profile tests passed\n'