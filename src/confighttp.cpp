Nonary/vibeshine

fix(logs): Export retained log bundles from the v2 UI on Linux
The v2 download saved only visible text, and Linux lacked the bundle endpoint.
Export retained sessions and rollovers with shared sanitization and ZIP encoding,
matching Windows' 32-file/64 MiB limits. Verify rotation, retention, and downloads.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Medium

src/confighttp.cpp
@@ -54,6 +54,7 @@
 
 #endif
 #include "logging.h"
+#include "log_export.h"
 #include "network.h"
 #include "nvhttp.h"
 #include "remote_display_topology.h"
@@ -4108,6 +4109,71 @@ namespace confighttp {
     }
   }
 
+#ifndef _WIN32
+  void downloadLogs(resp_https_t response, req_https_t request) {
+    if (!authenticate(response, request)) {
+      return;
+    }
+    try {
+      logging::log_flush();
+      std::vector<std::pair<std::filesystem::path, std::filesystem::file_time_type>> candidates;
+      for (const auto &path : logging::recent_session_logs(30)) {
+        std::error_code ec;
+        const auto mtime = std::filesystem::last_write_time(path, ec);
+        if (!ec) {
+          candidates.emplace_back(path, mtime);
+        }
+      }
+      // Snapshot timestamps before sorting: the active log can change during collection.
+      // Match the Windows support bundle limits, selecting newest files first.
+      std::sort(candidates.begin(), candidates.end(), [](const auto &a, const auto &b) {
+        return a.second != b.second ? a.second > b.second : a.first < b.first;
+      });
+      constexpr std::size_t max_files = 32;
+      constexpr std::size_t max_bytes = 64 * 1024 * 1024;
+      std::size_t bytes = 0;
+      log_export::export_log_sanitizer_t sanitizer;
+      std::vector<log_export::ZipDataEntry> entries;
+      for (const auto &[path, mtime] : candidates) {
+        if (entries.size() >= max_files || bytes >= max_bytes) {
+          break;
+        }
+        std::error_code ec;
+        if (!std::filesystem::is_regular_file(std::filesystem::symlink_status(path, ec))) {
+          continue;
+        }
+        const auto size = std::filesystem::file_size(path, ec);
+        if (ec || size > max_bytes - bytes) {
+          continue;
+        }
+        std::ifstream file(path, std::ios::binary);
+        if (!file) {
+          continue;
+        }
+        // Bound reads even if the active file grows during collection.
+        std::string data(static_cast<std::size_t>(size), '\0');
+        file.read(data.data(), static_cast<std::streamsize>(data.size()));
+        data.resize(static_cast<std::size_t>(file.gcount()));
+        bytes += data.size();
+        entries.push_back(log_export::make_export_log_entry(sanitizer, path.filename().string(), std::move(data), mtime));
+      }
+      if (entries.empty()) {
+        bad_request(response, request, "No retained log files are available");
+        return;
+      }
+      SimpleWeb::CaseInsensitiveMultimap headers;
+      headers.emplace("Content-Type", "application/zip");
+      headers.emplace("Content-Disposition", "attachment; filename=\"vibeshine_logs.zip\"");
+      headers.emplace("Cache-Control", "no-store");
+      headers.emplace("X-Frame-Options", "DENY");
+      headers.emplace("Content-Security-Policy", "frame-ancestors 'none';");
+      response->write(success_ok, log_export::build_zip_from_entries(entries), headers);
+    } catch (const std::exception &e) {
+      bad_request(response, request, e.what());
+    }
+  }
+#endif
+
   /**
    * @brief Get the logs from the log file.
    * @param response The HTTP response object.
@@ -5561,6 +5627,8 @@ namespace confighttp {
     register_blocking_api_route("^/api/logs/export$", "GET", downloadPlayniteLogs);
     register_blocking_api_route("^/api/logs/export_crash/manifest$", "GET", getCrashBundleManifest);
     register_blocking_api_route("^/api/logs/export_crash$", "GET", downloadCrashBundle);
+#else
+    register_blocking_api_route("^/api/logs/export$", "GET", downloadLogs);
 #endif
     register_api_route("^/api/token$", "POST", generateApiToken);
     register_api_route("^/api/tokens$", "GET", listApiTokens);

src_assets/common/assets/web/e2e/ui.spec.ts
@@ -904,3 +904,25 @@ test('initial library setup refreshes apps and persists editable settings', asyn
   await dialog.getByRole('button', { name: 'Next: Library settings' }).click();
   await expect(dialog.getByRole('spinbutton', { name: 'Recent games', exact: true })).toHaveValue('6');
 });
+
+for (const platform of ['linux', 'windows'] as const) {
+  test(`${platform} logs download requests the retained bundle even with an empty viewer`, async ({
+    page,
+  }) => {
+    await host(page, platform, {});
+    await page.route('**/api/logs?*', (route) =>
+      route.fulfill({ body: '', contentType: 'text/plain' }),
+    );
+    await page.route('**/api/logs/export', (route) =>
+      route.fulfill({
+        body: 'test bundle',
+        contentType: 'application/zip',
+        headers: { 'Content-Disposition': 'attachment; filename="vibeshine_logs.zip"' },
+      }),
+    );
+    await page.goto('/v2/logs');
+    const download = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'Download logs bundle' }).click();
+    expect((await download).suggestedFilename()).toBe('vibeshine_logs.zip');
+  });
+}

Nonary/Vibepollo

(cherry picked from commit 7113ac75aad456df42f1191a5c45f256e72d911c)

src/confighttp.cpp
@@ -59,6 +59,7 @@
 
 #endif
 #include "logging.h"
+#include "log_export.h"
 #include "network.h"
 #include "nvhttp.h"
 #include "remote_display_topology.h"
@@ -4517,6 +4518,71 @@ namespace confighttp {
     }
   }
 
+#ifndef _WIN32
+  void downloadLogs(resp_https_t response, req_https_t request) {
+    if (!authenticate(response, request)) {
+      return;
+    }
+    try {
+      logging::log_flush();
+      std::vector<std::pair<std::filesystem::path, std::filesystem::file_time_type>> candidates;
+      for (const auto &path : logging::recent_session_logs(30)) {
+        std::error_code ec;
+        const auto mtime = std::filesystem::last_write_time(path, ec);
+        if (!ec) {
+          candidates.emplace_back(path, mtime);
+        }
+      }
+      // Snapshot timestamps before sorting: the active log can change during collection.
+      // Match the Windows support bundle limits, selecting newest files first.
+      std::sort(candidates.begin(), candidates.end(), [](const auto &a, const auto &b) {
+        return a.second != b.second ? a.second > b.second : a.first < b.first;
+      });
+      constexpr std::size_t max_files = 32;
+      constexpr std::size_t max_bytes = 64 * 1024 * 1024;
+      std::size_t bytes = 0;
+      log_export::export_log_sanitizer_t sanitizer;
+      std::vector<log_export::ZipDataEntry> entries;
+      for (const auto &[path, mtime] : candidates) {
+        if (entries.size() >= max_files || bytes >= max_bytes) {
+          break;
+        }
+        std::error_code ec;
+        if (!std::filesystem::is_regular_file(std::filesystem::symlink_status(path, ec))) {
+          continue;
+        }
+        const auto size = std::filesystem::file_size(path, ec);
+        if (ec || size > max_bytes - bytes) {
+          continue;
+        }
+        std::ifstream file(path, std::ios::binary);
+        if (!file) {
+          continue;
+        }
+        // Bound reads even if the active file grows during collection.
+        std::string data(static_cast<std::size_t>(size), '\0');
+        file.read(data.data(), static_cast<std::streamsize>(data.size()));
+        data.resize(static_cast<std::size_t>(file.gcount()));
+        bytes += data.size();
+        entries.push_back(log_export::make_export_log_entry(sanitizer, path.filename().string(), std::move(data), mtime));
+      }
+      if (entries.empty()) {
+        bad_request(response, request, "No retained log files are available");
+        return;
+      }
+      SimpleWeb::CaseInsensitiveMultimap headers;
+      headers.emplace("Content-Type", "application/zip");
+      headers.emplace("Content-Disposition", "attachment; filename=\"vibepollo_logs.zip\"");
+      headers.emplace("Cache-Control", "no-store");
+      headers.emplace("X-Frame-Options", "DENY");
+      headers.emplace("Content-Security-Policy", "frame-ancestors 'none';");
+      response->write(success_ok, log_export::build_zip_from_entries(entries), headers);
+    } catch (const std::exception &e) {
+      bad_request(response, request, e.what());
+    }
+  }
+#endif
+
   /**
    * @brief Get the logs from the log file.
    * @param response The HTTP response object.
@@ -6115,6 +6181,8 @@ namespace confighttp {
     register_blocking_api_route("^/api/logs/export$", "GET", downloadPlayniteLogs);
     register_blocking_api_route("^/api/logs/export_crash/manifest$", "GET", getCrashBundleManifest);
     register_blocking_api_route("^/api/logs/export_crash$", "GET", downloadCrashBundle);
+#else
+    register_blocking_api_route("^/api/logs/export$", "GET", downloadLogs);
 #endif
     server.resource["^/images/sunshine.ico$"]["GET"] = getFaviconImage;
     server.resource["^/images/logo-apollo-45.png$"]["GET"] = getApolloLogoImage;

src_assets/common/assets/web/e2e/ui.spec.ts
@@ -905,3 +905,25 @@ test('initial library setup refreshes apps and persists editable settings', asyn
   await dialog.getByRole('button', { name: 'Next: Library settings' }).click();
   await expect(dialog.getByRole('spinbutton', { name: 'Recent games', exact: true })).toHaveValue('6');
 });
+
+for (const platform of ['linux', 'windows'] as const) {
+  test(`${platform} logs download requests the retained bundle even with an empty viewer`, async ({
+    page,
+  }) => {
+    await host(page, platform, {});
+    await page.route('**/api/logs?*', (route) =>
+      route.fulfill({ body: '', contentType: 'text/plain' }),
+    );
+    await page.route('**/api/logs/export', (route) =>
+      route.fulfill({
+        body: 'test bundle',
+        contentType: 'application/zip',
+        headers: { 'Content-Disposition': 'attachment; filename="vibepollo_logs.zip"' },
+      }),
+    );
+    await page.goto('/v2/logs');
+    const download = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'Download logs bundle' }).click();
+    expect((await download).suggestedFilename()).toBe('vibepollo_logs.zip');
+  });
+}