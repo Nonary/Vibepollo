Nonary/vibeshine

fix(web): notify users of available updates across the new UI
The new Web UI only fetched release notes on demand, leaving Linux
users without an automatic update notice. Check on opening and at the
configured interval, honor release channels, and offer retries on failure.
Cover version selection and Linux browser notification behavior.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Medium

src_assets/common/assets/web/components/layout/UpdateNotice.vue
@@ -1,0 +1,105 @@
+<script setup lang="ts">
+import { onBeforeUnmount, ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { apiGet } from '@/api/client';
+import { AppButton, InlineAlert } from '@/components/ui';
+import { useSystemStore } from '@/stores/system';
+import type { ChangelogEntry } from '@/utils/changelog';
+import { selectAvailableUpdate } from '@/utils/updates';
+
+const system = useSystemStore();
+const { t } = useI18n();
+const available = ref<ChangelogEntry | null>(null);
+const failed = ref(false);
+const loading = ref(false);
+let timer: ReturnType<typeof setTimeout> | undefined;
+let stopped = false;
+let controller: AbortController | undefined;
+
+async function check(): Promise<void> {
+  if (loading.value || !system.metadata?.version || stopped) return;
+  clearTimeout(timer);
+  loading.value = true;
+  let nextCheck = 300000;
+  controller = new AbortController();
+  const timeout = setTimeout(() => controller?.abort(), 15000);
+  try {
+    const config = await apiGet<Record<string, unknown>>('/api/config', {
+      signal: controller.signal,
+    });
+    const response = await fetch('https://api.github.com/repos/Nonary/vibeshine/releases', {
+      headers: { Accept: 'application/json' },
+      credentials: 'omit',
+      signal: controller.signal,
+    });
+    if (!response.ok) throw new Error('releases-unavailable');
+    const releases = await response.json();
+    if (!Array.isArray(releases)) throw new Error('invalid-releases');
+    available.value = selectAvailableUpdate(
+      system.metadata.version,
+      releases,
+      config.notify_pre_releases,
+    );
+    failed.value = false;
+    const interval = Number(config.update_check_interval ?? 86400);
+    nextCheck = Number.isFinite(interval) && interval >= 0 ? interval * 1000 : 86400000;
+  } catch {
+    if (!stopped) failed.value = true;
+  } finally {
+    clearTimeout(timeout);
+    loading.value = false;
+    if (!stopped && nextCheck > 0)
+      timer = setTimeout(() => void check(), Math.min(2147483647, Math.max(60000, nextCheck)));
+  }
+}
+
+watch(
+  () => system.metadata?.version,
+  () => void check(),
+  { immediate: true },
+);
+onBeforeUnmount(() => {
+  stopped = true;
+  clearTimeout(timer);
+  controller?.abort();
+});
+</script>
+
+<template>
+  <div v-if="available || failed" class="update-notice">
+    <InlineAlert
+      v-if="available"
+      tone="info"
+      announce="polite"
+      :title="t('ui.updates.available', { version: available.tag })"
+    >
+      <a
+        :href="
+          available.url?.startsWith('https://github.com/Nonary/vibeshine/releases/')
+            ? available.url
+            : 'https://github.com/Nonary/vibeshine/releases'
+        "
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.maintenance.releases.open') }}</a
+      >
+    </InlineAlert>
+    <InlineAlert v-if="failed" tone="warning" :title="t('ui.maintenance.releases.unavailable')">
+      <AppButton
+        :label="t('ui.maintenance.releases.check')"
+        :busy="loading"
+        :busy-label="t('ui.maintenance.releases.loading')"
+        variant="secondary"
+        @click="check"
+      />
+    </InlineAlert>
+  </div>
+</template>
+
+<style scoped>
+.update-notice {
+  display: grid;
+  gap: var(--vs-space-12);
+  margin: var(--vs-space-24);
+}
+</style>

src_assets/common/assets/web/e2e/ui.spec.ts
@@ -926,3 +926,50 @@ for (const platform of ['linux', 'windows'] as const) {
     expect((await download).suggestedFilename()).toBe('vibeshine_logs.zip');
   });
 }
+
+test('Linux shows an automatic update notice across pages and retries failed checks', async ({
+  page,
+}) => {
+  await host(page);
+  let unavailable = true;
+  await page.route('https://api.github.com/repos/Nonary/vibeshine/releases', async (route) => {
+    await route.fulfill(
+      unavailable
+        ? { status: 403, json: { message: 'API rate limit exceeded' } }
+        : {
+            json: [
+              {
+                tag_name: 'v1.1.0',
+                prerelease: false,
+                html_url: 'https://github.com/Nonary/vibeshine/releases/tag/v1.1.0',
+              },
+            ],
+          },
+    );
+  });
+  await page.goto('/v2/');
+  const notice = page.locator('.update-notice');
+  await expect(
+    notice.getByText('Release information is unavailable. Try again later.'),
+  ).toBeVisible();
+  unavailable = false;
+  await notice.getByRole('button', { name: 'Check for updates' }).click();
+  await expect(notice.getByText('Vibeshine 1.1.0 is available')).toBeVisible();
+  await expect(notice.getByRole('link', { name: 'Read release notes' })).toHaveAttribute(
+    'href',
+    'https://github.com/Nonary/vibeshine/releases/tag/v1.1.0',
+  );
+  await page.getByRole('link', { name: 'Library', exact: true }).click();
+  await expect(notice.getByText('Vibeshine 1.1.0 is available')).toBeVisible();
+});
+
+test('Linux stable install does not advertise a prerelease without opt-in', async ({ page }) => {
+  await host(page);
+  await page.route('https://api.github.com/repos/Nonary/vibeshine/releases', async (route) => {
+    await route.fulfill({ json: [{ tag_name: 'v2.0.0-beta.1', prerelease: true }] });
+  });
+  const response = page.waitForResponse('https://api.github.com/repos/Nonary/vibeshine/releases');
+  await page.goto('/v2/');
+  await response;
+  await expect(page.locator('.update-notice')).toHaveCount(0);
+});

Nonary/Vibepollo

(cherry picked from commit b387a2c8ba9a048a53d51c221e48e3f3a6ca05e2)

src_assets/common/assets/web/components/layout/UpdateNotice.vue
@@ -1,0 +1,105 @@
+<script setup lang="ts">
+import { onBeforeUnmount, ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { apiGet } from '@/api/client';
+import { AppButton, InlineAlert } from '@/components/ui';
+import { useSystemStore } from '@/stores/system';
+import type { ChangelogEntry } from '@/utils/changelog';
+import { selectAvailableUpdate } from '@/utils/updates';
+
+const system = useSystemStore();
+const { t } = useI18n();
+const available = ref<ChangelogEntry | null>(null);
+const failed = ref(false);
+const loading = ref(false);
+let timer: ReturnType<typeof setTimeout> | undefined;
+let stopped = false;
+let controller: AbortController | undefined;
+
+async function check(): Promise<void> {
+  if (loading.value || !system.metadata?.version || stopped) return;
+  clearTimeout(timer);
+  loading.value = true;
+  let nextCheck = 300000;
+  controller = new AbortController();
+  const timeout = setTimeout(() => controller?.abort(), 15000);
+  try {
+    const config = await apiGet<Record<string, unknown>>('/api/config', {
+      signal: controller.signal,
+    });
+    const response = await fetch('https://api.github.com/repos/Nonary/Vibepollo/releases', {
+      headers: { Accept: 'application/json' },
+      credentials: 'omit',
+      signal: controller.signal,
+    });
+    if (!response.ok) throw new Error('releases-unavailable');
+    const releases = await response.json();
+    if (!Array.isArray(releases)) throw new Error('invalid-releases');
+    available.value = selectAvailableUpdate(
+      system.metadata.version,
+      releases,
+      config.notify_pre_releases,
+    );
+    failed.value = false;
+    const interval = Number(config.update_check_interval ?? 86400);
+    nextCheck = Number.isFinite(interval) && interval >= 0 ? interval * 1000 : 86400000;
+  } catch {
+    if (!stopped) failed.value = true;
+  } finally {
+    clearTimeout(timeout);
+    loading.value = false;
+    if (!stopped && nextCheck > 0)
+      timer = setTimeout(() => void check(), Math.min(2147483647, Math.max(60000, nextCheck)));
+  }
+}
+
+watch(
+  () => system.metadata?.version,
+  () => void check(),
+  { immediate: true },
+);
+onBeforeUnmount(() => {
+  stopped = true;
+  clearTimeout(timer);
+  controller?.abort();
+});
+</script>
+
+<template>
+  <div v-if="available || failed" class="update-notice">
+    <InlineAlert
+      v-if="available"
+      tone="info"
+      announce="polite"
+      :title="t('ui.updates.available', { version: available.tag })"
+    >
+      <a
+        :href="
+          available.url?.startsWith('https://github.com/Nonary/Vibepollo/releases/')
+            ? available.url
+            : 'https://github.com/Nonary/Vibepollo/releases'
+        "
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ t('ui.maintenance.releases.open') }}</a
+      >
+    </InlineAlert>
+    <InlineAlert v-if="failed" tone="warning" :title="t('ui.maintenance.releases.unavailable')">
+      <AppButton
+        :label="t('ui.maintenance.releases.check')"
+        :busy="loading"
+        :busy-label="t('ui.maintenance.releases.loading')"
+        variant="secondary"
+        @click="check"
+      />
+    </InlineAlert>
+  </div>
+</template>
+
+<style scoped>
+.update-notice {
+  display: grid;
+  gap: var(--vs-space-12);
+  margin: var(--vs-space-24);
+}
+</style>

src_assets/common/assets/web/e2e/ui.spec.ts
@@ -927,3 +927,50 @@ for (const platform of ['linux', 'windows'] as const) {
     expect((await download).suggestedFilename()).toBe('vibepollo_logs.zip');
   });
 }
+
+test('Linux shows an automatic update notice across pages and retries failed checks', async ({
+  page,
+}) => {
+  await host(page);
+  let unavailable = true;
+  await page.route('https://api.github.com/repos/Nonary/Vibepollo/releases', async (route) => {
+    await route.fulfill(
+      unavailable
+        ? { status: 403, json: { message: 'API rate limit exceeded' } }
+        : {
+            json: [
+              {
+                tag_name: 'v1.1.0',
+                prerelease: false,
+                html_url: 'https://github.com/Nonary/Vibepollo/releases/tag/v1.1.0',
+              },
+            ],
+          },
+    );
+  });
+  await page.goto('/v2/');
+  const notice = page.locator('.update-notice');
+  await expect(
+    notice.getByText('Release information is unavailable. Try again later.'),
+  ).toBeVisible();
+  unavailable = false;
+  await notice.getByRole('button', { name: 'Check for updates' }).click();
+  await expect(notice.getByText('Vibepollo 1.1.0 is available')).toBeVisible();
+  await expect(notice.getByRole('link', { name: 'Read release notes' })).toHaveAttribute(
+    'href',
+    'https://github.com/Nonary/Vibepollo/releases/tag/v1.1.0',
+  );
+  await page.getByRole('link', { name: 'Library', exact: true }).click();
+  await expect(notice.getByText('Vibepollo 1.1.0 is available')).toBeVisible();
+});
+
+test('Linux stable install does not advertise a prerelease without opt-in', async ({ page }) => {
+  await host(page);
+  await page.route('https://api.github.com/repos/Nonary/Vibepollo/releases', async (route) => {
+    await route.fulfill({ json: [{ tag_name: 'v2.0.0-beta.1', prerelease: true }] });
+  });
+  const response = page.waitForResponse('https://api.github.com/repos/Nonary/Vibepollo/releases');
+  await page.goto('/v2/');
+  await response;
+  await expect(page.locator('.update-notice')).toHaveCount(0);
+});