Nonary/vibeshine

fix(linux): apply stream limits to external Proton launches
Desktop-stream games bypassed the managed application limiter handoff.
Apply the selected provider through session-owned Proton launch hooks,
preserving user settings and managed per-application limits. Override stale
DXVK_FRAME_RATE values that GE-Proton can prefer over DXVK_CONFIG.
Limits remain launch-time only; external native games are not covered.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Medium

src/platform/linux/frame_limiter.cpp
@@ -1,0 +1,132 @@
+#include "frame_limiter.h"
+
+#include "mangohud_policy.h"
+#include "src/config.h"
+#include "src/logging.h"
+
+#include <array>
+#include <cerrno>
+#include <chrono>
+#include <cstdlib>
+#include <gio/gio.h>
+#include <gio/gunixinputstream.h>
+#include <memory>
+#include <mutex>
+#include <poll.h>
+#include <unistd.h>
+
+namespace platf {
+  namespace {
+    struct limiter_lease {
+      GSubprocess *process = nullptr;
+
+      ~limiter_lease() {
+        if (process) {
+          // Disconnecting the client makes the generation-bound broker cancel
+          // its exact transient unit. The socket policy dies with that unit.
+          g_subprocess_force_exit(process);
+          g_subprocess_wait(process, nullptr, nullptr);
+          g_object_unref(process);
+        }
+      }
+
+      bool alive() const {
+        auto *output = g_subprocess_get_stdout_pipe(process);
+        pollfd watched {g_unix_input_stream_get_fd(G_UNIX_INPUT_STREAM(output)), POLLIN, 0};
+        const int result = poll(&watched, 1, 0);
+        return result == 0 || (result < 0 && errno == EINTR);
+      }
+    };
+
+    std::mutex limiter_mutex;
+    std::array<bool, 2> owners {};
+    std::unique_ptr<limiter_lease> lease;
+
+    std::unique_ptr<limiter_lease> start(const mangohud::launch_policy_t &policy) {
+      const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
+      gchar *mangohud_path = g_find_program_in_path("mangohud");
+      const bool available = mangohud_path != nullptr;
+      g_free(mangohud_path);
+      if (!proton && !available) {
+        return {};
+      }
+      const bool overlay = available && (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
+      const char *provider = proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
+      const std::string limit = std::to_string(policy.limit_millihz);
+      const auto &configured_preset = config::frame_limiter.mangohud_preset;
+      const char *preset = overlay && (configured_preset == "1" || configured_preset == "2" ||
+                                       configured_preset == "3" || configured_preset == "4") ?
+                             configured_preset.c_str() :
+                             "custom";
+      const char *graph = overlay && config::frame_limiter.mangohud_always_show_graph ? "1" : "0";
+      const char *method = !proton && config::frame_limiter.mangohud_limiter_method == "early" ? "early" : "late";
+      auto result = std::make_unique<limiter_lease>();
+      GError *error = nullptr;
+      if (std::getenv("VIBESHINE_MACHINE_HOST")) {
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, nullptr);
+      } else {
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", nullptr);
+      }
+      if (!result->process) {
+        BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
+        g_clear_error(&error);
+        return {};
+      }
+      auto *output = g_subprocess_get_stdout_pipe(result->process);
+      pollfd watched {g_unix_input_stream_get_fd(G_UNIX_INPUT_STREAM(output)), POLLIN, 0};
+      const auto deadline = std::chrono::steady_clock::now() + std::chrono::seconds(15);
+      std::string ready_line;
+      while (std::chrono::steady_clock::now() < deadline) {
+        const int ready = poll(&watched, 1, 100);
+        if (ready < 0 && errno == EINTR) {
+          continue;
+        }
+        if (ready < 0 || (ready > 0 && !(watched.revents & POLLIN))) {
+          break;
+        }
+        if (!ready) {
+          continue;
+        }
+        char token;
+        if (read(watched.fd, &token, 1) != 1) {
+          break;
+        }
+        if (token == '\n') {
+          if (ready_line.starts_with("READY ")) {
+            BOOST_LOG(info) << "Global Linux " << provider << " limiter ready at " << policy.limit
+                            << " FPS for external Proton launches (" << ready_line.substr(6) << " installations).";
+            return result;
+          }
+          break;
+        }
+        if (ready_line.size() >= 64) {
+          break;
+        }
+        ready_line += token;
+      }
+      BOOST_LOG(warning) << "Global Linux limiter could not prepare external Proton launches; managed launch limiting remains available.";
+      return {};
+    }
+  }  // namespace
+
+  void frame_limiter_streaming_start(frame_limiter_owner owner, const framegen::stream_start_policy_t &stream_policy) {
+    std::lock_guard lock(limiter_mutex);
+    owners[static_cast<std::size_t>(owner)] = true;
+    if (lease && lease->alive()) {
+      return;
+    }
+    lease.reset();
+    const auto policy = mangohud::make_launch_policy(config::frame_limiter.provider, config::frame_limiter.enable, config::frame_limiter.virtual_display_limiter_enabled(), stream_policy, config::frame_limiter.fps_limit_millihz);
+    if (policy.enabled) {
+      lease = start(policy);
+    }
+  }
+
+  void frame_limiter_streaming_stop(frame_limiter_owner owner, bool) {
+    std::lock_guard lock(limiter_mutex);
+    owners[static_cast<std::size_t>(owner)] = false;
+    if (!owners[0] && !owners[1]) {
+      lease.reset();
+    }
+  }
+}  // namespace platf

tests/unit/platform/linux/test_global_limiter.py
@@ -1,0 +1,123 @@
+"""Exercise the real launch hook in a separate Proton-style process."""
+import importlib.util
+import json
+import os
+from pathlib import Path
+import select
+import subprocess
+import sys
+import tempfile
+import unittest
+import uuid
+
+SOURCE = Path(__file__).resolve().parents[4] / "packaging/linux/vibeshine-global-limiter.py"
+spec = importlib.util.spec_from_file_location("limiter", SOURCE)
+limiter = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(limiter)
+
+
+class GlobalLimiter(unittest.TestCase):
+    def setUp(self):
+        self.temporary = tempfile.TemporaryDirectory()
+        self.addCleanup(self.temporary.cleanup)
+        self.tool = Path(self.temporary.name)
+        self.source = self.tool / "helper.py"
+        self.source.write_bytes(SOURCE.read_bytes().replace(b"vibeshine.proton-limiter.v1.", ("vibeshine.test." + uuid.uuid4().hex + ".").encode()))
+        (self.tool / "user_settings.sample.py").write_text("user_settings = {}\n")
+        # Match Proton's copy-before-import and missing-keys-only semantics.
+        (self.tool / "proton").write_text('''import os, json
+class Session: pass
+g_session = Session()
+g_session.env = dict(os.environ)
+import user_settings
+for key, value in user_settings.user_settings.items():
+    g_session.env.setdefault(key, value)
+print(json.dumps({k: v for k, v in g_session.env.items() if k.startswith(("DXVK", "VKD3D", "MANGOHUD", "KEEP_"))}))
+''')
+        self.original = b'user_settings = {"KEEP_SETTING": "yes", "DXVK_CONFIG": "dxvk.hud = fps"}\n'
+        self.settings = self.tool / "user_settings.py"
+        self.settings.write_bytes(self.original)
+        self.settings.chmod(0o640)
+
+    def server(self, provider="proton", millihz=59940):
+        child = subprocess.Popen([sys.executable, "-I", str(self.source), provider, str(millihz), "custom", "0", "late", str(self.tool)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        def stop():
+            if child.poll() is None:
+                child.terminate()
+                child.wait(timeout=8)
+            child.stdout.close()
+            child.stderr.close()
+        self.addCleanup(stop)
+        self.assertTrue(select.select([child.stdout], [], [], 8)[0], "helper readiness timed out")
+        ready = child.stdout.readline()
+        self.assertEqual(ready, "READY 1\n", child.stderr.read() if not ready else ready)
+        return child
+
+    def launch(self, **overrides):
+        env = {k: v for k, v in os.environ.items() if not k.startswith(("DXVK", "VKD3D", "MANGOHUD", "LD_PRELOAD"))}
+        env.update(overrides)
+        return json.loads(subprocess.check_output([sys.executable, str(self.tool / "proton")], env=env, text=True))
+
+    def test_external_launch_and_disconnect(self):
+        child = self.server()
+        active = self.launch(DXVK_FRAME_RATE="30", VKD3D_FRAME_RATE="30", KEEP_ENV="untouched", MANGOHUD="1")
+        self.assertEqual(active["VKD3D_FRAME_RATE"], "59.94")
+        self.assertEqual(active["DXVK_FRAME_RATE"], "60")
+        self.assertIn("dxvk.hud = fps", active["DXVK_CONFIG"])
+        self.assertIn("dxgi.maxFrameRate = 60", active["DXVK_CONFIG"])
+        self.assertNotIn("MANGOHUD", active)
+        self.assertEqual(active["KEEP_ENV"], "untouched")
+        self.assertEqual(active["KEEP_SETTING"], "yes")
+        managed = self.launch(VIBESHINE_LIMITER_MANAGED="1", VKD3D_FRAME_RATE="45")
+        self.assertEqual(managed["VKD3D_FRAME_RATE"], "45")
+        self.assertEqual(self.settings.read_bytes(), self.original + limiter.BLOCK)
+        self.assertEqual(self.settings.stat().st_mode & 0o777, 0o640)
+        child.terminate()
+        child.wait(timeout=8)
+        inactive = self.launch(DXVK_FRAME_RATE="30")
+        self.assertEqual(inactive["DXVK_FRAME_RATE"], "30")
+        self.assertNotIn("VKD3D_FRAME_RATE", inactive)
+        self.assertEqual(inactive["DXVK_CONFIG"], "dxvk.hud = fps")
+
+    def test_crash_leaves_inert_hook(self):
+        child = self.server()
+        child.kill()
+        child.wait(timeout=8)
+        self.assertNotIn("VKD3D_FRAME_RATE", self.launch())
+        limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(self.settings.read_bytes(), self.original + limiter.BLOCK)
+
+    def test_fractional_mangohud_and_combined(self):
+        child = self.server("mangohud")
+        active = self.launch(MANGOHUD_FPS_LIMIT="30", MANGOHUD_CONFIG="fps_limit=30")
+        self.assertNotIn("MANGOHUD_FPS_LIMIT", active)
+        self.assertTrue(active["MANGOHUD_CONFIG"].endswith("fps_limit=59.94"))
+        child.terminate()
+        child.wait(timeout=8)
+        self.server("mangohud-proton")
+        combined = self.launch(MANGOHUD_FPS_LIMIT="30")
+        self.assertEqual(combined["MANGOHUD"], "1")
+        self.assertTrue(combined["MANGOHUD_CONFIG"].endswith("fps_limit=0"))
+        self.assertEqual(combined["VKD3D_FRAME_RATE"], "59.94")
+
+    def test_no_settings_and_symlink_preservation(self):
+        self.settings.unlink()
+        limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(self.settings.read_bytes(), limiter.BLOCK)
+        self.settings.unlink()
+        other = self.tool / "other.py"
+        other.write_bytes(self.original)
+        self.settings.symlink_to(other)
+        with self.assertRaises(OSError):
+            limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(other.read_bytes(), self.original)
+
+    def test_discovery_and_validation(self):
+        self.assertEqual(limiter.tools_in([self.tool, self.tool]), {self.tool})
+        for invalid in (["proton", 0, "custom", False, "late"], ["bogus", 60000, "custom", False, "late"], ["proton", 60000, "custom", False, "late", "extra"]):
+            with self.assertRaises(ValueError):
+                limiter.environment(invalid, {})
+
+
+if __name__ == "__main__":
+    unittest.main()

Nonary/Vibepollo

(cherry picked from commit 25362b93130d970ecbd5d1ef95e793670ffb8202)

src/platform/linux/frame_limiter.cpp
@@ -1,0 +1,132 @@
+#include "frame_limiter.h"
+
+#include "mangohud_policy.h"
+#include "src/config.h"
+#include "src/logging.h"
+
+#include <array>
+#include <cerrno>
+#include <chrono>
+#include <cstdlib>
+#include <gio/gio.h>
+#include <gio/gunixinputstream.h>
+#include <memory>
+#include <mutex>
+#include <poll.h>
+#include <unistd.h>
+
+namespace platf {
+  namespace {
+    struct limiter_lease {
+      GSubprocess *process = nullptr;
+
+      ~limiter_lease() {
+        if (process) {
+          // Disconnecting the client makes the generation-bound broker cancel
+          // its exact transient unit. The socket policy dies with that unit.
+          g_subprocess_force_exit(process);
+          g_subprocess_wait(process, nullptr, nullptr);
+          g_object_unref(process);
+        }
+      }
+
+      bool alive() const {
+        auto *output = g_subprocess_get_stdout_pipe(process);
+        pollfd watched {g_unix_input_stream_get_fd(G_UNIX_INPUT_STREAM(output)), POLLIN, 0};
+        const int result = poll(&watched, 1, 0);
+        return result == 0 || (result < 0 && errno == EINTR);
+      }
+    };
+
+    std::mutex limiter_mutex;
+    std::array<bool, 2> owners {};
+    std::unique_ptr<limiter_lease> lease;
+
+    std::unique_ptr<limiter_lease> start(const mangohud::launch_policy_t &policy) {
+      const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
+      gchar *mangohud_path = g_find_program_in_path("mangohud");
+      const bool available = mangohud_path != nullptr;
+      g_free(mangohud_path);
+      if (!proton && !available) {
+        return {};
+      }
+      const bool overlay = available && (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
+      const char *provider = proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
+      const std::string limit = std::to_string(policy.limit_millihz);
+      const auto &configured_preset = config::frame_limiter.mangohud_preset;
+      const char *preset = overlay && (configured_preset == "1" || configured_preset == "2" ||
+                                       configured_preset == "3" || configured_preset == "4") ?
+                             configured_preset.c_str() :
+                             "custom";
+      const char *graph = overlay && config::frame_limiter.mangohud_always_show_graph ? "1" : "0";
+      const char *method = !proton && config::frame_limiter.mangohud_limiter_method == "early" ? "early" : "late";
+      auto result = std::make_unique<limiter_lease>();
+      GError *error = nullptr;
+      if (std::getenv("VIBESHINE_MACHINE_HOST")) {
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, nullptr);
+      } else {
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", nullptr);
+      }
+      if (!result->process) {
+        BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
+        g_clear_error(&error);
+        return {};
+      }
+      auto *output = g_subprocess_get_stdout_pipe(result->process);
+      pollfd watched {g_unix_input_stream_get_fd(G_UNIX_INPUT_STREAM(output)), POLLIN, 0};
+      const auto deadline = std::chrono::steady_clock::now() + std::chrono::seconds(15);
+      std::string ready_line;
+      while (std::chrono::steady_clock::now() < deadline) {
+        const int ready = poll(&watched, 1, 100);
+        if (ready < 0 && errno == EINTR) {
+          continue;
+        }
+        if (ready < 0 || (ready > 0 && !(watched.revents & POLLIN))) {
+          break;
+        }
+        if (!ready) {
+          continue;
+        }
+        char token;
+        if (read(watched.fd, &token, 1) != 1) {
+          break;
+        }
+        if (token == '\n') {
+          if (ready_line.starts_with("READY ")) {
+            BOOST_LOG(info) << "Global Linux " << provider << " limiter ready at " << policy.limit
+                            << " FPS for external Proton launches (" << ready_line.substr(6) << " installations).";
+            return result;
+          }
+          break;
+        }
+        if (ready_line.size() >= 64) {
+          break;
+        }
+        ready_line += token;
+      }
+      BOOST_LOG(warning) << "Global Linux limiter could not prepare external Proton launches; managed launch limiting remains available.";
+      return {};
+    }
+  }  // namespace
+
+  void frame_limiter_streaming_start(frame_limiter_owner owner, const framegen::stream_start_policy_t &stream_policy) {
+    std::lock_guard lock(limiter_mutex);
+    owners[static_cast<std::size_t>(owner)] = true;
+    if (lease && lease->alive()) {
+      return;
+    }
+    lease.reset();
+    const auto policy = mangohud::make_launch_policy(config::frame_limiter.provider, config::frame_limiter.enable, config::frame_limiter.virtual_display_limiter_enabled(), stream_policy, config::frame_limiter.fps_limit_millihz);
+    if (policy.enabled) {
+      lease = start(policy);
+    }
+  }
+
+  void frame_limiter_streaming_stop(frame_limiter_owner owner, bool) {
+    std::lock_guard lock(limiter_mutex);
+    owners[static_cast<std::size_t>(owner)] = false;
+    if (!owners[0] && !owners[1]) {
+      lease.reset();
+    }
+  }
+}  // namespace platf

tests/unit/platform/linux/test_global_limiter.py
@@ -1,0 +1,123 @@
+"""Exercise the real launch hook in a separate Proton-style process."""
+import importlib.util
+import json
+import os
+from pathlib import Path
+import select
+import subprocess
+import sys
+import tempfile
+import unittest
+import uuid
+
+SOURCE = Path(__file__).resolve().parents[4] / "packaging/linux/vibepollo-global-limiter.py"
+spec = importlib.util.spec_from_file_location("limiter", SOURCE)
+limiter = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(limiter)
+
+
+class GlobalLimiter(unittest.TestCase):
+    def setUp(self):
+        self.temporary = tempfile.TemporaryDirectory()
+        self.addCleanup(self.temporary.cleanup)
+        self.tool = Path(self.temporary.name)
+        self.source = self.tool / "helper.py"
+        self.source.write_bytes(SOURCE.read_bytes().replace(b"vibepollo.proton-limiter.v1.", ("vibepollo.test." + uuid.uuid4().hex + ".").encode()))
+        (self.tool / "user_settings.sample.py").write_text("user_settings = {}\n")
+        # Match Proton's copy-before-import and missing-keys-only semantics.
+        (self.tool / "proton").write_text('''import os, json
+class Session: pass
+g_session = Session()
+g_session.env = dict(os.environ)
+import user_settings
+for key, value in user_settings.user_settings.items():
+    g_session.env.setdefault(key, value)
+print(json.dumps({k: v for k, v in g_session.env.items() if k.startswith(("DXVK", "VKD3D", "MANGOHUD", "KEEP_"))}))
+''')
+        self.original = b'user_settings = {"KEEP_SETTING": "yes", "DXVK_CONFIG": "dxvk.hud = fps"}\n'
+        self.settings = self.tool / "user_settings.py"
+        self.settings.write_bytes(self.original)
+        self.settings.chmod(0o640)
+
+    def server(self, provider="proton", millihz=59940):
+        child = subprocess.Popen([sys.executable, "-I", str(self.source), provider, str(millihz), "custom", "0", "late", str(self.tool)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        def stop():
+            if child.poll() is None:
+                child.terminate()
+                child.wait(timeout=8)
+            child.stdout.close()
+            child.stderr.close()
+        self.addCleanup(stop)
+        self.assertTrue(select.select([child.stdout], [], [], 8)[0], "helper readiness timed out")
+        ready = child.stdout.readline()
+        self.assertEqual(ready, "READY 1\n", child.stderr.read() if not ready else ready)
+        return child
+
+    def launch(self, **overrides):
+        env = {k: v for k, v in os.environ.items() if not k.startswith(("DXVK", "VKD3D", "MANGOHUD", "LD_PRELOAD"))}
+        env.update(overrides)
+        return json.loads(subprocess.check_output([sys.executable, str(self.tool / "proton")], env=env, text=True))
+
+    def test_external_launch_and_disconnect(self):
+        child = self.server()
+        active = self.launch(DXVK_FRAME_RATE="30", VKD3D_FRAME_RATE="30", KEEP_ENV="untouched", MANGOHUD="1")
+        self.assertEqual(active["VKD3D_FRAME_RATE"], "59.94")
+        self.assertEqual(active["DXVK_FRAME_RATE"], "60")
+        self.assertIn("dxvk.hud = fps", active["DXVK_CONFIG"])
+        self.assertIn("dxgi.maxFrameRate = 60", active["DXVK_CONFIG"])
+        self.assertNotIn("MANGOHUD", active)
+        self.assertEqual(active["KEEP_ENV"], "untouched")
+        self.assertEqual(active["KEEP_SETTING"], "yes")
+        managed = self.launch(VIBEPOLLO_LIMITER_MANAGED="1", VKD3D_FRAME_RATE="45")
+        self.assertEqual(managed["VKD3D_FRAME_RATE"], "45")
+        self.assertEqual(self.settings.read_bytes(), self.original + limiter.BLOCK)
+        self.assertEqual(self.settings.stat().st_mode & 0o777, 0o640)
+        child.terminate()
+        child.wait(timeout=8)
+        inactive = self.launch(DXVK_FRAME_RATE="30")
+        self.assertEqual(inactive["DXVK_FRAME_RATE"], "30")
+        self.assertNotIn("VKD3D_FRAME_RATE", inactive)
+        self.assertEqual(inactive["DXVK_CONFIG"], "dxvk.hud = fps")
+
+    def test_crash_leaves_inert_hook(self):
+        child = self.server()
+        child.kill()
+        child.wait(timeout=8)
+        self.assertNotIn("VKD3D_FRAME_RATE", self.launch())
+        limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(self.settings.read_bytes(), self.original + limiter.BLOCK)
+
+    def test_fractional_mangohud_and_combined(self):
+        child = self.server("mangohud")
+        active = self.launch(MANGOHUD_FPS_LIMIT="30", MANGOHUD_CONFIG="fps_limit=30")
+        self.assertNotIn("MANGOHUD_FPS_LIMIT", active)
+        self.assertTrue(active["MANGOHUD_CONFIG"].endswith("fps_limit=59.94"))
+        child.terminate()
+        child.wait(timeout=8)
+        self.server("mangohud-proton")
+        combined = self.launch(MANGOHUD_FPS_LIMIT="30")
+        self.assertEqual(combined["MANGOHUD"], "1")
+        self.assertTrue(combined["MANGOHUD_CONFIG"].endswith("fps_limit=0"))
+        self.assertEqual(combined["VKD3D_FRAME_RATE"], "59.94")
+
+    def test_no_settings_and_symlink_preservation(self):
+        self.settings.unlink()
+        limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(self.settings.read_bytes(), limiter.BLOCK)
+        self.settings.unlink()
+        other = self.tool / "other.py"
+        other.write_bytes(self.original)
+        self.settings.symlink_to(other)
+        with self.assertRaises(OSError):
+            limiter.install(self.tool, self.source.read_bytes())
+        self.assertEqual(other.read_bytes(), self.original)
+
+    def test_discovery_and_validation(self):
+        self.assertEqual(limiter.tools_in([self.tool, self.tool]), {self.tool})
+        for invalid in (["proton", 0, "custom", False, "late"], ["bogus", 60000, "custom", False, "late"], ["proton", 60000, "custom", False, "late", "extra"]):
+            with self.assertRaises(ValueError):
+                limiter.environment(invalid, {})
+
+
+if __name__ == "__main__":
+    unittest.main()